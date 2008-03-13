f1f09da4d6b54d269499bba15ed7a50669cb8282
mustafa <mustafa@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-03-13 13:56:32 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@341 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

---
+++ RapidCMDB/src/groovy/x.groovy
@@ -0,0 +1,10 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: Mar 13, 2008
+ * Time: 3:51:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class x {
+
+}

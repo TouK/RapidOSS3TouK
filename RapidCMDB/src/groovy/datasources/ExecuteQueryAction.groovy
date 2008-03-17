b5cc93030fca26081c2a6545cc2ea4e46a7702a2
mustafa <mustafa@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-03-17 06:51:01 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@372 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

---
+++ RapidCMDB/src/groovy/datasources/ExecuteQueryAction.groovy
@@ -0,0 +1,45 @@
+package datasources;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import org.apache.log4j.Logger;
+
+import com.ifountain.core.connection.IConnection;
+import com.ifountain.core.datasource.Action;
+import connections.DatabaseConnectionImpl;
+
+public class ExecuteQueryAction implements Action {
+
+    private Logger logger;
+    private Object[] queryParams;
+    private String sql;
+    private int fetchSize = 0;
+    private ResultSet resultSet;
+    public ExecuteQueryAction(Logger logger, String sql, Object[] queryParams, int fetchSize) {
+        this.logger = logger;
+        this.queryParams = queryParams;
+        this.sql = sql;
+        this.fetchSize = fetchSize;
+    }
+    public ExecuteQueryAction(Logger logger, String sql, Object[] queryParams) {
+        this(logger, sql, queryParams, 0);
+    }
+
+    public void execute(IConnection conn) throws Exception {
+        if(queryParams == null) {
+            throw new Exception("QueryParameters cannot be null.");
+        }
+        logger.debug("Preparing statement.");
+        PreparedStatement stmt = ((DatabaseConnectionImpl)conn).getConnection().prepareStatement(sql);
+        DatabaseConnectionImpl.setStatementParameters(queryParams, stmt);
+        if(fetchSize > 0)
+            stmt.setFetchSize(fetchSize);
+        logger.debug("Executing query.");
+        resultSet = stmt.executeQuery();
+    }
+    public ResultSet getResultSet() {
+        return resultSet;
+    }
+
+}

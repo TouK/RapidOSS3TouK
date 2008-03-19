73cd30a27ab6b3f2b72554ca90ff1bfa13219d0b
sezgin <sezgin@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-03-19 16:16:33 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@415 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/grails-app/conf/BootStrap.groovy
+++ RapidCMDB/grails-app/conf/BootStrap.groovy
@@ -3,6 +3,10 @@ import org.apache.log4j.Logger
 class BootStrap {
 
      def init = { servletContext ->
+        def rcmdbDatasource = RCMDBDatasource.findByName(RapidCMDBConstants.RCMDB);
+        if(rcmdbDatasource == null){
+            new RCMDBDatasource(name:RapidCMDBConstants.RCMDB).save();
+        }
      }
      def destroy = {
      }

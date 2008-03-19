0d66baa84a530e49def3ba1f9f25025e0e018934
mustafa <mustafa@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-03-19 00:06:53 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@405 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

---
+++ RapidCMDB/rs.bat
@@ -0,0 +1,12 @@
+@if "%DEBUG%" == "" @echo off
+
+@rem 
+@rem $Revision: 2770 $ $Date: 2005-08-29 10:49:42 +0000 (Mon, 29 Aug 2005) $
+@rem 
+
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+
+:begin
+
+CALL "%GRAILS_HOME%\bin\startGrails.bat" "%GRAILS_HOME%" org.codehaus.groovy.grails.cli.GrailsScriptRunner  %* -Dserver.port=12222

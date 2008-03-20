7728d98b637f96f8bcb57fb19cea27b25acbc491
pinar <pinar@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-03-20 14:44:00 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@440 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

---
+++ RapidCMDB/grails-app/scripts/FindCustomersWithGivenSlalevelForServicesWithDownLinks.groovy
@@ -0,0 +1,49 @@
+/* All content copyright (C) 2004-2008 iFountain, LLC., except as may otherwise be 
+ * noted in a separate copyright notice. All rights reserved.
+ * This file is part of RapidCMDB.
+ * 
+ * RapidCMDB is free software; you can redistribute it and/or modify
+ * it under the terms version 2 of the GNU General Public License as
+ * published by the Free Software Foundation. This program is distributed
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA.
+ */
+/**
+ * Created by IntelliJ IDEA.
+ * User: Pinar Kinikoglu
+ * Date: Mar 20, 2008
+ * Time: 2:35:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+/*
+  Find the customers with a specific service agreement for those services with down links
+
+  Slalevel is provided as parameter
+*/
+def slaLevel = params.Slalevel;
+def custInfo = [];
+
+def downLinks = Links.findByOperationalstate(slaLevel);
+downLinks.each{
+    def services = it.getServices();
+    for (service in services){
+        def slas = Sla.findByServiceAndSlalevel(service, slaLevel);
+        for (sla in slas){
+            custInfo.add(sla.customer.accountmanager);
+        }
+    }
+}
+
+renderOutput(custInfo);
+
+def renderOutput(info){
+
+}
+
---
+++ RapidCMDB/grails-app/scripts/allTests.groovy
@@ -0,0 +1,62 @@
+/* All content copyright (C) 2004-2008 iFountain, LLC., except as may otherwise be 
+ * noted in a separate copyright notice. All rights reserved.
+ * This file is part of RapidCMDB.
+ * 
+ * RapidCMDB is free software; you can redistribute it and/or modify
+ * it under the terms version 2 of the GNU General Public License as
+ * published by the Free Software Foundation. This program is distributed
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA.
+ */
+/**
+ * Created by IntelliJ IDEA.
+ * User: Pinar Kinikoglu
+ * Date: Mar 20, 2008
+ * Time: 2:57:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+// Test lazy load
+import datasources.SingleTableDatabaseAdapter;
+def name = "device1";
+Device device = Device.findByName(name);
+def oldip = device.ipaddress;
+SingleTableDatabaseDatasource ds = SingleTableDatabaseDatasource.findByName("DeviceDS");
+SingleTableDatabaseAdapter adapter = ds.adapter;
+adapter.updateRecord(['id':name, 'ipaddress':'bogus ip']);
+def record = adapter.getRecord(name);
+if (record.ipaddress == oldip){
+    println "Could not properly update record!"
+}
+def newip = device.ipaddress;
+if (oldip == newip){
+    println "Error: Lazy load doesn't work! (oldip: $oldip vs. newip: $newip)"
+}
+else{
+    println "Success in lazy load! (oldip: $oldip vs. newip: $newip)"
+}
+adapter.updateRecord(['id':name, 'ipaddress':oldip]);
+
+// Test federated data can not be modified
+device.ipaddress = '192.168.1.1';
+newip = device.ipaddress ;
+
+if (newip != oldip){
+    println("Error: Federated data is updated!")
+}
+
+ //  Test master data can be updated in RCMDB database
+device.name = 'device1_1';
+def newname = device.name;
+if (newip == oldip){
+     println("Error: Master data could not be updated!")
+}
+
+
+

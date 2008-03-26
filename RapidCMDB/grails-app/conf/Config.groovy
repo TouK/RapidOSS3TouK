de38b20608e7683b34c4d3a120c73c9a9b32e540
sezgin <sezgin@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-03-26 09:46:35 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@456 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/grails-app/conf/Config.groovy
+++ RapidCMDB/grails-app/conf/Config.groovy
@@ -27,6 +27,7 @@ grails.mime.types = [ html: ['text/html','application/xhtml+xml'],
 grails.views.default.codec="none" // none, html, base64
 grails.views.gsp.encoding="UTF-8"
 grails.converters.encoding="UTF-8"
+grails.test.default.rollback = true
 
 // enabled native2ascii conversion of i18n properties files
 grails.enable.native2ascii = true

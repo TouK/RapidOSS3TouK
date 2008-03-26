f7704ac9255df38ced8d328ce526c442ae1e096e
sezgin <sezgin@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-03-26 09:47:05 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@457 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/grails-app/controllers/model/ModelDatasourceController.groovy
+++ RapidCMDB/grails-app/controllers/model/ModelDatasourceController.groovy
@@ -5,13 +5,13 @@ class ModelDatasourceController {
     def scaffold = ModelDatasource;
 
     def save = {
-        def modelDatasource = new ModelDatasource(params)
+        def modelDatasource = new ModelDatasource(params);
         if(!modelDatasource.hasErrors() && modelDatasource.save()) {
             flash.message = "ModelDatasource ${modelDatasource.id} created"
             redirect(action:show,controller:'model', id:modelDatasource.model?.id)
         }
         else {
-            render(view:'create',model:[modelDatasource:modelDatasource])
+            _render(view:'create',model:[modelDatasource:modelDatasource])
         }
     }
 
@@ -25,7 +25,13 @@ class ModelDatasourceController {
         }
         else {
             flash.message = "ModelDatasource not found with id ${params.id}"
-            redirect(action:list)
+            redirect(action:list, controller:'modelDatasource')
         }
+    }
+
+    def _render = {Map params ->
+        def view = params.view;
+        def model = params.model;
+        render(view:view, model:model);
     }
 }

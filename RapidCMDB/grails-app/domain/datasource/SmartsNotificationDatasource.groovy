a53aa01a8f88b52ebe291967f8e24cd60ed8318a
pinar <pinar@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-03-28 13:00:08 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@472 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/grails-app/domain/datasource/SmartsNotificationDatasource.groovy
+++ RapidCMDB/grails-app/domain/datasource/SmartsNotificationDatasource.groovy
@@ -10,22 +10,153 @@ class SmartsNotificationDatasource extends BaseDatasource{
 
 
     def onLoad = {
-       this.adapter = new NotificationAdapter(connection.name, 0, Logger.getRootLogger());
+        this.adapter = new NotificationAdapter(connection.name, 0, Logger.getRootLogger());
     }
 
-    def getProperty(Map keys, String propName)
-     {
-         def prop = this.adapter.getNotification (keys.ClassName, keys.InstanceName, keys.EventName, [propName]);
-         if(prop)
-         {
-             return prop[propName];
-         }
-         return "";
-     }
+    def getProperty(Map keys, String propName){
+        def prop = this.adapter.getNotification (keys.ClassName, keys.InstanceName, keys.EventName, [propName]);
+        if(prop)
+        {
+            return prop[propName];
+        }
+        return "";
+    }
 
-     def getProperties(Map keys, List properties)
-     {
-         def prop = this.adapter.getNotification (keys.ClassName, keys.InstanceName, keys.EventName);
-         return prop;
-     }
+    def getProperties(Map keys, List properties){
+        def prop = this.adapter.getNotification (keys.ClassName, keys.InstanceName, keys.EventName);
+        return prop;
+    }
+
+    def addNotification(Map params){
+        def tempParams = [:];
+        tempParams.putAll(params);
+        def className = tempParams.ClassName;
+        def instanceName = tempParams.InstanceName;
+        def eventName = tempParams.EventName;
+        tempParams.remove("ClassName");
+        tempParams.remove("InstanceName");
+        tempParams.remove("EventName");
+        this.adapter.createNotification(className, instanceName, eventName, tempParams);
+    }
+
+    def getNotification(Map keys) {
+        Map<String, Object> result = this.adapter.getNotification(keys.ClassName, keys.InstanceName, keys.EventName);
+        if(!result){
+            result = [:];
+        }
+        return result;
+    }
+
+    def getNotification(Map keys, List attributes) {
+        Map<String, Object> result = this.adapter.getNotification(keys.ClassName, keys.InstanceName, keys.EventName, attributes);
+        if(!result){
+            result = [:];
+        }
+        return result;
+    }
+
+    def getNotifications(Map keys) {
+        return this.adapter.getNotifications(keys.ClassName, keys.InstanceName, keys.EventName);
+    }
+
+    def getNotifications(Map keys, boolean expEnabled) {
+        return this.adapter.getNotifications(keys.ClassName, keys.InstanceName, keys.EventName, expEnabled);
+    }
+
+    def getNotifications(Map keys, List properties, boolean expEnabled) {
+        return this.adapter.getNotifications(keys.ClassName, keys.InstanceName, keys.EventName, properties, expEnabled);
+    }
+
+
+    def updateNotification(Map params){
+        def tempParams = [:];
+        tempParams.putAll(params);
+        def className = tempParams.ClassName;
+        def instanceName = tempParams.InstanceName;
+        def eventName = tempParams.EventName;
+        tempParams.remove("ClassName");
+        tempParams.remove("InstanceName");
+        tempParams.remove("EventName");
+        this.adapter.updateNotification(className, instanceName, eventName, tempParams);
+    }
+
+    def archiveNotification(Map params){
+        def tempParams = [:];
+        tempParams.putAll(params);
+        def className = tempParams.ClassName;
+        def instanceName = tempParams.InstanceName;
+        def eventName = tempParams.EventName;
+        def user = tempParams.User;
+        def auditTrailText = tempParams.AuditTrailText;
+        return this.adapter.archiveNotification(className, instanceName, eventName, user, auditTrailText);
+    }
+
+    def clearNotification(Map params){
+        def tempParams = [:];
+        tempParams.putAll(params);
+        def className = tempParams.ClassName;
+        def instanceName = tempParams.InstanceName;
+        def eventName = tempParams.EventName;
+        def user = tempParams.User;
+        def source = tempParams.Source;
+        def auditTrailText = tempParams.AuditTrailText;
+        return this.adapter.clearNotification(className, instanceName, eventName, source, user, auditTrailText);
+    }
+
+    def acknowledge(Map params){
+        def tempParams = [:];
+        tempParams.putAll(params);
+        def className = tempParams.ClassName;
+        def instanceName = tempParams.InstanceName;
+        def eventName = tempParams.EventName;
+        def user = tempParams.User;
+        def auditTrailText = tempParams.AuditTrailText;
+        return this.adapter.acknowledge(className, instanceName, eventName, user, auditTrailText);
+    }
+
+    def unacknowledge(Map params){
+        def tempParams = [:];
+        tempParams.putAll(params);
+        def className = tempParams.ClassName;
+        def instanceName = tempParams.InstanceName;
+        def eventName = tempParams.EventName;
+        def user = tempParams.User;
+        def auditTrailText = tempParams.AuditTrailText;
+        return this.adapter.unacknowledge(className, instanceName, eventName, user, auditTrailText);
+    }
+
+    def takeOwnership(Map params){
+        def tempParams = [:];
+        tempParams.putAll(params);
+        def className = tempParams.ClassName;
+        def instanceName = tempParams.InstanceName;
+        def eventName = tempParams.EventName;
+        def user = tempParams.User;
+        def auditTrailText = tempParams.AuditTrailText;
+        return this.adapter.takeOwnership(className, instanceName, eventName, user, auditTrailText);
+    }
+
+    def releaseOwnership(Map params){
+        def tempParams = [:];
+        tempParams.putAll(params);
+        def className = tempParams.ClassName;
+        def instanceName = tempParams.InstanceName;
+        def eventName = tempParams.EventName;
+        def user = tempParams.User;
+        def auditTrailText = tempParams.AuditTrailText;
+        return this.adapter.releaseOwnership(className, instanceName, eventName, user, auditTrailText);
+    }
+
+    def addAuditLog(Map params){
+        def tempParams = [:];
+        tempParams.putAll(params);
+        def className = tempParams.ClassName;
+        def instanceName = tempParams.InstanceName;
+        def eventName = tempParams.EventName;
+        def user = tempParams.User;
+        def auditTrailText = tempParams.AuditTrailText;
+        def action = tempParams.Action;
+        return this.adapter.addAuditEntry(className, instanceName, eventName, user, auditTrailText, action);
+    }
+
 }
--- ext/smarts/groovy/datasources/NotificationAdapter.groovy
+++ ext/smarts/groovy/datasources/NotificationAdapter.groovy
@@ -12,16 +12,4 @@ public class NotificationAdapter extends BaseNotificationAdapter {
     public NotificationAdapter(String datasourceName, long reconnectInterval, Logger logger) {
         super(datasourceName, reconnectInterval, logger); 
     }
-    
-
-    
-    public Map<String, Object> getNotification(String className, String instanceName, String eventName, List<String> attributes) {
-        Map<String, Object> result = super.getNotification(className, instanceName, eventName, attributes); //To change body of overridden methods use File | Settings | File Templates.
-        if(!result)
-        {
-            result = [:];
-        }
-        return result;
-    }
-    
 }

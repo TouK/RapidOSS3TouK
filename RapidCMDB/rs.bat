66c641dcd5dc6a9341fd0249f8af467c12de1cbc
pinar <pinar@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-03-31 12:37:24 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@500 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/rs.bat
+++ RapidCMDB/rs.bat
@@ -1,9 +1,5 @@
 @if "%DEBUG%" == "" @echo off
 
-@rem 
-@rem $Revision: 2770 $ $Date: 2005-08-29 10:49:42 +0000 (Mon, 29 Aug 2005) $
-@rem 
-
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 

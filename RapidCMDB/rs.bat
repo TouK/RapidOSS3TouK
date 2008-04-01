78500a25af4d7fa43169b3e8667d3e2fe9f2e43e
pinar <pinar@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-04-01 14:23:08 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@529 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/rs.bat
+++ RapidCMDB/rs.bat
@@ -5,5 +5,5 @@ if "%OS%"=="Windows_NT" setlocal
 
 :begin
 set GRAILS_HOME=%RS_HOME%
-rm -r %RS_HOME%\temp
+rmdir /s /q %RS_HOME%\temp
 CALL "%GRAILS_HOME%\bin\startGrails.bat" "%GRAILS_HOME%" org.codehaus.groovy.grails.cli.GrailsScriptRunner  %* -Dserver.port=12222 -Dgrails.work.dir=%RS_HOME%\temp

c97084ef102229f4a9fbceb65b2474107862b8a1
pinar <pinar@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-04-02 06:41:36 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@542 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/scripts/build/RapidCmdbBuild.groovy
+++ RapidCMDB/scripts/build/RapidCmdbBuild.groovy
@@ -47,10 +47,14 @@ class RapidCmdbBuild extends Build{
 			ant.fileset(dir : "$env.rapid_cmdb_cvs/grails-app"){
                 ant.exclude(name:"**/test/**")
                 ant.exclude(name:"**/*Test*")
-                ant.exclude(name:"**/grails-app/domain/*.groovy")
+                ant.exclude(name:"/domain/*.groovy")
+                ant.exclude(name:"/controller/*.groovy")
                 ant.exclude(name:"**/scripts/*.groovy")
             }
 		}
+
+		ant.copy(file : "$env.rapid_cmdb_cvs/grails-app/scripts/HelloWorld.groovy", toDir : "$env.dist_rapid_cmdb/grails-app/scripts" );
+
 		ant.copy(todir : "$env.dist_rapid_cmdb/lib"){
 			ant.fileset(dir : "$env.rapid_cmdb_cvs/lib")
         }

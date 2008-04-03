b2a7e9095d3dcb9da475e6dedcb0272405a108c6
tugrul <tugrul@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-04-03 11:26:10 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@557 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/rs.bat
+++ RapidCMDB/rs.bat
@@ -3,7 +3,20 @@
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
+if "%JAVA_HOME%"=="" (
+  echo JAVA_HOME is not defined.
+  goto:end
+)
+
+if "%RS_HOME%"=="" (
+  echo RS_HOME is not defined.
+  goto:end
+)
+
 :begin
 set GRAILS_HOME=%RS_HOME%
 rmdir /s /q %RS_HOME%\temp
-CALL "%GRAILS_HOME%\bin\startGrails.bat" "%GRAILS_HOME%" org.codehaus.groovy.grails.cli.GrailsScriptRunner  %* -Dserver.port=12222 -Dgrails.work.dir=%RS_HOME%\temp
+CALL "%GRAILS_HOME%\bin\startGrails.bat" "%GRAILS_HOME%" org.codehaus.groovy.grails.cli.GrailsScriptRunner  %* -Dserver.port=12222
+-Dgrails.work.dir=%RS_HOME%\temp
+
+:end

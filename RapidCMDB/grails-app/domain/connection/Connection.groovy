a8edc25c8440dea9d3bc8711ef6bffadb0a43366
sezgin <sezgin@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-04-04 14:05:11 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@578 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/grails-app/domain/connection/Connection.groovy
+++ RapidCMDB/grails-app/domain/connection/Connection.groovy
@@ -7,6 +7,9 @@ class Connection {
             name(unique:true, blank:false, nullable:false);
             connectionClass(blank:false, nullable:false);
        };
+       static mapping = {
+	      table 'base_connection'
+	  } 
 
        String toString(){
            return "$name";

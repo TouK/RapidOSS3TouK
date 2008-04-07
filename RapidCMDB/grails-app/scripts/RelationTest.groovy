a3bf0b97ccdc5dbc0ac93d4ee73b949ee3f2ee2c
sezgin <sezgin@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-04-07 11:36:22 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@591 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/grails-app/scripts/RelationTest.groovy
+++ RapidCMDB/grails-app/scripts/RelationTest.groovy
@@ -30,6 +30,10 @@ import model.ModelRelation
  * Time: 10:59:07 AM
  * To change this template use File | Settings | File Templates.
  */
+Model.list().each{
+    it.parentModel = null;
+    it.save(flush: true);
+}
 Model.list()*.delete(flush:true);
 def rcmdbDatasource = BaseDatasource.findByName("RCMDB");
 
@@ -37,23 +41,45 @@ def smartsObject = Model.create(name:"SmartsObject");
 def modelDatasource = ModelDatasource.create(datasource:rcmdbDatasource, master:true);
 def creationClassName = ModelProperty.create(name:"creationClassName", type:ModelProperty.stringType, blank:false, lazy:false, propertyDatasource:modelDatasource);
 def name = ModelProperty.create(name:"name", type:ModelProperty.stringType, blank:false, lazy:false, propertyDatasource:modelDatasource);
-def keyMappings = [new ModelDatasourceKeyMapping(property:creationClassName, datasource:modelDatasource),
-                    new ModelDatasourceKeyMapping(property:name, datasource:modelDatasource)]
+def keyMappings = [ModelDatasourceKeyMapping.create(property:creationClassName, datasource:modelDatasource),
+                    ModelDatasourceKeyMapping.create(property:name, datasource:modelDatasource)]
 smartsObject = constructModel(smartsObject, [creationClassName, name], [modelDatasource], keyMappings);
 
 def redundancyGroup = Model.create(name:"RedundancyGroup");
 modelDatasource = ModelDatasource.create(datasource:rcmdbDatasource, master:true);
 creationClassName = ModelProperty.create(name:"creationClassName", type:ModelProperty.stringType, blank:false, lazy:false, propertyDatasource:modelDatasource);
 name = ModelProperty.create(name:"name", type:ModelProperty.stringType, blank:false, lazy:false, propertyDatasource:modelDatasource);
-keyMappings = [new ModelDatasourceKeyMapping(property:creationClassName, datasource:modelDatasource),
-                    new ModelDatasourceKeyMapping(property:name, datasource:modelDatasource)]
+keyMappings = [ModelDatasourceKeyMapping.create(property:creationClassName, datasource:modelDatasource),
+                    ModelDatasourceKeyMapping.create(property:name, datasource:modelDatasource)]
 redundancyGroup = constructModel(redundancyGroup, [creationClassName, name], [modelDatasource], keyMappings);
 
+
+def device = Model.create(name:"Device", parentModel:smartsObject);
+constructModel(device, [], [], []);
+def link = Model.create(name:"Link", parentModel:smartsObject);
+constructModel(link, [], [], []);
+def deviceComponent = Model.create(name:"DeviceComponent", parentModel:smartsObject);
+constructModel(deviceComponent, [], [], []);
+def deviceAdapter = Model.create(name:"DeviceAdapter", parentModel:deviceComponent);
+constructModel(deviceAdapter, [], [], []);
+def ip = Model.create(name:"IP", parentModel:deviceComponent);
+constructModel(ip, [], [], []);
+def deviceInterface = Model.create(name:"DeviceInterface", parentModel:deviceAdapter);
+constructModel(deviceInterface, [], [], []);
+def port = Model.create(name:"Port", parentModel:deviceAdapter);
+constructModel(port, [], [], []);
+def card = Model.create(name:"Card", parentModel:deviceComponent);
+constructModel(card, [], [], []);
+
 createRelation(smartsObject, redundancyGroup, "memberOf", "consistsOf", ModelRelation.MANY, ModelRelation.ONE);
+createRelation(device, deviceComponent, "composedOf", "partOf", ModelRelation.ONE, ModelRelation.MANY);
+createRelation(device, link, "connectedVia", "connectedSystems", ModelRelation.MANY, ModelRelation.MANY);
+createRelation(link, deviceAdapter, "connectedTo", "connectedVia", ModelRelation.ONE, ModelRelation.MANY);
+createRelation(deviceAdapter, card, "realizedBy", "realises", ModelRelation.MANY, ModelRelation.ONE);
+createRelation(device, ip, "hostsAccessPoints", "hostedBy", ModelRelation.ONE, ModelRelation.MANY);
+createRelation(deviceInterface, ip, "underlying", "layeredOver", ModelRelation.ONE, ModelRelation.ONE);
 
-def device = Model.create(name:"Device")
-
-
+ModelGenerator.getInstance().generateModel(deviceInterface);
 
 
 def constructModel(model, listOfProperties, listOfDatasources, listOfKeyMappings)
@@ -71,7 +97,7 @@ def constructModel(model, listOfProperties, listOfDatasources, listOfKeyMappings
 
     listOfKeyMappings.each
     {
-        it.save();        
+        it.save();
     }
     model.refresh();
     return model;

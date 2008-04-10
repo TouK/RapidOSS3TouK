7eac38c8e5e11d24312852ca3ec8c349c9f70dcb
sezgin <sezgin@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-04-10 15:18:05 +0000

    Rejected commit: Default
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@636 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

---
+++ RapidCMDB/scripts/build/SmartsModuleTest.groovy
@@ -0,0 +1,33 @@
+package build;
+
+class SmartsModuleTest extends Test{
+	
+ 	public static void main(args){
+		SmartsModuleTest smartsTest = new SmartsModuleTest();
+		smartsTest.run(args);
+	}
+	
+	def runAllTests(){
+		setupForTestExecution();
+		runTests("com.ifountain.smarts.test.AllTests", env.smarts_testreport, "Smarts_AllTests", env.smarts_testhtml);		
+	}
+	
+	def setupForTestExecution(){
+		setClasspathForTestExecution();
+		ant.delete(dir : env.smarts_testreport);
+		ant.delete(dir : env.smarts_testhtml);		
+	}
+	
+	def setClasspathForTestExecution(){
+		addProductionLibJarsToClasspath();
+		ant.path(id : "testexecutionclasspath"){
+			ant.path(refid : "libJars");
+			// test execution only jars, not required for production
+			ant.pathelement(location : (String)classpath.getProperty("junit_jar"));
+			ant.pathelement(location : (String)classpath.getProperty("skclient_jar"));
+			ant.pathelement(location : (String)classpath.getProperty("net_jar"));
+			ant.pathelement(location : (String)classpath.getProperty("mysql-connector-java-3_1_8-bin_jar"));
+		}
+	}
+	
+}

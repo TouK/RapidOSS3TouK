40b55b7a1805c4707709ab19167090566be1b0dc
mustafa <mustafa@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-04-17 12:56:51 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@703 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/scripts/build/Parent.groovy
+++
@@ -1,9 +0,0 @@
-package build;
-
-class Parent {
-	AntBuilder ant = new AntBuilder();
-	Env env = new Env(ant);
-	Properties classpath = env.thirdPartyJars;	
-	public static boolean TEST = false;
-
-}

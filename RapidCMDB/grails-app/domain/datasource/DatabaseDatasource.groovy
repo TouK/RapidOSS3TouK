62257f046c59c627ec7d31fd998f7b6393dbff0d
pinar <pinar@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-04-25 14:51:12 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@821 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/grails-app/domain/datasource/DatabaseDatasource.groovy
+++ RapidCMDB/grails-app/domain/datasource/DatabaseDatasource.groovy
@@ -7,8 +7,23 @@ class DatabaseDatasource extends BaseDatasource{
     DatabaseConnection connection;
     def adapter;
     static transients =  ['adapter']
-    
+
     def onLoad = {
        this.adapter = new DatabaseAdapter(connection.name, 0, Logger.getRootLogger());
+    }
+    def runUpdate(sql){
+        return this.adapter.executeUpdate(sql, []);
+    }
+    def runUpdate(sql, queryParams){
+        return this.adapter.executeUpdate(sql, queryParams);
+    }
+    def runQuery(sql){
+        return this.adapter.executeQuery(sql, []);
+    }
+    def runQuery(sql,  queryParams){
+        return this.adapter.executeQuery(sql, queryParams);
+    }
+    def runQuery(sql,  queryParams, fetchSize){
+        return this.adapter.executeQuery(sql, queryParams, fetchSize);
     }
 }

d5711b7bbf09ad6061d63e81967f818db481d869
pinar <pinar@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-04-28 07:47:11 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@871 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/grails-app/domain/datasource/HttpDatasource.groovy
+++ RapidCMDB/grails-app/domain/datasource/HttpDatasource.groovy
@@ -10,4 +10,20 @@ class HttpDatasource extends BaseDatasource{
     def onLoad = {
        this.adapter = new HttpAdapter(connection.name, 0, Logger.getRootLogger());
     }
+
+    def doRequest(String url, Map params, int type){
+        return adapter.doRequest(url, params, type);
+    }
+
+    def doRequest(String url, Map params){
+        return adapter.doRequest(url, params);
+    }
+
+    def doGetRequest(String url, Map params){
+        return adapter.doGetRequest(url, params);
+    }
+
+    def doPostRequest(String url, Map params){
+        return adapter.doPostRequest(url, params);
+    }
 }

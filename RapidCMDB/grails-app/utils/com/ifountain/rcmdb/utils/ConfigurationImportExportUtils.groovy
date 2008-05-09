5de43acb3f91be1421209eebfe105d9c5fdfc261
mustafa <mustafa@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-05-09 00:18:19 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@1003 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/grails-app/utils/com/ifountain/rcmdb/utils/ConfigurationImportExportUtils.groovy
+++ RapidCMDB/grails-app/utils/com/ifountain/rcmdb/utils/ConfigurationImportExportUtils.groovy
@@ -3,7 +3,10 @@ package com.ifountain.rcmdb.utils
 import connection.Connection
 import groovy.text.SimpleTemplateEngine
 import groovy.text.Template
-import org.apache.log4j.Logger;
+import org.apache.log4j.Logger
+import datasource.BaseDatasource
+import script.CmdbScript
+import model.Model;
 /**
  * Created by IntelliJ IDEA.
  * User: Administrator
@@ -34,36 +37,64 @@ class ConfigurationImportExportUtils {
             File templateFile = new File("${basedir}/${templatePath}")
             if(templateFile.exists())
             {
-                foundTemplate = templateEngine.createTemplate(templateFile)
+                def inputStr = templateFile.newInputStream();
+                foundTemplate = templateEngine.createTemplate(new InputStreamReader(inputStr))
                 templates[templatePath] = foundTemplate;
+                inputStr.close();
             }
             else
             {
-                logger.info ("Could not find template ${templateFile.path}");    
+                logger.info ("Could not find template ${templateFile.path}");
             }
+            
         }
         return foundTemplate;
 
     }
     public void export(List configurationObjects)
     {
+        new File(exportDir).mkdirs();
+        new File(exportDir+"/models").mkdirs();
         def connectionXmls = [];
+        def datasourceXmls = [];
+        def scriptXmls = [];
+        Template connectionsTemplate = getTemplate("connection/Connections.xml")
+        Template datasourcesTemplate = getTemplate("datasource/Datasources.xml")
+        Template scriptsTemplate = getTemplate("script/Scripts.xml")
+        Template modelTemplate = getTemplate("model/Model.xml")
         configurationObjects.each {configurationObject->
             def templatePath = "${configurationObject.class.name.replaceAll ("\\.", "/")}.xml"
             Template template = getTemplate(templatePath);
             if(template)
             {
-                connectionXmls += template.make ([object:configurationObject]).toString();
+                if(configurationObject instanceof Connection)
+                {
+                    connectionXmls += template.make ([object:configurationObject]).toString();
+                }
+                else if(configurationObject instanceof BaseDatasource)
+                {
+                    datasourceXmls += template.make ([object:configurationObject]).toString();
+                }
+                else if(configurationObject instanceof CmdbScript)
+                {
+                    scriptXmls += template.make ([object:configurationObject]).toString();
+                }
+                else if(configurationObject instanceof Model)
+                {
+                    File modelFile = new File("${exportDir}/models/${configurationObject.name}.xml");
+                    modelTemplate.make([model:configurationObject]).writeTo(modelFile.newWriter());
+                }
             }
         }
 
         File connectionsFile = new File("${exportDir}/connections.xml");
-        if(connectionsFile.parentFile)
-        {
-            connectionsFile.parentFile.mkdirs();
-        }
-        Template connectionsTemplate = getTemplate("connection/Connections.xml")
-        connectionsTemplate.make([connectionXmls:connectionXmls]).writeTo(connectionsFile.newWriter());
+        File datasourcesFile = new File("${exportDir}/datasources.xml");
+        File scriptsFile = new File("${exportDir}/scripts.xml");
+
+
+        connectionsFile.setText(connectionsTemplate.make([connectionXmls:connectionXmls]).toString());
+        datasourcesFile.setText(datasourcesTemplate.make([datasourceXmls:datasourceXmls]).toString());
+        scriptsFile.setText(scriptsTemplate.make([scriptXmls:scriptXmls]).toString());
     }
 
 }

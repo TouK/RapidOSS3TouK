1c5f058416fae98a71cca15af7fe9153a8f786a0
sezgin <sezgin@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-05-22 08:43:15 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@1115 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/grails-app/conf/BootStrap.groovy
+++ RapidCMDB/grails-app/conf/BootStrap.groovy
@@ -9,11 +9,22 @@ import auth.UserRoleRel
 class BootStrap {
 
     def init = {servletContext ->
-        def adminRole = new Role(name: "Administrator").save()
-        def userRole = new Role(name: "User").save()
-        def adminUser = new User(username: "rsadmin", passwordHash: new Sha1Hash("changeme").toHex()).save()
+    	def adminRole = Role.findByName("Administrator");
+    	if(!adminRole){
+	    	adminRole = new Role(name: "Administrator");
+	    	adminRole.save();	
+	    }
+	    def userRole = Role.findByName("User");
+    	if(!userRole){
+	    	userRole = new Role(name: "User");
+	    	userRole.save();	
+	    }
+	    def adminUser = User.findByUsername("rsadmin");
+	    if(!adminUser){
+			adminUser = new User(username: "rsadmin", passwordHash: new Sha1Hash("changeme").toHex());
+			adminUser.save();    
+		}
         new UserRoleRel(user: adminUser, role: adminRole).save()
-        
         def rcmdbDatasource = RCMDBDatasource.findByName(RapidCMDBConstants.RCMDB);
         if (rcmdbDatasource == null) {
             new RCMDBDatasource(name: RapidCMDBConstants.RCMDB).save();
--- RapidCMDB/grails-app/conf/SecurityFilters.groovy
+++ RapidCMDB/grails-app/conf/SecurityFilters.groovy
@@ -25,15 +25,29 @@ class SecurityFilters {
     def filters = {
         // Ensure that all controllers and actions require an authenticated user,
         // except for the "public" controller
-        auth(controller: "*", action: "*") {
+        authentication(controller: "*", action: "*") {
             before = {
                 // Exclude the "public" controller.
                 if (controllerName == "public") return true
 
                 // This just means that the user must be authenticated. He does
                 // not need any particular role or permission.
-                accessControl { true }
+                accessControl { 
+	                role("Administrator") || role("User")
+	            }
             }
         }
+        def adminControllers = ["user", "userRoleRel", 
+        						"application",
+        						 "model", "modelDatasource", "modelDatasourceKeyMapping", "modelRelation", "modelProperty"];
+        authorization(controller:"(" + adminControllers.join("|")+ ")", action:"*"){
+	        before = {
+		      accessControl { 
+	                role("Administrator")
+	            }  
+		        
+		    }
+	    	    
+	    }
     }
 }

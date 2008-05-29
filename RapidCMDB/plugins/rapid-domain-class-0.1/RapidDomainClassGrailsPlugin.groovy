015843efb9b76662f46e6e121d842f6cf9dccaac
mustafa <mustafa@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-05-29 08:07:58 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@1199 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/plugins/rapid-domain-class-0.1/RapidDomainClassGrailsPlugin.groovy
+++ RapidCMDB/plugins/rapid-domain-class-0.1/RapidDomainClassGrailsPlugin.groovy
@@ -145,7 +145,7 @@ class RapidDomainClassGrailsPlugin {
         def relations = DomainClassUtils.getRelations(dc, domainClassMap);
         def keys = DomainClassUtils.getKeys(dc);
         def addMethod = new AddMethod(mc, dc.validator, relations, keys);
-        def removeMethod = new RemoveMethod(mc);
+        def removeMethod = new RemoveMethod(mc, relations);
         def updateMethod = new UpdateMethod(mc, dc.validator, relations, keys);
         def addRelationMethod = new AddRelationMethod(mc, relations);
         def removeRelationMethod = new RemoveRelationMethod(mc, relations);

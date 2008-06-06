5e1d4d9362b5ec3862fbf09ea1879c7e8b62b01b
sezgin <sezgin@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-06-06 12:27:45 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@1249 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/devDocs/scripts/build/RapidExtBuild.groovy
+++ RapidCMDB/devDocs/scripts/build/RapidExtBuild.groovy
@@ -6,66 +6,73 @@ package build
  * Time: 1:37:14 AM
  * To change this template use File | Settings | File Templates.
  */
-class RapidExtBuild extends Build{
-    public static void main(String[] args){
-		RapidExtBuild rapidExtBuilder = new RapidExtBuild();
-		rapidExtBuilder.run(args);
-	}
-    def String getExcludedClasses(){
-		if (!TEST){
-			return "**/*Test*, **/*Mock*, **/test/**";
-		}
-		return "";
-	}
+class RapidExtBuild extends Build {
+    public static void main(String[] args) {
+        RapidExtBuild rapidExtBuilder = new RapidExtBuild();
+        rapidExtBuilder.run(args);
+    }
+    def String getExcludedClasses() {
+        if (!TEST) {
+            return "**/*Test*, **/*Mock*, **/test/**";
+        }
+        return "";
+    }
 
-	def build(){
+    def build() {
         clean();
         compile();
-		copyResourcesForJar();
-		ant.jar(destfile : env.rapid_ext_jar, basedir : env.rapid_ext_build);
-        ant.copy(file : env.rapid_ext_jar, toDir : env.dist_rapid_cmdb_lib);
-        ant.copy(file : env.rapid_ext_jar, toDir : env.dist_rapid_cmdb_modeler_lib);
+        copyResourcesForJar();
+        ant.jar(destfile: env.rapid_ext_jar, basedir: env.rapid_ext_build);
+        ant.copy(file: env.rapid_ext_jar, toDir: env.dist_rapid_cmdb_lib);
+        ant.copy(file: env.rapid_ext_jar, toDir: env.dist_rapid_cmdb_modeler_lib);
 
-		copyDependentJars();
-	}
+        copyDependentJars();
+    }
 
-	def copyDependentJars(){
-	}
+    def copyDependentJars() {
+    }
 
-    def clean(){
-		ant.delete(dir : env.rapid_ext_build);
-		ant.mkdir(dir : env.rapid_ext_build);
-	}
-	def compile(){
-		ant.javac(srcdir : "$env.rapid_ext/database/java", destdir : env.rapid_ext_build, excludes: getExcludedClasses()){
-			ant.classpath(refid : "classpath");
-		}
-        ant.javac(srcdir : "$env.rapid_ext/snmp/java", destdir : env.rapid_ext_build, excludes: getExcludedClasses()){
-			ant.classpath(refid : "classpath");
-		}
-	}
+    def clean() {
+        ant.delete(dir: env.rapid_ext_build);
+        ant.mkdir(dir: env.rapid_ext_build);
+    }
+    def compile() {
+        ant.javac(srcdir: "$env.rapid_ext/database/java", destdir: env.rapid_ext_build, excludes: getExcludedClasses()) {
+            ant.classpath(refid: "classpath");
+        }
+        ant.javac(srcdir: "$env.rapid_ext/snmp/java", destdir: env.rapid_ext_build, excludes: getExcludedClasses()) {
+            ant.classpath(refid: "classpath");
+        }
+    }
 
 
-	def copyResourcesForJar(){
-        ant.copy(todir : "$env.dist_rapid_cmdb/grails-app/ext"){
-			ant.fileset(dir : "$env.rapid_ext/database/groovy"){
-                if(!TEST){
-                    ant.exclude(name:"**/test/**")
-                    ant.exclude(name:"**/*Test*")    
+    def copyResourcesForJar() {
+        ant.copy(todir: "$env.dist_rapid_cmdb/grails-app/ext") {
+            ant.fileset(dir: "$env.rapid_ext/database/groovy") {
+                if (!TEST) {
+                    ant.exclude(name: "**/test/**")
+                    ant.exclude(name: "**/*Test*")
                 }
             };
-            ant.fileset(dir : "$env.rapid_ext/rapidinsight/groovy"){
-                if(!TEST){
-                    ant.exclude(name:"**/test/**")
-                    ant.exclude(name:"**/*Test*")    
+            ant.fileset(dir: "$env.rapid_ext/rapidinsight/groovy") {
+                if (!TEST) {
+                    ant.exclude(name: "**/test/**")
+                    ant.exclude(name: "**/*Test*")
                 }
             };
-            ant.fileset(dir : "$env.rapid_ext/http/groovy"){
-                if(!TEST){
-                    ant.exclude(name:"**/test/**")
-                    ant.exclude(name:"**/*Test*")
+            ant.fileset(dir: "$env.rapid_ext/http/groovy") {
+                if (!TEST) {
+                    ant.exclude(name: "**/test/**")
+                    ant.exclude(name: "**/*Test*")
                 }
             };
-		}
+        }
+        if (!TEST) {
+            ant.copy(todir: "$env.dist_rapid_cmdb/grails-app/ext") {
+                ant.fileset(dir: "$env.rapid_ext/database/groovy")
+                ant.fileset(dir: "$env.rapid_ext/rapidinsight/groovy")
+                ant.fileset(dir: "$env.rapid_ext/http/groovy")
+            }
+        }
     }
 }

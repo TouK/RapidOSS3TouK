cb29d772e7d3e921cdd00c6f396e9de148e61db4
sezgin <sezgin@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-06-19 10:38:14 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@1459 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RcmdbCommons/grails-app/controllers/script/ScriptController.groovy
+++ RcmdbCommons/grails-app/controllers/script/ScriptController.groovy
@@ -10,73 +10,65 @@ import com.ifountain.rcmdb.domain.util.ControllerUtils
 class ScriptController {
     public static final String SUCCESSFULLY_CREATED = "Script created";
     public static final String SCRIPT_DOESNOT_EXIST = "Script does not exist";
-    
-    def index = { redirect(action:list,params:params) }
+
+    def index = {redirect(action: list, params: params)}
 
     // the delete, save and update actions only accept POST requests
-    def allowedMethods = [delete:'POST', save:'POST', update:'POST']
+    def allowedMethods = [delete: 'POST', save: 'POST', update: 'POST']
 
     def list = {
-        if(!params.max) params.max = 10
-        [ cmdbScriptList: CmdbScript.list( params ) ]
+        if (!params.max) params.max = 10
+        [cmdbScriptList: CmdbScript.list(params)]
     }
 
-    
-    def show = {
-        def cmdbScript = CmdbScript.get([id:params.id])
 
-        if(!cmdbScript) {
+    def show = {
+        def cmdbScript = CmdbScript.get([id: params.id])
+
+        if (!cmdbScript) {
             flash.message = "CmdbScript not found with id ${params.id}"
-            redirect(action:list)
+            redirect(action: list)
         }
         else {
-            if(cmdbScript.class != CmdbScript)
+            if (cmdbScript.class != CmdbScript)
             {
                 def controllerName = cmdbScript.class.name;
-                if(controllerName.length() == 1)
+                if (controllerName.length() == 1)
                 {
                     controllerName = controllerName.toLowerCase();
                 }
                 else
                 {
-                    controllerName = controllerName.substring(0,1).toLowerCase()+controllerName.substring(1);
+                    controllerName = controllerName.substring(0, 1).toLowerCase() + controllerName.substring(1);
                 }
-                redirect(action:show, controller:controllerName, id:params.id)
+                redirect(action: show, controller: controllerName, id: params.id)
             }
             else
             {
-                return [ cmdbScript : cmdbScript ]
+                return [cmdbScript: cmdbScript]
             }
         }
     }
-    
+
     def create = {
         def cmdbScript = new CmdbScript()
         cmdbScript.properties = params
-        return ['cmdbScript':cmdbScript]
+        return ['cmdbScript': cmdbScript]
     }
-    
+
     def save = {
-        try{
-            def script = CmdbScript.addScript(ControllerUtils.getClassProperties(params, CmdbScript), true)
-            if(script.hasErrors()){
-                 render(view: 'create', controller: 'script', model: [cmdbScript: script])
-            }
-            else{
-	           
-                flash.message = SUCCESSFULLY_CREATED
-                redirect(action: show, controller: 'script', id: script.id)
-            }
+        def script = CmdbScript.addScript(ControllerUtils.getClassProperties(params, CmdbScript), true)
+        if (script.hasErrors()) {
+            render(view: 'create', controller: 'script', model: [cmdbScript: script])
         }
-        catch(e){
-	        
-            flash.errors = [e.getMessage()]
-            redirect(action: edit, id: params.id)
+        else {
+            flash.message = SUCCESSFULLY_CREATED
+            redirect(action: show, controller: 'script', id: script.id)
         }
     }
 
     def delete = {
-        def script = CmdbScript.get([id:params.id])
+        def script = CmdbScript.get([id: params.id])
         if (script) {
             CmdbScript.deleteScript(script);
             flash.message = "Script ${params.id} deleted"
@@ -87,35 +79,29 @@ class ScriptController {
             redirect(action: list)
         }
     }
-    
-    def edit = {
-        def cmdbScript = CmdbScript.get( [id:params.id] )
 
-        if(!cmdbScript) {
+    def edit = {
+        def cmdbScript = CmdbScript.get([id: params.id])
+
+        if (!cmdbScript) {
             flash.message = "CmdbScript not found with id ${params.id}"
-            redirect(action:list)
+            redirect(action: list)
         }
         else {
-            return [ cmdbScript : cmdbScript ]
+            return [cmdbScript: cmdbScript]
         }
     }
 
     def update = {
-        def script = CmdbScript.get( [id:params.id] )
+        def script = CmdbScript.get([id: params.id])
         if (script) {
-            try{
-                script = CmdbScript.updateScript(script, ControllerUtils.getClassProperties(params, CmdbScript), true);
-                if(script.hasErrors()){
-                    render(view: 'edit', model: [cmdbScript: script])
-                }
-                else{
-                     flash.message = "Script ${params.id} updated"
-                     redirect(action: show, id: script.id)
-                }
+            script = CmdbScript.updateScript(script, ControllerUtils.getClassProperties(params, CmdbScript), true);
+            if (script.hasErrors()) {
+                render(view: 'edit', model: [cmdbScript: script])
             }
-            catch(e){
-                 flash.errors = [e.getMessage()]
-                 redirect(action: edit, id: params.id)
+            else {
+                flash.message = "Script ${params.id} updated"
+                redirect(action: show, id: script.id)
             }
         }
         else {
@@ -170,7 +156,7 @@ class ScriptController {
             redirect(action: list, controller: 'script');
         }
     }
-   
+
     def test =
     {
         def testDir = "test/reports"

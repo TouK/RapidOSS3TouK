4d5e90c052ab9fc2fafba5edc8b34896174f3ea1
mustafa <mustafa@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-07-08 11:22:15 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@1614 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

---
+++ Netcool/applications/RapidSearchForNetcool/scripts/CrateDefaults.groovy
@@ -0,0 +1,12 @@
+import script.CmdbScript
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: Jul 7, 2008
+ * Time: 5:29:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+CmdbScript.add(name:"NetcoolColumnMapping");
+CmdbScript.add(name:"NetcoolConfigurationLoader");

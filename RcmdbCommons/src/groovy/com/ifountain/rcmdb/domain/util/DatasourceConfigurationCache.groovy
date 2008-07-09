dbef99bdcdd5740840ffb10e4f054f9be952f1a6
mustafa <mustafa@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-07-09 09:04:54 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@1624 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RcmdbCommons/src/groovy/com/ifountain/rcmdb/domain/util/DatasourceConfigurationCache.groovy
+++ RcmdbCommons/src/groovy/com/ifountain/rcmdb/domain/util/DatasourceConfigurationCache.groovy
@@ -1,6 +1,7 @@
 package com.ifountain.rcmdb.domain.util
 
 import org.codehaus.groovy.grails.commons.GrailsClassUtils
+import datasource.BaseDatasource
 
 /* All content copyright (C) 2004-2008 iFountain, LLC., except as may otherwise be
 * noted in a separate copyright notice. All rights reserved.

7ad6abbbec8c0a5e2d34644ff0c12a67da2a5df5
nurullah <nurullah@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-07-28 13:24:04 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@1912 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/integration/hyperic/plugin/rapidcmdb/app/AlertController.groovy
+++ RapidCMDB/integration/hyperic/plugin/rapidcmdb/app/AlertController.groovy
@@ -5,17 +5,14 @@ import org.hyperic.hq.events.server.session.AlertManagerEJBImpl as AlertMan
 
 import java.text.SimpleDateFormat
 import org.hyperic.hibernate.PageInfo
-import org.hyperic.hq.appdef.shared.AppdefEntityID
-import org.hyperic.hq.appdef.shared.AppdefEntityValue
+import org.hyperic.hq.appdef.shared.*
 import org.hyperic.hq.authz.server.session.AuthzSubjectManagerEJBImpl
-import org.hyperic.hq.authz.server.session.ResourceSortField
 import org.hyperic.hq.events.server.session.AlertDefinition
 import org.hyperic.hq.events.server.session.AlertSortField
 import org.hyperic.hq.hqu.rendit.BaseController
 import org.hyperic.hq.hqu.rendit.helpers.AlertHelper
 import org.hyperic.hq.hqu.rendit.helpers.ResourceHelper
 import org.hyperic.hq.hqu.rendit.util.HQUtil
-import org.hyperic.hq.events.AlertSeverity
 import org.hyperic.util.pager.PageControl
 import org.hyperic.util.pager.PageList
 
@@ -31,34 +28,46 @@ class AlertController
         def pageInfo = new PageInfo(AlertSortField.DATE, true)
         def overlord     = HQUtil.overlord
         def rhelp        = new ResourceHelper(overlord)
-        def plats        = rhelp.findAllPlatforms()
+        //def plats        = rhelp.findAllPlatforms()
         def pMan = PlatMan.one
         def ahelp = new AlertHelper(overlord)
         def aMan = AlertMan.one
+        def plats = pMan.getAllPlatforms(overlord.authzSubjectValue, PageControl.PAGE_ALL)
+
+        def begin = params.getOne('begin')
 
         xml.'RapidCMDB'('source':'Hyperic HQ', 'date':formatter.format(new Date())) {
             def alerts = aMan.findAllAlerts()
-            xml.'allAlerts'('numberOfAlerts': alerts.getTotalSize()) {
-                for (myAlert2 in alerts) {
-                    def myAlert = aMan.findAlertById(myAlert2.id)
-                    AlertDefinition alertDef = myAlert.getAlertDefinition()
-                    AppdefEntityID aeid = new AppdefEntityID(alertDef.getAppdefType(), alertDef.getAppdefId())
-                    AppdefEntityValue aev = new AppdefEntityValue(aeid, AuthzSubjectManagerEJBImpl.getOne().getOverlordPojo())
 
-                    xml.'alert'('id': myAlert.id,
-                                'alert_name': myAlert.alertDefinition.alertDefinitionValue.name,
-                                'owner_name': aev.getName(),
-/*                                'aeid': aeid,
-                                'aeid-type': aeid.authzTypeId,*/
-                                'creationTime': formatter.format(new Date(myAlert.alertValue.ctime)),
-                                'fixed': myAlert.fixed,
-//                                'reason': aMan.getShortReason(myAlert),
-                                'long_reason': aMan.getLongReason(myAlert)
-                    )
+            if (alerts != null) {
+                xml.'Alerts'() {
+                    for (myAlert2 in alerts) {
+                        def myAlert = aMan.findAlertById(myAlert2.id)
+                        if (begin != null) {
+                            if (myAlert.alertValue.ctime.toString() < begin.toString()) {
+                                continue;
+                            }
+                        }
+                        AlertDefinition alertDef = myAlert.getAlertDefinition()
+                        AppdefEntityID aeid = new AppdefEntityID(alertDef.getAppdefType(), alertDef.getAppdefId())
+                        AppdefEntityValue aev = new AppdefEntityValue(aeid, AuthzSubjectManagerEJBImpl.getOne().getOverlordPojo())
+
+                        xml.'alert'('id': myAlert.id,
+                                    'alert_name': myAlert.alertDefinition.alertDefinitionValue.name,
+                                    'owner_name': aev.getName(),
+    /*                                'aeid': aeid,
+                                    'aeid-type': aeid.authzTypeId,*/
+                                    'creationTime': formatter.format(new Date(myAlert.alertValue.ctime)),
+                                    'timestamp': myAlert.alertValue.ctime,
+                                    'fixed': myAlert.fixed,
+    //                                'reason': aMan.getShortReason(myAlert),
+                                    'long_reason': aMan.getLongReason(myAlert)
+                        )
+                    }
                 }
             }
         }
-       xml
+        xml
     }
     
 
@@ -76,18 +85,36 @@ class AlertController
 
         def owner = params.getOne('owner_name')
         def type = params.getOne('type')
+        def begin = params.getOne('begin')
+
         def source
         def id
         def alerts
         switch (type) {
             case "platform":
-                source = pMan.getPlatformByName(owner)
-                alerts = aMan.findAlerts(overlord.authzSubjectValue, AppdefEntityID.newPlatformID(source.id), PageControl.PAGE_ALL)
+                try {
+                    if (owner != null) {
+                        source = pMan.getPlatformByName(owner)
+                    }
+                }
+                catch (PlatformNotFoundException e) {
+                    source = null
+                }
+                if (source != null) {
+                    alerts = aMan.findAlerts(overlord.authzSubjectValue, AppdefEntityID.newPlatformID(source.id), PageControl.PAGE_ALL)
+                }
                 break;
             case "server":
                 source = new PageList()
-
-                def sers = svrMan.findServersByName(overlord.authzSubjectValue, owner)
+                def sers
+                try {
+                    if (owner != null) {
+                        sers = svrMan.findServersByName(overlord.authzSubjectValue, owner)
+                    }
+                }
+                catch (ServerNotFoundException e) {
+                    sers = null
+                }
                 for(svr2 in sers) {
                     //def svr = rhelp.find('server': svr2.id)
                     source.add(svr2)
@@ -99,7 +126,15 @@ class AlertController
                 break;
             case "service":
                 source = new PageList()
-                def servs = svcMan.findServicesByName(overlord.authzSubjectValue, owner)
+                def servs
+                try {
+                    if (owner != null) {
+                        servs = svcMan.findServicesByName(overlord.authzSubjectValue, owner)
+                    }
+                }
+                catch (ServiceNotFoundException e) {
+                    servs = null
+                }
                 for(svc2 in servs) {
                     //def svc = rhelp.find('service': svc2.id)
                     def svc = svcMan.findServiceById(svc2.id)
@@ -114,21 +149,32 @@ class AlertController
 
         xml.'RapidCMDB'('source':'Hyperic HQ', 'date':formatter.format(new Date())) {
             //def alerts = aMan.findAllAlerts()
-            xml.'alerts'('numberOfAlerts': alerts.getTotalSize()) {
-                for (myAlert2 in alerts) {
-                    def myAlert = aMan.findAlertById(myAlert2.id)
-                    AlertDefinition alertDef = myAlert.getAlertDefinition()
-                    AppdefEntityID aeid = new AppdefEntityID(alertDef.getAppdefType(), alertDef.getAppdefId())
-                    AppdefEntityValue aev = new AppdefEntityValue(aeid, AuthzSubjectManagerEJBImpl.getOne().getOverlordPojo())
+            if (alerts != null) {
+                xml.'Alerts'(/*'numberOfAlerts': alerts.getTotalSize()*/) {
+                    for (myAlert2 in alerts) {
+                        def myAlert = aMan.findAlertById(myAlert2.id)
+                        if (begin != null) {
+                            if (myAlert.alertValue.ctime.toString() < begin.toString()) {
+                                continue;
+                            }
+                        }
+                        AlertDefinition alertDef = myAlert.getAlertDefinition()
+                        AppdefEntityID aeid = new AppdefEntityID(alertDef.getAppdefType(), alertDef.getAppdefId())
+                        AppdefEntityValue aev = new AppdefEntityValue(aeid, AuthzSubjectManagerEJBImpl.getOne().getOverlordPojo())
 
-                    xml.'alert'('id': myAlert.id,
-                                'alert_name': myAlert.alertDefinition.alertDefinitionValue.name,
-                                'owner_name': aev.getName(),
-                                'creationTime': formatter.format(new Date(myAlert.alertValue.ctime)),
-                                'fixed': myAlert.fixed,
-                                'long_reason': aMan.getLongReason(myAlert)
-                    )
+                        xml.'alert'('id': myAlert.id,
+                                    'alert_name': myAlert.alertDefinition.alertDefinitionValue.name,
+                                    'owner_name': aev.getName(),
+                                    'creationTime': formatter.format(new Date(myAlert.alertValue.ctime)),
+                                    'timestamp': myAlert.alertValue.ctime,
+                                    'fixed': myAlert.fixed,
+                                    'long_reason': aMan.getLongReason(myAlert)
+                        )
+                    }
                 }
+            }
+            else {
+                xml.'alerts'('numberOfAlerts': 0)
             }
         }
        xml

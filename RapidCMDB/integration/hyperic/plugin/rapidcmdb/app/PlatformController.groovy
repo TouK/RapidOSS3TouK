f6c3f94491825bd245e1543fbd6c855b37544c90
nurullah <nurullah@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-07-28 13:38:14 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@1913 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/integration/hyperic/plugin/rapidcmdb/app/PlatformController.groovy
+++ RapidCMDB/integration/hyperic/plugin/rapidcmdb/app/PlatformController.groovy
@@ -2,13 +2,11 @@ import org.hyperic.hq.appdef.server.session.PlatformManagerEJBImpl as PlatMan
 
 import java.text.SimpleDateFormat
 import org.hyperic.hibernate.PageInfo
+import org.hyperic.hq.appdef.shared.PlatformNotFoundException
 import org.hyperic.hq.authz.server.session.ResourceSortField
-import org.hyperic.hq.appdef.server.session.Platform
 import org.hyperic.hq.hqu.rendit.BaseController
 import org.hyperic.hq.hqu.rendit.helpers.ResourceHelper
 import org.hyperic.hq.hqu.rendit.util.HQUtil
-
-import org.hyperic.hq.authz.server.session.ResourceManagerEJBImpl as ResMan
 
 class PlatformController
 	extends BaseController
@@ -20,30 +18,39 @@ class PlatformController
     def list(xml, params) {
         def formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
         def pageInfo = new PageInfo(ResourceSortField.NAME, true)
-        def platforms = resourceHelper.findPlatforms(pageInfo);
         def overlord     = HQUtil.overlord
         def rhelp        = new ResourceHelper(overlord)
-        def plats        = rhelp.findAllPlatforms()
+        def platforms
+        def plats
+        try {
+            plats        = rhelp.findAllPlatforms()
+            platforms = resourceHelper.findPlatforms(pageInfo)
+        }
+        catch (PlatformNotFoundException e) {
+            platforms = null
+        }
         def pMan = PlatMan.one
         def i = 0
 
         xml.'RapidCMDB'('source':'Hyperic HQ', 'date':formatter.format(new Date())) {
-            platforms.each { res ->
-            	def plat = plats.getAt(i)
-                def p = pMan.findPlatformById(res.instanceId)
-                xml.platform('id':p.id, 'platform-ip':p.fqdn) {
-                	for (metric in plat.enabledMetrics) {
-                        def metricData = metric.lastDataPoint
-                        xml.'metric'('name': metric.template.name,
-                        		 	 'value': metricData.value,
-                        		 	 'units': metric.template.units,
-                        		 	 'time': formatter.format(new Date(metricData.timestamp)))
-                	}
-	            i++
+            xml.'Platforms'() {
+                platforms.each { res ->
+                    def plat = plats.getAt(i)
+                    def p = pMan.findPlatformById(res.instanceId)
+                    xml.platform('id':p.id, 'name': p.name, 'ip':p.fqdn) {
+                        for (metric in plat.enabledMetrics) {
+                            def metricData = metric.lastDataPoint
+                            xml.'metric'('name': metric.template.name,
+                                         'value': metricData.value,
+                                         'units': metric.template.units,
+                                         'time': formatter.format(new Date(metricData.timestamp)))
+                        }
+                    i++
+                    }
                 }
             }
         }
-       xml
+        xml
     }
 
     def get(xml, params) {
@@ -53,18 +60,33 @@ class PlatformController
         def rhelp = new ResourceHelper(overlord)
         def pMan = PlatMan.one
         def platName = params.getOne('name')
-        def platform = pMan.getPlatformByName(platName)
+        def platform
+        def plat
+        try {
+            if (platName != null) {
+                platform = pMan.getPlatformByName(platName)
+            }
+        }
+        catch (PlatformNotFoundException e) {
+            platform = null
+        }
+        if (platform != null) {
+            plat = rhelp.find('platform': platform.id)
+        }
 
-        def plat = rhelp.find('platform': platform.id)
         xml.'RapidCMDB'('source':'Hyperic HQ', 'date':formatter.format(new Date())) {
-            def p = pMan.findPlatformById(plat.instanceId)
-            xml.platform('id':p.id, 'platform-ip':p.fqdn) {
-                for (metric in plat.enabledMetrics) {
-                    def metricData = metric.lastDataPoint
-                    xml.'metric'('name': metric.template.name,
-                                 'value': metricData.value,
-                                 'units': metric.template.units,
-                                 'time': formatter.format(new Date(metricData.timestamp)))
+            xml.'Platforms'() {
+                if (platform != null) {
+                    def p = pMan.findPlatformById(plat.instanceId)
+                    xml.platform('id':p.id, 'name': p.name, 'ip':p.fqdn) {
+                        for (metric in plat.enabledMetrics) {
+                            def metricData = metric.lastDataPoint
+                            xml.'metric'('name': metric.template.name,
+                                         'value': metricData.value,
+                                         'units': metric.template.units,
+                                         'time': formatter.format(new Date(metricData.timestamp)))
+                        }
+                    }
                 }
             }
         }

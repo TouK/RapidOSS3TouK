d04b04440b3b02f25c6628d5608c0106c018af67
sezgin <sezgin@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-07-30 15:04:44 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@1978 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RcmdbCommons/plugins/jsecurity-0.2.1/grails-app/controllers/auth/RsUserController.groovy
+++ RcmdbCommons/plugins/jsecurity-0.2.1/grails-app/controllers/auth/RsUserController.groovy
@@ -7,7 +7,7 @@ class RsUserController {
     def index = { redirect(action:list,params:params) }
 
     // the delete, save and update actions only accept POST requests
-    def allowedMethods = [delete:'POST', save:'POST', update:'POST']
+    def allowedMethods = []
 
     def list = {
         if(!params.max) params.max = 10

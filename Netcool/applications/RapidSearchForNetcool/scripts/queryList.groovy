bb433c5e938454b33a1b949e685ca43b0f9804b6
kamil <kamil@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-08-01 07:04:26 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@2001 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- Netcool/applications/RapidSearchForNetcool/scripts/queryList.groovy
+++ Netcool/applications/RapidSearchForNetcool/scripts/queryList.groovy
@@ -4,26 +4,31 @@ import search.SearchQueryGroup
 import auth.*;
 
 def user = RsUser.findByUsername(web.session.username);
-def defaultGroup = SearchQueryGroup.get(name: "Default", user: user);
-if (defaultGroup == null) {
-    SearchQueryGroup.add(name: "Default", user: user);
-}
-
 def queryGroups = SearchQueryGroup.list();
 def writer = new StringWriter();
 def queryBuilder = new MarkupBuilder(writer);
+
+def tenMinBeforeNow = (System.currentTimeMillis() / 1000) - (1000*60*10) ;
+
+SearchQueryGroup.add(name: "Default", user: user);
+SearchQuery.add(group:defaultGroup, name: "All Events", query: "id:*", user: user);
+SearchQuery.add(group:defaultGroup, name: "Critical Events", query: "severity: Critical", user: user);
+SearchQuery.add(group:defaultGroup, name: "Last 10 Minutes", query: "statechange: [" +tenMinBeforeNow+ "TO NOW]", user: user);
+SearchQuery.add(group:defaultGroup, name: "In Maintenance", query: "manager: * - manager:*Watch suppressescl:Maintenance", user: user);
+SearchQuery.add(group:defaultGroup, name: "Escalated", query: "manager: * - manager:*Watch (suppressescl:Normal OR suppressescl:Escalated OR suppressescl:Escalated-Level 2 OR suppressescl:Escalated-Level 3 OR suppressescl:Suppressed) ", user: user);
+
 queryBuilder.Filters
 {
     queryGroups.each {SearchQueryGroup group ->
         if (group.user.username.equals( user.username)) {
             queryBuilder.Filter(id: group.id, name: group.name, nodeType: "group")
                     {
-                        group.queries.each {SearchQuery query ->
-                            queryBuilder.Filter(id: query.id, name: query.name, nodeType: "filter", query: query.query)
-                        }
+	                        group.queries.each {SearchQuery query ->
+	                            queryBuilder.Filter(id: query.id, name: query.name, nodeType: "filter", query: query.query)
+
+                    		}
                     }
         }
-
     }
 }
 

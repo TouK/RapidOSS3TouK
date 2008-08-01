7a12cc39c4f07e17a9ab52a427fb55ce9b0baf5c
kamil <kamil@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-08-01 09:23:48 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@2008 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- Netcool/applications/RapidSearchForNetcool/scripts/queryList.groovy
+++ Netcool/applications/RapidSearchForNetcool/scripts/queryList.groovy
@@ -4,14 +4,13 @@ import search.SearchQueryGroup
 import auth.*;
 
 def user = RsUser.findByUsername(web.session.username);
-def queryGroups = SearchQueryGroup.list();
+
 def writer = new StringWriter();
 def queryBuilder = new MarkupBuilder(writer);
 
 def tenMinBeforeNow = (System.currentTimeMillis() / 1000) - (1000*60*10) ;
 
-SearchQueryGroup.add(name: "Default", user: user);
-def defaultGroup = SearchQueryGroup.get(name: "Default", user: user);
+def defaultGroup = SearchQueryGroup.add(name: "Default", user: user);
 
 SearchQuery.add(group:defaultGroup, name: "All Events", query: "id:*", user: user);
 SearchQuery.add(group:defaultGroup, name: "Critical Events", query: "severity: Critical", user: user);
@@ -19,6 +18,7 @@ SearchQuery.add(group:defaultGroup, name: "Last 10 Minutes", query: "statechange
 SearchQuery.add(group:defaultGroup, name: "In Maintenance", query: "manager: * - manager:*Watch suppressescl:Maintenance", user: user);
 SearchQuery.add(group:defaultGroup, name: "Escalated", query: "manager: * - manager:*Watch (suppressescl:Normal OR suppressescl:Escalated OR suppressescl:Escalated-Level 2 OR suppressescl:Escalated-Level 3 OR suppressescl:Suppressed) ", user: user);
 
+def queryGroups = SearchQueryGroup.list();
 queryBuilder.Filters
 {
     queryGroups.each {SearchQueryGroup group ->
@@ -27,11 +27,9 @@ queryBuilder.Filters
                     {
 	                        group.queries.each {SearchQuery query ->
 	                            queryBuilder.Filter(id: query.id, name: query.name, nodeType: "filter", query: query.query)
-
                     		}
                     }
         }
     }
 }
-
-return writer.toString();
+return writer.toString();;

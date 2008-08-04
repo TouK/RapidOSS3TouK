2950edb295806cad8bc2c837163b16cbb8789297
mustafa <mustafa@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-08-04 15:33:56 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@2042 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

---
+++ Netcool/applications/RapidSearchForNetcool/scripts/removeAll.groovy
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: Aug 4, 2008
+ * Time: 6:34:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+NetcoolEvent.unindex("id:[0 TO *]");
+NetcoolJournal.unindex("id:[0 TO *]");

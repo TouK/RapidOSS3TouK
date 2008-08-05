b3a5926ab85359806d9626897ecc7b92e78f5e1d
kamil <kamil@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-08-05 15:03:08 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@2080 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidUiPlugin/web-app/js/rapidjs/component/tools/SettingsTool.js
+++ RapidUiPlugin/web-app/js/rapidjs/component/tools/SettingsTool.js
@@ -9,7 +9,7 @@ YAHOO.rapidjs.component.tool.SettingsTool = function(container, component) {
 	{
 		component.events["loadstatechanged"].subscribe(this.refreshState, this, true);
 	}
-	this.dialog = new YAHOO.rapidjs.component.Dialog({width:300,height:100,
+	this.dialog = new YAHOO.rapidjs.component.Dialog({width:300,height:200, minWidth: 300, minHeight: 200,
 													  buttons:[{text:"Apply", handler:this.handleApplyClick, scope:this },
 															   {text:"Cancel", handler:this.handleCancelClick, scope:this }]});
 	var dh = YAHOO.ext.DomHelper;
@@ -18,12 +18,19 @@ YAHOO.rapidjs.component.tool.SettingsTool = function(container, component) {
 
     html:
 
-    '<table width="100%">' +
-    '<tr><td width="50%"><label>Polling Interval:</label></td><td width="50%"><input type="textbox" name="PollingInterval" style="width:100px"/></td></tr>'+
-    '</table>'
+    '<table width="100%"><tbody>' +
+    '<tr><td width="50%"><label>Set polling interval:</label></td>' +
+    '<td width="50%"><input type="textbox" name="PollingInterval" style="width:100px"/></td></tr>'+
+    '<tr><td width="0%"><select><option value="1">1</option><option value="2">2</option>' +
+	'<option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option>' +
+	'<option value="7">7</option><option value="8">8</option></select></td>' +
+    '</tr>'+
+    '</tbody></table>'
 
     },true);
     this.dialog.container.getElementsByTagName('input')[0].value = this.component.getPollingInterval();
+
+
     YAHOO.util.Event.addListener(this.dialog.container.getElementsByTagName('input')[0], 'keypress', this.applyButtonPress, this, true);
 
 

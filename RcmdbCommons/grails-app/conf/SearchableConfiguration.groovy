dce1dffdc4b21e7fb745d83da0422920e2753c53
mustafa <mustafa@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-08-05 13:37:57 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@2074 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RcmdbCommons/grails-app/conf/SearchableConfiguration.groovy
+++ RcmdbCommons/grails-app/conf/SearchableConfiguration.groovy
@@ -58,7 +58,7 @@ class SearchableConfiguration {
     "compass.engine.analyzer.default.type": WhiteSpaceLowerCaseAnalyzer.class.name,
     "compass.transaction.disableThreadBoundLocalTransaction":"true",
     "compass.cache.first":"org.compass.core.cache.first.NullFirstLevelCache",
-    "compass.transaction.lockTimeout":600];
+    "compass.transaction.lockTimeout":45];
 
     /**
      * Default mapping property exclusions

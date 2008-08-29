75139a8ac71bb9a49a4865bc6962fec6354fa7f0
mustafa <mustafa@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-08-29 15:12:43 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@2436 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/devDocs/scripts/build/hudsonScripts/hudsonUtilities.sh
+++ RapidCMDB/devDocs/scripts/build/hudsonScripts/hudsonUtilities.sh
@@ -155,6 +155,7 @@ compileBuildFiles() {
     groovyc -d $GROOVY_HOME/build RapidModules/RapidCMDB/devDocs/scripts/build/RapidCmdbBuild.groovy
     groovyc -d $GROOVY_HOME/build RapidModules/RapidCMDB/devDocs/scripts/build/RapidInsightForNetcoolBuild.groovy
     groovyc -d $GROOVY_HOME/build RapidModules/RapidCMDB/devDocs/scripts/build/RapidInsightForSmartsBuild.groovy
+    groovyc -d $GROOVY_HOME/build RapidModules/RapidCMDB/devDocs/scripts/build/HypericBuild.groovy
     groovyc -d $GROOVY_HOME/build RapidModules/RapidCMDB/devDocs/scripts/build/SmartsModuleTest.groovy
     groovyc -d $GROOVY_HOME/build RapidModules/RapidCMDB/devDocs/scripts/build/CoreModuleTest.groovy
     groovyc -d $GROOVY_HOME/build RapidModules/RapidCMDB/devDocs/scripts/build/CompModuleTest.groovy

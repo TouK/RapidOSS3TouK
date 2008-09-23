8f28340d6742ebd642425034d20c49bf0df40f6f
(no author) <(no author)@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-09-23 15:45:17 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@2753 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/test/integration/ModelRelationTests.groovy
+++ RapidCMDB/test/integration/ModelRelationTests.groovy
@@ -205,6 +205,7 @@ class ModelRelationTests extends RapidCmdbIntegrationTestCase {
 
         link.addRelation(connectedTo: devAdapter1);
         assertEquals(1, link.connectedTo.size());
+        println "devAdapter1:"+devAdapter1.id + " " + link.connectedTo.id
         assertTrue(link.connectedTo.toString(), link.connectedTo.contains(devAdapter1))
         def linkInCompass = Link.get(name: "myLink", creationClassName: "Link")
         assertEquals(1, linkInCompass.connectedTo.size());

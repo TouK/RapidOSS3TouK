37b516bbc44837a9d76060bb3f6cefd321cdb3a1
(no author) <(no author)@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-09-29 15:00:19 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@2861 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- Smarts/applications/RapidInsightForSmarts/scripts/getViewFields.groovy
+++ Smarts/applications/RapidInsightForSmarts/scripts/getViewFields.groovy
@@ -1,13 +1,25 @@
 import groovy.xml.MarkupBuilder
 import com.ifountain.rcmdb.domain.util.DomainClassUtils
+import org.codehaus.groovy.grails.commons.GrailsDomainClass
 
-def notificationProps = DomainClassUtils.getFilteredProperties(RsSmartsNotification.class.getName(), ["id", "rsDatasource"]);
-def props = notificationProps.sort{it.name};
+def extraFilteredProps = ["rsDatasource", "id"];
+def allProps = [];
+GrailsDomainClass domainClass = web.grailsApplication.getDomainClass("RsEvent")
+domainClass.getSubClasses().each{
+    allProps.addAll(DomainClassUtils.getFilteredProperties(it.name, extraFilteredProps));
+}
+allProps.addAll(DomainClassUtils.getFilteredProperties("RsEvent", extraFilteredProps));
+def sortedProps = allProps.sort{it.name}
+def propertyMap = [:]
 def writer = new StringWriter();
 def builder = new MarkupBuilder(writer);
 builder.Fields() {
-    props.each {
-        builder.Field(Name: it.name)
+    sortedProps.each {
+        def propertyName = it.name;
+        if(!propertyMap.containsKey(propertyName)){
+            builder.Field(Name: propertyName)
+            propertyMap.put(propertyName, propertyName);
+        }
     }
 }
 return writer.toString();

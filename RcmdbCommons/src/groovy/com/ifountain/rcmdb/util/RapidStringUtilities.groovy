5de7b6ce03a1656869a0c9a038540eedc70f80ae
(no author) <(no author)@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-09-29 10:54:23 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@2855 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RcmdbCommons/src/groovy/com/ifountain/rcmdb/util/RapidStringUtilities.groovy
+++ RcmdbCommons/src/groovy/com/ifountain/rcmdb/util/RapidStringUtilities.groovy
@@ -11,23 +11,54 @@ import org.codehaus.groovy.runtime.InvokerHelper
 * To change this template use File | Settings | File Templates.
 */
 class RapidStringUtilities {
-     private RapidStringUtilities(){}
-     public static void registerStringUtils()
-     {
-        String.metaClass.methodMissing = {java.lang.String methodName, params->
-                def newParams = new ArrayList(InvokerHelper.asList(params));
-                newParams.add (0, delegate);
-                try
-                {
-                    return StringUtils.metaClass.invokeStaticMethod(StringUtils, methodName, newParams as Object[]);
+    private RapidStringUtilities() {}
+    public static void registerStringUtils()
+    {
+        String.metaClass.methodMissing = {java.lang.String methodName, params ->
+            def newParams = new ArrayList(InvokerHelper.asList(params));
+            newParams.add(0, delegate);
+            try
+            {
+                return StringUtils.metaClass.invokeStaticMethod(StringUtils, methodName, newParams as Object[]);
+            }
+            catch (MissingMethodException e1)
+            {
+                try {
+                    return RapidStringUtilities.metaClass.invokeStaticMethod(RapidStringUtilities, methodName, newParams as Object[]);
                 }
-                catch(MissingMethodException e)
-                {
-                    throw new MissingMethodException (methodName, delegate.class, params);
+                catch (MissingMethodException e2) {
+                    throw new MissingMethodException(methodName, delegate.class, params);
                 }
-        }
-     }
 
-    
+            }
+        }
+    }
+
+    public static String toASCII(String s, char placeholder) {
+        byte[] value = s.getBytes();
+        StringBuffer buf = new StringBuffer(value.length);
+        for (int i = 0; i < value.length; i++) {
+            if ((Character.isISOControl((char) value[i])) ||
+                    ((value[i] & 0xFF) >= 0x80)) {
+                buf.append(placeholder);
+            }
+            else {
+                buf.append((char) value[i]);
+            }
+        }
+        return buf.toString();
+    }
+
+    public static String toASCII(String s) {
+        byte[] value = s.getBytes();
+        StringBuffer buf = new StringBuffer(value.length);
+        for (int i = 0; i < value.length; i++) {
+            if (!(Character.isISOControl((char) value[i])) && !((value[i] & 0xFF) >= 0x80)) {
+                buf.append((char) value[i]);
+            }
+        }
+        return buf.toString();
+    }
+
 }
 

79b7a49c41581a33561c39be3bd4ec4066ffe35f
(no author) <(no author)@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-10-17 05:47:39 +0000

    Rejected commit: Default (2)
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@3001 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RcmdbCommons/test/unit/connection/LdapConnectionTest.groovy
+++ RcmdbCommons/test/unit/connection/LdapConnectionTest.groovy
@@ -4,7 +4,7 @@ import com.ifountain.rcmdb.test.util.RapidCmdbTestCase
 import org.apache.log4j.Logger
 import com.ifountain.rcmdb.test.util.LdapConnectionTestUtils
 import com.ifountain.core.test.util.RapidCoreTestCase
-import connection.LdapConnectionOperations
+//import connection.LdapConnectionOperations
 /**
 * Created by IntelliJ IDEA.
 * User: deneme
@@ -13,104 +13,104 @@ import connection.LdapConnectionOperations
 * To change this template use File | Settings | File Templates.
 */
 class LdapConnectionTest extends RapidCoreTestCase{
-    def testConnection()
-    {
-        Map params = LdapConnectionTestUtils.getConnectionParams();
-        
-        //ms params
-        //def params=["url":"ldap://192.168.1.178","username":"ldapuser@molkay.selfip.net","userPassword":"123"]
-        //apache params
-        //def params=["url":"ldap://localhost:10389/","username":"uid=admin,ou=system","userPassword":"123"]
-
-
-        LdapConnectionOperationsMock oper = new LdapConnectionOperationsMock(params);
-        assertFalse (oper.isConnected());
-        oper.connect();
-        assertTrue (oper.isConnected());
-        oper.disconnect();
-        assertFalse (oper.isConnected());
-    }
-    def testAuthentication()
-    {
-        Map params = LdapConnectionTestUtils.getAuthorizationParams();
-        //Map params = LdapConnectionTestUtils.getConnectionParams();
-        /*
-        //ms params
-        def params=["url":"ldap://192.168.1.178"]
-        def username="cn=ldapuser,cn=users,DC=molkay,DC=selfip,DC=net";
-        def password="123"
-        */
-        /*
-        //apache params
-        def params=["url":"ldap://localhost:10389/"]
-        def username="uid=tempuser,ou=users,ou=system";
-        def password="123"
-        */
-        LdapConnectionOperationsMock oper = new LdapConnectionOperationsMock(url:params.url)
-
-        assertTrue(oper.checkAuthentication(params.username,params.userPassword))
-    }
-    def testAuthenticationFailsWhenParametersInvalid()
-    {
-        Map params = LdapConnectionTestUtils.getAuthorizationParams();
-
-        LdapConnectionOperationsMock oper = new LdapConnectionOperationsMock(url:params.url)
-
-        assertFalse(oper.checkAuthentication(params.username,params.userPassword+"extra"))
-    }
-    def testDisconnectWithoutConnectionThrowsException()
-    {
-        LdapConnectionOperationsMock oper = new LdapConnectionOperationsMock();
-        assertFalse (oper.isConnected());
-        try{
-            oper.disconnect();
-            fail("Should throw exception");
-        }
-        catch(javax.naming.NamingException e)
-        {
-
-        }
-        assertFalse (oper.isConnected());
-    }
-    def testConnectionThrowsExceptionWhenParametersInvalid()
-    {
-        LdapConnectionOperations oper = new LdapConnectionOperationsMock();
-        assertFalse (oper.isConnected());
-        
-        try{
-            oper.connect();
-            fail("Should throw exception");
-        }
-        catch(javax.naming.NamingException e)
-        {
-
-        }
-        assertFalse(oper.isConnected());
-
-    }
-    def testQuery()
-    {
-        Map params = LdapConnectionTestUtils.getConnectionParams();
-        Map queryparams = LdapConnectionTestUtils.getQueryParams();
-
-        LdapConnectionOperationsMock oper = new LdapConnectionOperationsMock(params);
-        assertFalse (oper.isConnected());
-        oper.connect();
-        assertTrue (oper.isConnected());
-
-        def result=oper.query(queryparams.searchBase,queryparams.searchFilter,queryparams.searchSubDirectories)
-        assertTrue(result.hasMore());
-        oper.disconnect();
-        assertFalse (oper.isConnected());
-    }
+//    def testConnection()
+//    {
+//        Map params = LdapConnectionTestUtils.getConnectionParams();
+//
+//        //ms params
+//        //def params=["url":"ldap://192.168.1.178","username":"ldapuser@molkay.selfip.net","userPassword":"123"]
+//        //apache params
+//        //def params=["url":"ldap://localhost:10389/","username":"uid=admin,ou=system","userPassword":"123"]
+//
+//
+//        LdapConnectionOperationsMock oper = new LdapConnectionOperationsMock(params);
+//        assertFalse (oper.isConnected());
+//        oper.connect();
+//        assertTrue (oper.isConnected());
+//        oper.disconnect();
+//        assertFalse (oper.isConnected());
+//    }
+//    def testAuthentication()
+//    {
+//        Map params = LdapConnectionTestUtils.getAuthorizationParams();
+//        //Map params = LdapConnectionTestUtils.getConnectionParams();
+//        /*
+//        //ms params
+//        def params=["url":"ldap://192.168.1.178"]
+//        def username="cn=ldapuser,cn=users,DC=molkay,DC=selfip,DC=net";
+//        def password="123"
+//        */
+//        /*
+//        //apache params
+//        def params=["url":"ldap://localhost:10389/"]
+//        def username="uid=tempuser,ou=users,ou=system";
+//        def password="123"
+//        */
+//        LdapConnectionOperationsMock oper = new LdapConnectionOperationsMock(url:params.url)
+//
+//        assertTrue(oper.checkAuthentication(params.username,params.userPassword))
+//    }
+//    def testAuthenticationFailsWhenParametersInvalid()
+//    {
+//        Map params = LdapConnectionTestUtils.getAuthorizationParams();
+//
+//        LdapConnectionOperationsMock oper = new LdapConnectionOperationsMock(url:params.url)
+//
+//        assertFalse(oper.checkAuthentication(params.username,params.userPassword+"extra"))
+//    }
+//    def testDisconnectWithoutConnectionThrowsException()
+//    {
+//        LdapConnectionOperationsMock oper = new LdapConnectionOperationsMock();
+//        assertFalse (oper.isConnected());
+//        try{
+//            oper.disconnect();
+//            fail("Should throw exception");
+//        }
+//        catch(javax.naming.NamingException e)
+//        {
+//
+//        }
+//        assertFalse (oper.isConnected());
+//    }
+//    def testConnectionThrowsExceptionWhenParametersInvalid()
+//    {
+//        LdapConnectionOperations oper = new LdapConnectionOperationsMock();
+//        assertFalse (oper.isConnected());
+//
+//        try{
+//            oper.connect();
+//            fail("Should throw exception");
+//        }
+//        catch(javax.naming.NamingException e)
+//        {
+//
+//        }
+//        assertFalse(oper.isConnected());
+//
+//    }
+//    def testQuery()
+//    {
+//        Map params = LdapConnectionTestUtils.getConnectionParams();
+//        Map queryparams = LdapConnectionTestUtils.getQueryParams();
+//
+//        LdapConnectionOperationsMock oper = new LdapConnectionOperationsMock(params);
+//        assertFalse (oper.isConnected());
+//        oper.connect();
+//        assertTrue (oper.isConnected());
+//
+//        def result=oper.query(queryparams.searchBase,queryparams.searchFilter,queryparams.searchSubDirectories)
+//        assertTrue(result.hasMore());
+//        oper.disconnect();
+//        assertFalse (oper.isConnected());
+//    }
 }
 
 
-class LdapConnectionOperationsMock extends LdapConnectionOperations
-{
-    Logger log = Logger.getRootLogger();
-    String url="";
-    String userPassword ="";
-    String username ="";
-    String contextFactory ="com.sun.jndi.ldap.LdapCtxFactory";
-}
+//class LdapConnectionOperationsMock extends LdapConnectionOperations
+//{
+//    Logger log = Logger.getRootLogger();
+//    String url="";
+//    String userPassword ="";
+//    String username ="";
+//    String contextFactory ="com.sun.jndi.ldap.LdapCtxFactory";
+//}

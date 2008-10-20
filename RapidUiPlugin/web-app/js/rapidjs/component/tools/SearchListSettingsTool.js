e55214a44b8d23bbbb80920021843a35bae6abb5
(no author) <(no author)@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-10-20 13:15:34 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@3033 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidUiPlugin/web-app/js/rapidjs/component/tools/SearchListSettingsTool.js
+++ RapidUiPlugin/web-app/js/rapidjs/component/tools/SearchListSettingsTool.js
@@ -2,26 +2,33 @@ YAHOO.namespace('rapidjs', 'rapidjs.component', 'rapidjs.component.tool');
 YAHOO.rapidjs.component.tool.SearchListSettingsTool = function(container, component) {
 
     YAHOO.rapidjs.component.tool.SearchListSettingsTool.superclass.constructor.call(this, container, component,
-    						{width:300,height:130}
-    );
-
-	this.panelFields = YAHOO.ext.DomHelper.append(this.dialog.body, {tag:'div', cls:'panel-body-fields',
-	html:
-    '<table width="100%"><tbody><tr><td style="padding-top:5px" width="50%"><label>Set line size:</label></td><td style="padding-top:5px"><select><option value="1">1</option><option value="2">2</option>' +
-	'<option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option>' +
-	'<option value="7">7</option><option value="8">8</option></select></td>' +
-    '</tr></tbody></table>'
-    },true);
-
-    this.lineSizeSelector = this.dialog.container.getElementsByTagName('select')[0];
-    SelectUtils.selectTheValue(this.lineSizeSelector, this.component.lineSize, 0);
+    {height:130}
+            );
 };
 
 YAHOO.lang.extend(YAHOO.rapidjs.component.tool.SearchListSettingsTool, YAHOO.rapidjs.component.tool.SettingsTool, {
+    render: function() {
+        var dh = YAHOO.ext.DomHelper;
 
-    handleApplyClick: function(){
-	    YAHOO.rapidjs.component.tool.SearchListSettingsTool.superclass.handleApplyClick.call(this);
-	    this.component.handleLineSizeChange(parseInt(this.lineSizeSelector.options[this.lineSizeSelector.selectedIndex].value, 10));
+        var wrap = dh.append(this.dialog.body, {tag:'div',
+            html:   '<form action="javascript:void(0)"><table><tbody>' +
+                    '<tr><td width="50%"><label>Set polling interval:</label></td>' +
+                    '<td width="50%"><input type="textbox" name="PollingInterval" style="width:100px"/></td></tr>' +
+                    '<tr><td width="50%"><label>Set line size:</label></td>' +
+                    '<td width="50%"><select><option value="1">1</option><option value="2">2</option>' +
+                    '<option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option>' +
+                    '<option value="7">7</option><option value="8">8</option></select></td></tr>' +
+                    '</tbody></table></form>'
+        });
+        this.pollIntervalInput = wrap.getElementsByTagName('input')[0]
+        this.lineSizeSelector = wrap.getElementsByTagName('select')[0]
+        this.pollIntervalInput.value = this.component.getPollingInterval();
+        SelectUtils.selectTheValue(this.lineSizeSelector, this.component.lineSize, 0);
+        YAHOO.util.Event.addListener(this.dialog.container.getElementsByTagName('form')[0], 'keypress', this.applyButtonPress, this, true);
+    },
+    handleApplyClick: function() {
+        YAHOO.rapidjs.component.tool.SearchListSettingsTool.superclass.handleApplyClick.call(this);
+        this.component.handleLineSizeChange(parseInt(this.lineSizeSelector.options[this.lineSizeSelector.selectedIndex].value, 10));
 
     }
 });
--- RapidUiPlugin/web-app/js/rapidjs/component/tools/SettingsTool.js
+++ RapidUiPlugin/web-app/js/rapidjs/component/tools/SettingsTool.js
@@ -1,60 +1,64 @@
 YAHOO.namespace('rapidjs', 'rapidjs.component', 'rapidjs.component.tool');
 YAHOO.rapidjs.component.tool.SettingsTool = function(container, component, config) {
     this.config = config;
-	if(!this.config)
-		this.config = new Object();
+    if (!this.config)
+        this.config = new Object();
 
-	this.config.className = this.config.classname || "r-tool-setting";
-	this.config.tooltip = this.config.tooltip || "Configure";
-	this.title = this.config.title || "Configure";
-	this.width = this.config.width || 300;
-	this.height = this.config.height || 100;
-	this.minWidth = this.config.minWidth || 300;
-	this.minHeight = this.config.minHeight || 100;
-	YAHOO.rapidjs.component.tool.SettingsTool.superclass.constructor.call(this, container, component,this.config);
+    this.config.className = this.config.classname || "r-tool-setting";
+    this.config.tooltip = this.config.tooltip || "Configure";
+    this.title = this.config.title || "Configure";
+    this.width = this.config.width || 230;
+    this.height = this.config.height || 100;
+    this.minWidth = this.config.minWidth || 300;
+    this.minHeight = this.config.minHeight || 100;
+    YAHOO.rapidjs.component.tool.SettingsTool.superclass.constructor.call(this, container, component, this.config);
 
-	this.dialog = new YAHOO.rapidjs.component.Dialog({ width:this.width,height:this.height, minWidth: this.minWidth,
-													   minHeight: this.minHeight , title: this.title, resizable: false,
-													   buttons:[{text:"Apply", handler:this.handleApplyClick, scope:this, isDefault:true },
-															    {text:"Cancel", handler:this.handleCancelClick, scope:this }]});
-	var dh = YAHOO.ext.DomHelper;
+    this.dialog = new YAHOO.rapidjs.component.Dialog({ width:this.width,height:this.height, minWidth: this.minWidth,
+        minHeight: this.minHeight , title: this.title, resizable: false,
+        buttons:[{text:"Apply", handler:this.handleApplyClick, scope:this, isDefault:true },
+            {text:"Cancel", handler:this.handleCancelClick, scope:this }]});
+    this.render();
 
-    this.panelFields = dh.append(this.dialog.body, {tag:'div', cls:'panel-body-fields',
-	html:
-    '<table width="100%"><tbody>' +
-    '<tr><td width="50%"><label>Set polling interval:</label></td>' +
-    '<td width="50%"><input type="textbox" name="PollingInterval" style="width:100px"/></td></tr>'+
-    '</tbody></table>'
-    },true);
-    this.dialog.container.getElementsByTagName('input')[0].value = this.component.getPollingInterval();
-    YAHOO.util.Event.addListener(this.dialog.container.getElementsByTagName('input')[0], 'keypress', this.applyButtonPress, this, true);
 };
 
 YAHOO.lang.extend(YAHOO.rapidjs.component.tool.SettingsTool, YAHOO.rapidjs.component.tool.BasicTool, {
+    render: function() {
+        var dh = YAHOO.ext.DomHelper;
+
+        var wrap = dh.append(this.dialog.body, {tag:'div',
+            html:   '<form action="javascript:void(0)"><table><tbody>' +
+                    '<tr><td width="50%"><label>Set polling interval:</label></td>' +
+                    '<td width="50%"><input type="textbox" name="PollingInterval" style="width:100px"/></td></tr>' +
+                    '</tbody></table></form>'
+        });
+        this.pollIntervalInput = wrap.getElementsByTagName('input')[0]
+        this.pollIntervalInput.value = this.component.getPollingInterval();
+        YAHOO.util.Event.addListener(this.dialog.container.getElementsByTagName('form')[0], 'keypress', this.applyButtonPress, this, true);
+    },
     performAction : function() {
-	    this.clearAllFields();
-	    this.dialog.container.getElementsByTagName('input')[0].value = this.component.getPollingInterval();
+        this.clearAllFields();
+        this.pollIntervalInput.value = this.component.getPollingInterval();
         this.dialog.show();
     },
-    handleApplyClick: function(){
-	    if(this.dialog.container.getElementsByTagName('input')[0].value != '')
-    	{
-	    	var pollingInt = parseInt(this.dialog.container.getElementsByTagName('input')[0].value);
-	    	if(YAHOO.lang.isNumber(pollingInt))
-	    		this.component.setPollingInterval(pollingInt);
+    handleApplyClick: function() {
+        if (this.pollIntervalInput.value != '')
+        {
+            var pollingInt = parseInt(this.pollIntervalInput.value);
+            if (YAHOO.lang.isNumber(pollingInt))
+                this.component.setPollingInterval(pollingInt);
 
-	    	this.component.poll();
-      		this.dialog.hide();
-  		}
+            this.component.poll();
+            this.dialog.hide();
+        }
     },
-    handleCancelClick: function(){
-	    this.dialog.hide();
+    handleCancelClick: function() {
+        this.dialog.hide();
     },
-    applyButtonPress: function(e){
-	   	if (e.type == "keypress" && e.keyCode == 13){
+    applyButtonPress: function(e) {
+        if (e.type == "keypress" && e.keyCode == 13) {
 
-	  		this.handleApplyClick();
-  		}
+            this.handleApplyClick();
+        }
     },
     clearAllFields: function()
     {

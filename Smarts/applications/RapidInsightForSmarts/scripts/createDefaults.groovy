361547a32068492fc091aff2581bec0bc143133a
(no author) <(no author)@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-10-21 12:47:05 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@3056 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- Smarts/applications/RapidInsightForSmarts/scripts/createDefaults.groovy
+++ Smarts/applications/RapidInsightForSmarts/scripts/createDefaults.groovy
@@ -33,6 +33,7 @@ CmdbScript.addScript(name: "getMapData");
 def adminUser = RsUser.RSADMIN;
 def defaultEventGroup = SearchQueryGroup.add(name: "Default", username:adminUser, isPublic:true, type:"notification");
 
+SearchQuery.add(group: defaultEventGroup, name: "All Events", query: "alias:*", sortProperty: "lastChangedAt", sortOrder: "desc", username:adminUser, isPublic:true, type:"notification");
 SearchQuery.add(group: defaultEventGroup, name: "Critical Events", query: "severity:1", sortProperty: "lastChangedAt", sortOrder: "desc", username:adminUser, isPublic:true, type:"notification");
 SearchQuery.add(group: defaultEventGroup, name: "Major Events", query: "severity:2", sortProperty: "lastChangedAt", sortOrder: "desc", username:adminUser, isPublic:true, type:"notification");
 SearchQuery.add(group: defaultEventGroup, name: "Minor Events", query: "severity:3", sortProperty: "lastChangedAt", sortOrder: "desc", username:adminUser, isPublic:true, type:"notification");

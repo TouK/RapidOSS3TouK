1bccb449712a236b0070f9df4d14af4d467ac673
(no author) <(no author)@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-10-31 12:10:01 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@3174 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- Smarts/applications/RapidInsightForSmarts/scripts/getSummaryData.groovy
+++ Smarts/applications/RapidInsightForSmarts/scripts/getSummaryData.groovy
@@ -1,7 +1,7 @@
 def nodeType = params.nodeType;
 def name = params.name;
 
-def severityMap = [1:0, 2:0, 3:0, 4:0, 5:0]
+def severityMap = ["1":0, "2":0, "3":0, "4":0, "5":0]
 def severitySummary = null
 if(nodeType == "Container"){
     severitySummary = RsSmartsNotification.propertySummary("className:\"${name}\"", ["severity"]);
@@ -10,14 +10,14 @@ else{
    severitySummary = RsSmartsNotification.propertySummary("instanceName:\"${name}\"", ["severity"]);
 }
  severitySummary.severity.each{propValue, numberOfObjects ->
-     severityMap.put(propValue, numberOfObjects);
+     severityMap.put("" + propValue, numberOfObjects);
  }
 web.render(contentType: 'text/xml'){
    Items(){
-      Item(severity:"Critical", count:severityMap.get(1))
-      Item(severity:"Major", count:severityMap.get(2))
-      Item(severity:"Minor", count:severityMap.get(3))
-      Item(severity:"Unknown", count:severityMap.get(4))
-      Item(severity:"Normal", count:severityMap.get(5))
+      Item(severity:"Critical", count:severityMap.get("1"))
+      Item(severity:"Major", count:severityMap.get("2"))
+      Item(severity:"Minor", count:severityMap.get("3"))
+      Item(severity:"Unknown", count:severityMap.get("4"))
+      Item(severity:"Normal", count:severityMap.get("5"))
    }
 }

5904d75509b7a90999c2fb77cab8192b5b7b40c9
(no author) <(no author)@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-10-31 10:03:02 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@3171 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

---
+++ Smarts/applications/RapidInsightForSmarts/scripts/getEventHistory.groovy
@@ -0,0 +1,26 @@
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+def timeStampFormat = "MMM dd yyyy HH:mm:ss";
+def formatter = new SimpleDateFormat(timeStampFormat);
+def nodeType = params.nodeType;
+def name = params.name;
+
+def historicalEvents = null;
+if(nodeType == "Container"){
+    historicalEvents = RsSmartsHistoricalNotification.search("className:\"${name}\"").results;
+}
+else{
+   historicalEvents = RsSmartsHistoricalNotification.search("instanceName:\"${name}\"").results;
+}
+
+web.render(contentType: 'text/xml'){
+   data(){
+       historicalEvents.each{RsSmartsHistoricalNotification historicalEvent ->
+           def start = formatter.format(new Timestamp(historicalEvent.lastNotifiedAt)) + " GMT";
+           def end = formatter.format(new Timestamp(historicalEvent.lastClearedAt)) + " GMT";
+           def title = historicalEvent.instanceName + " " + historicalEvent.eventName;
+           event(title:title, start:start, end:end, isDuration:"true", historicalEvent.eventText)
+       }
+   }
+}

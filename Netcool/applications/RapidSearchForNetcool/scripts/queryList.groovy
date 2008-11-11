b7c36074745f8996f1214875c6abc8aa2faad9a2
(no author) <(no author)@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-11-11 15:18:15 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@3277 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- Netcool/applications/RapidSearchForNetcool/scripts/queryList.groovy
+++ Netcool/applications/RapidSearchForNetcool/scripts/queryList.groovy
@@ -4,26 +4,29 @@ import search.SearchQuery
 import search.SearchQueryGroup
 
 def user = RsUser.findByUsername(web.session.username);
-if(user == null){
+if (user == null) {
     throw new Exception("User ${web.session.username} does not exist");
 }
 
 def writer = new StringWriter();
 def queryBuilder = new MarkupBuilder(writer);
 
-SearchQueryGroup.add(name:"My Queries", username:web.session.username, type:"default");
-def queryGroups = SearchQueryGroup.list();
+SearchQueryGroup.add(name: "My Queries", username: web.session.username, type: "default");
+def queryGroups = SearchQueryGroup.searchEvery("type:\"${params.type}\" OR type:\"default\"");
 queryBuilder.Filters
 {
     queryGroups.each {SearchQueryGroup group ->
         def userName = group.username;
-        if((userName.equals(RsUser.RSADMIN) && group.isPublic) || userName.equals(user.username)){
-           queryBuilder.Filter(id: group.id, name: group.name, nodeType: "group",  isPublic:group.isPublic) {
-              group.queries.each {SearchQuery query ->
-                  queryBuilder.Filter(id: query.id, name: query.name, nodeType: "filter", viewName:query.viewName, group:group.name, 
-                          query: query.query, sortProperty: query.sortProperty, sortOrder: query.sortOrder, isPublic:query.isPublic)
-              }
-           }
+        if ((userName.equals(RsUser.RSADMIN) && group.isPublic) || userName.equals(user.username)) {
+            queryBuilder.Filter(id: group.id, name: group.name, nodeType: "group", isPublic: group.isPublic) {
+                group.queries.each {SearchQuery query ->
+                    if (query.type == params.type || query.type == "") {
+                        queryBuilder.Filter(id: query.id, name: query.name, nodeType: "filter", viewName: query.viewName, group: group.name,
+                                query: query.query, sortProperty: query.sortProperty, sortOrder: query.sortOrder, isPublic: query.isPublic)
+                    }
+
+                }
+            }
         }
     }
 }

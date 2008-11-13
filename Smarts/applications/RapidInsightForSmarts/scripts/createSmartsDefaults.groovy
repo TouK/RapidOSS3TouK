57c6be2f2b0cf78a49c07e5fa576093967b130ef
(no author) <(no author)@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-11-13 13:09:37 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@3295 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- Smarts/applications/RapidInsightForSmarts/scripts/createSmartsDefaults.groovy
+++ Smarts/applications/RapidInsightForSmarts/scripts/createSmartsDefaults.groovy
@@ -19,14 +19,14 @@ CmdbScript.addScript(name: "getGeocodes");
 CmdbScript.addScript(name: "getDeviceLocations");
 
 def adminUser = RsUser.RSADMIN;
-def defaultEventGroup = SearchQueryGroup.add(name: "Default", username:adminUser, isPublic:true, type:"notification");
+def defaultEventGroup = SearchQueryGroup.add(name: "Default", username:adminUser, isPublic:true, type:"event");
 
-SearchQuery.add(group: defaultEventGroup, name: "All Events", query: "alias:*", sortProperty: "lastChangedAt", sortOrder: "desc", username:adminUser, isPublic:true, type:"notification");
-SearchQuery.add(group: defaultEventGroup, name: "Critical Events", query: "severity:1", sortProperty: "lastChangedAt", sortOrder: "desc", username:adminUser, isPublic:true, type:"notification");
-SearchQuery.add(group: defaultEventGroup, name: "Major Events", query: "severity:2", sortProperty: "lastChangedAt", sortOrder: "desc", username:adminUser, isPublic:true, type:"notification");
-SearchQuery.add(group: defaultEventGroup, name: "Minor Events", query: "severity:3", sortProperty: "lastChangedAt", sortOrder: "desc", username:adminUser, isPublic:true, type:"notification");
-SearchQuery.add(group: defaultEventGroup, name: "Unknown Events", query: "severity:4", sortProperty: "lastChangedAt", sortOrder: "desc", username:adminUser, isPublic:true, type:"notification");
-SearchQuery.add(group: defaultEventGroup, name: "Normal Events", query: "severity:5", sortProperty: "lastChangedAt", sortOrder: "desc", username:adminUser, isPublic:true, type:"notification");
+SearchQuery.add(group: defaultEventGroup, name: "All Events", query: "alias:*", sortProperty: "lastChangedAt", sortOrder: "desc", username:adminUser, isPublic:true, type:"event");
+SearchQuery.add(group: defaultEventGroup, name: "Critical Events", query: "severity:1", sortProperty: "lastChangedAt", sortOrder: "desc", username:adminUser, isPublic:true, type:"event");
+SearchQuery.add(group: defaultEventGroup, name: "Major Events", query: "severity:2", sortProperty: "lastChangedAt", sortOrder: "desc", username:adminUser, isPublic:true, type:"event");
+SearchQuery.add(group: defaultEventGroup, name: "Minor Events", query: "severity:3", sortProperty: "lastChangedAt", sortOrder: "desc", username:adminUser, isPublic:true, type:"event");
+SearchQuery.add(group: defaultEventGroup, name: "Unknown Events", query: "severity:4", sortProperty: "lastChangedAt", sortOrder: "desc", username:adminUser, isPublic:true, type:"event");
+SearchQuery.add(group: defaultEventGroup, name: "Normal Events", query: "severity:5", sortProperty: "lastChangedAt", sortOrder: "desc", username:adminUser, isPublic:true, type:"event");
 
 def defaultTopologyGroup = SearchQueryGroup.add(name: "Default", username:adminUser, isPublic:true, type:"topology");
 

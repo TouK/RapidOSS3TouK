f379d59d922e4dd9602aca8e92a7bb134770f432
(no author) <(no author)@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-11-13 12:55:46 +0000

    Rejected commit: Default
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@3292 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

---
+++ RcmdbCommons/plugins/searchable-extension/src/groovy/com/ifountain/rcmdb/domain/statistics/OperationStatistics.groovy
@@ -0,0 +1,130 @@
+package com.ifountain.rcmdb.domain.statistics
+
+import groovy.xml.MarkupBuilder
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mustafa sener
+ * Date: Nov 12, 2008
+ * Time: 4:47:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class OperationStatistics {
+
+    public static final ADD_OPERATION_NAME = "Add";
+    public static final ADD_RELATION_OPERATION_NAME = "AddRelation";
+    public static final REMOVE_OPERATION_NAME = "Remove";
+    public static final REMOVE_RELATION_OPERATION_NAME = "RemoveRelation";
+    public static final UPDATE_OPERATION_NAME = "Update";
+    private static OperationStatistics operationStatisticsObject;
+    Map operationStatistics = [:];
+    Map modelStatistics = [:];
+    private OperationStatistics()
+    {
+        reset();    
+    }
+
+    public static OperationStatistics getInstance()
+    {
+        if(operationStatisticsObject == null)
+        {
+            operationStatisticsObject = new OperationStatistics();
+        }
+        return operationStatisticsObject;
+    }
+
+    public static void destroyInstance()
+    {
+        operationStatisticsObject = null;
+    }
+
+    public void reset()
+    {
+        operationStatistics[ADD_OPERATION_NAME] = new GlobalOperationStatisticResult(name:ADD_OPERATION_NAME);
+        operationStatistics[REMOVE_OPERATION_NAME] = new GlobalOperationStatisticResult(name:REMOVE_OPERATION_NAME);
+        operationStatistics[ADD_RELATION_OPERATION_NAME] = new GlobalOperationStatisticResult(name:ADD_RELATION_OPERATION_NAME);
+        operationStatistics[REMOVE_RELATION_OPERATION_NAME] = new GlobalOperationStatisticResult(name:REMOVE_RELATION_OPERATION_NAME);
+        operationStatistics[UPDATE_OPERATION_NAME] = new GlobalOperationStatisticResult(name:UPDATE_OPERATION_NAME);
+        modelStatistics[ADD_OPERATION_NAME] = [:];
+        modelStatistics[REMOVE_OPERATION_NAME] = [:];
+        modelStatistics[ADD_RELATION_OPERATION_NAME] = [:];
+        modelStatistics[REMOVE_RELATION_OPERATION_NAME] = [:];
+        modelStatistics[UPDATE_OPERATION_NAME] = [:];
+    }
+    
+    public String getGlobalStatistics()
+    {
+        def sw = new StringWriter();
+        MarkupBuilder builder = new MarkupBuilder(sw);
+        builder.Reports()
+        {
+            operationStatistics.each{String operationName, GlobalOperationStatisticResult result-> 
+                builder.Report(result.getGeneralReport ())
+                {
+                    def classBasedStatistics = modelStatistics[operationName];
+                    classBasedStatistics.each{String modelName, GlobalOperationStatisticResult modelResult->
+                        def modelReport = modelResult.getGeneralReport();
+                        modelReport["ModelName"] = modelName;
+                        builder.ModelReport(modelReport);
+                    }
+                }
+            }
+        }
+        return sw.toString();
+    }
+
+    public void addStatisticResult(String operationType, OperationStatisticResult result)
+    {
+        GlobalOperationStatisticResult globalResult = operationStatistics[operationType]
+        if(globalResult != null)
+        {
+            globalResult.addOperationStatisticResult (result);
+            def modelStatistic = getModelStatistic(operationType, result.model)
+            modelStatistic.addOperationStatisticResult (result);
+        }
+    }
+
+    private GlobalOperationStatisticResult getModelStatistic(String operationType, String modelName)
+    {
+        Map modelStatisticsOfOperation = modelStatistics[operationType];
+        GlobalOperationStatisticResult modelStatistic = modelStatisticsOfOperation[modelName]
+        if(modelStatistic == null)
+        {
+            modelStatistic = new GlobalOperationStatisticResult();
+            modelStatisticsOfOperation[modelName] = modelStatistic;
+        }
+        return modelStatistic;
+    }
+}
+class GlobalOperationStatisticResult
+{
+    long numberOfOperationsPerformed;
+    String name;
+    long totalOperationDuration;
+    public synchronized void addOperationStatisticResult(OperationStatisticResult result)
+    {
+        numberOfOperationsPerformed++
+        totalOperationDuration += result.operationDuration;
+    }
+
+    public Map getGeneralReport()
+    {
+        def totalDurationInSecs = totalOperationDuration/Math.pow(10, 9);
+        return [Operation:name, NumberOfOperations:numberOfOperationsPerformed, TotalDuration:totalDurationInSecs, AvarageDuration:totalDurationInSecs/numberOfOperationsPerformed];
+    }
+
+    public Map getClassBasedStatistics()
+    {
+        def classStatisticsList = [];
+        setModelStatistics.each{String modelName, GlobalOperationStatisticResult result->
+            classStatisticsList.add(result.get);
+        }
+        def totalDurationInSecs = totalOperationDuration/Math.pow(10, 9);
+        return [Operation:name, NumberOfOperations:numberOfOperationsPerformed, TotalDuration:totalDurationInSecs, AvarageDuration:totalDurationInSecs/numberOfOperationsPerformed];
+    }
+}
+class OperationStatisticResult
+{
+    String model;
+    long operationDuration;
+}
---
+++ RcmdbCommons/test/unit/com/ifountain/rcmdb/domain/statistics/OperationStatisticsTest.groovy
@@ -0,0 +1,102 @@
+package com.ifountain.rcmdb.domain.statistics
+
+import com.ifountain.rcmdb.test.util.RapidCmdbTestCase
+import groovy.util.slurpersupport.GPathResult
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mustafa sener
+ * Date: Nov 13, 2008
+ * Time: 11:22:14 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class OperationStatisticsTest extends RapidCmdbTestCase{
+
+    protected void setUp() {
+        super.setUp();    //To change body of overridden methods use File | Settings | File Templates.
+        OperationStatistics.destroyInstance();
+    }
+
+    protected void tearDown() {
+        super.tearDown();    //To change body of overridden methods use File | Settings | File Templates.
+        OperationStatistics.destroyInstance();
+    }
+
+    public void testAddOperationStatisticResult()
+    {
+        String statisticsXml = OperationStatistics.getInstance().getGlobalStatistics();
+        def slurp = new XmlSlurper();
+        def xmlObj = slurp.parseText(statisticsXml);
+        def reports = xmlObj.Report;
+        
+        assertEquals (5, reports.size());
+        def addOperationXmlNode = reports.findAll {it.@Operation.text() == OperationStatistics.ADD_OPERATION_NAME}[0];
+        checkGlobalStatisticsResult (addOperationXmlNode, []);
+        
+        OperationStatisticResult res = new OperationStatisticResult(model:"Model1", operationDuration:1000);
+        OperationStatistics.getInstance().addStatisticResult (OperationStatistics.ADD_OPERATION_NAME, res)
+
+
+        statisticsXml = OperationStatistics.getInstance().getGlobalStatistics();
+        slurp = new XmlSlurper();
+        xmlObj = slurp.parseText(statisticsXml);
+        reports = xmlObj.Report;
+
+        assertEquals (5, reports.size());
+        addOperationXmlNode = reports.findAll {it.@Operation.text() == OperationStatistics.ADD_OPERATION_NAME}[0];
+        checkGlobalStatisticsResult(addOperationXmlNode, [res]);
+        checkModelStatisticsResult(addOperationXmlNode, "Model1", [res]);
+
+        OperationStatisticResult res2 = new OperationStatisticResult(model:"Model1", operationDuration:2000);
+        OperationStatisticResult res3 = new OperationStatisticResult(model:"Model2", operationDuration:2000);
+        OperationStatistics.getInstance().addStatisticResult (OperationStatistics.ADD_OPERATION_NAME, res2)
+        OperationStatistics.getInstance().addStatisticResult (OperationStatistics.ADD_OPERATION_NAME, res3)
+
+        statisticsXml = OperationStatistics.getInstance().getGlobalStatistics();
+        slurp = new XmlSlurper();
+        xmlObj = slurp.parseText(statisticsXml);
+        reports = xmlObj.Report;
+
+        assertEquals (5, reports.size());
+        addOperationXmlNode = reports.findAll {it.@Operation.text() == OperationStatistics.ADD_OPERATION_NAME}[0];
+        checkGlobalStatisticsResult(addOperationXmlNode, [res, res2, res3]);
+        checkModelStatisticsResult(addOperationXmlNode, "Model1", [res, res2]);
+        checkModelStatisticsResult(addOperationXmlNode, "Model2", [res3]);
+
+
+        OperationStatistics.getInstance().reset();
+        statisticsXml = OperationStatistics.getInstance().getGlobalStatistics();
+        slurp = new XmlSlurper();
+        xmlObj = slurp.parseText(statisticsXml);
+        reports = xmlObj.Report;
+
+        assertEquals (5, reports.size());
+        addOperationXmlNode = reports.findAll {it.@Operation.text() == OperationStatistics.ADD_OPERATION_NAME}[0];
+        checkGlobalStatisticsResult (addOperationXmlNode, []);
+    }
+
+    private void checkModelStatisticsResult(xmlNode, String expectedModelName, operationResults)
+    {
+        def modelName = xmlNode.@ModelName.text();
+        def modelNodes = xmlNode.ModelReport.findAll {it.@ModelName.text() == expectedModelName}
+        assertEquals (1, modelNodes.size());
+        checkGlobalStatisticsResult (modelNodes[0], operationResults);
+    }
+
+    private void checkGlobalStatisticsResult(xmlNode, operationResults)
+    {
+        def numberOfOperations = xmlNode.@NumberOfOperations.text().toInteger();
+        assertEquals (operationResults.size(), numberOfOperations)
+        def expectedTotalDur = 0;
+        operationResults.each{
+            expectedTotalDur += it.operationDuration;
+        }
+        assertEquals ((int)(100000 * expectedTotalDur/Math.pow(10,9)), (int)(100000 * xmlNode.@TotalDuration.text().toDouble()))
+        if(!operationResults.isEmpty())
+        {
+            assertEquals ((int)(100000 * expectedTotalDur/(numberOfOperations*Math.pow(10,9))), (int)(100000 * xmlNode.@AvarageDuration.text().toDouble()))
+        }
+    }
+
+   
+}

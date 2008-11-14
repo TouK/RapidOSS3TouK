7dee8554950fb40728ecd24c28ff127611eef4f6
(no author) <(no author)@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-11-14 16:55:57 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@3329 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/devDocs/test/manualTesting/NotificationOperations/RapidServer/RapidSuite/scripts/notificationWebSearcher.groovy
+++ RapidCMDB/devDocs/test/manualTesting/NotificationOperations/RapidServer/RapidSuite/scripts/notificationWebSearcher.groovy
@@ -2,16 +2,14 @@ import datasource.*;
 
 logger.warn("*gonna do notwebsearch");
 
-def output=""
+
 
 def ds=HttpDatasource.get(name:NotificationOperationsConstants.HTTP_DATASOURCE_NAME);
 
-output+=" ${ds} <br>"
 
-output+=ds.doGetRequest("search?format=xml",[format:"xml",sort:"id",order:"asc",searchIn:"RsEvent",offset:"0",max:"1000",rnd:"1225386195701",login:"rsadmin",password:"changeme"]);
+def result=ds.doGetRequest("search?format=xml",[format:"xml",sort:"id",order:"asc",searchIn:"RsEvent",offset:"0",max:"1000",rnd:"1225386195701",login:"rsadmin",password:"changeme"]);
 
-logger.warn("done notwebsearch");
+logger.warn("done notwebsearch "+result[0..25]);
 
 
 //http://localhost:12222/RapidSuite/search?format=xml&searchIn=RsEvent&offset=0&sort=id&order=asc&max=100&query=&rnd=1225386195701&login=rsadmin&password=changeme
-//return output;

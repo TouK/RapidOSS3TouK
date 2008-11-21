f18f29f97c6823a3544176fe488a61fb014c9526
(no author) <(no author)@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-11-21 09:21:02 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@3368 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/grails-app/domain/connection/RapidInsightConnection.groovy
+++ RapidCMDB/grails-app/domain/connection/RapidInsightConnection.groovy
@@ -8,16 +8,16 @@ class RapidInsightConnection extends Connection{
     
     static searchable = {
         except = ["rapidInsightDatasources"];
-        
+
     };
     static cascaded = ["rapidInsightDatasources":true]
     static datasources = [:]
 
-    
+
     String baseUrl ="";
-    
+
     String userPassword ="";
-    
+
     String username ="";
     String connectionClass = "connection.RapidInsightConnectionImpl";
     List rapidInsightDatasources = [];
@@ -27,12 +27,12 @@ class RapidInsightConnection extends Connection{
     ]
     static constraints={
     baseUrl(blank:true,nullable:true)
-        
+
      userPassword(blank:true,nullable:true)
-        
+
      username(blank:true,nullable:true)
-        
-     
+
+
     }
 
     static transients = [];

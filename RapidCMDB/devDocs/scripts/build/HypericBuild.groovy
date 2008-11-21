2ed88251b0626895e45b6aab15133648fe2e7e53
(no author) <(no author)@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-11-21 09:20:28 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@3367 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/devDocs/scripts/build/HypericBuild.groovy
+++ RapidCMDB/devDocs/scripts/build/HypericBuild.groovy
@@ -6,32 +6,39 @@ package build
  * Time: 1:29:38 PM
  * To change this template use File | Settings | File Templates.
  */
-class HypericBuild extends Build{
-    static void main(String []args){
-    	HypericBuild hypericBuild = new HypericBuild();
-    	hypericBuild.run(args);
-	}
+class HypericBuild extends Build {
+    static void main(String[] args) {
+        HypericBuild hypericBuild = new HypericBuild();
+        hypericBuild.run(args);
+    }
 
-     def clean() {
+    def clean() {
         ant.delete(dir: env.dist_modules);
         ant.mkdir(dir: env.dist_modules);
     }
     def build() {
         clean();
-        ant.copy(toDir: "${env.dist_modules_rapid_suite}/generatedModels/grails-app/domain") {
-            ant.fileset(file: "${env.rapid_hyperic}/grails-app/domain/*.groovy");
-        }
+
         ant.copy(todir: "$env.dist_modules_rapid_suite/grails-app") {
             ant.fileset(dir: "$env.rapid_hyperic/grails-app")
         }
-        ant.copy(todir: "$env.dist_modules_rapid_suite/scripts") {
-            ant.fileset(dir: "$env.rapid_hyperic/scripts"){
-                ant.exclude(name:"_Install.groovy")
-                ant.exclude(name:"_Upgrade.groovy")
-            }
+        ant.copy(todir: "$env.dist_modules_rapid_suite/operations") {
+            ant.fileset(dir: "$env.rapid_hyperic/operations")
+        }
+        ant.copy(todir: "$env.dist_modules_rapid_suite/src/groovy") {
+            ant.fileset(dir: "$env.rapid_hyperic/src/groovy")
+        }
+        ant.copy(toDir: "${env.dist_modules_rapid_suite}/generatedModels/grails-app/domain") {
+            ant.fileset(file: "${env.rapid_hyperic}/applications/RapidInsight/grails-app/domain/*.groovy");
+        }
+        ant.copy(todir: "$env.dist_modules_rapid_suite") {
+            ant.fileset(dir: "$env.rapid_hyperic/applications/RapidInsight")
         }
         ant.zip(destfile: "$env.distribution/HypericPlugin.zip") {
             ant.zipfileset(dir: "$env.dist_modules")
+        }
+        ant.zip(destfile: "${env.distribution}/hyperic-rcmdb-plugin.zip"){
+            ant.zipfileset(dir:"${env.rapid_hyperic}/integration/hyperic/plugin")
         }
     }
 

40b0e345e9ea7d8576a87dbd53906f7dc3d36f5e
sezgin <sezgin@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-12-03 17:43:43 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@3603 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/devDocs/scripts/build/hudsonScripts/hudsonUtilities.sh
+++ RapidCMDB/devDocs/scripts/build/hudsonScripts/hudsonUtilities.sh
@@ -160,6 +160,7 @@ compileBuildFiles() {
     groovyc -d $GROOVY_HOME/build RapidModules/RapidCMDB/devDocs/scripts/build/Test.groovy
     groovyc -d $GROOVY_HOME/build RapidModules/RapidCMDB/devDocs/scripts/build/SmartsModuleBuild.groovy
     groovyc -d $GROOVY_HOME/build RapidModules/RapidCMDB/devDocs/scripts/build/HypericBuild.groovy
+    groovyc -d $GROOVY_HOME/build RapidModules/RapidCMDB/devDocs/scripts/build/ApgBuild.groovy
     groovyc -d $GROOVY_HOME/build RapidModules/RapidCMDB/devDocs/scripts/build/NetcoolModuleBuild.groovy
     groovyc -d $GROOVY_HOME/build RapidModules/RapidCMDB/devDocs/scripts/build/RapidInsightPluginBuild.groovy
     groovyc -d $GROOVY_HOME/build RapidModules/RapidCMDB/devDocs/scripts/build/RapidCompBuild.groovy

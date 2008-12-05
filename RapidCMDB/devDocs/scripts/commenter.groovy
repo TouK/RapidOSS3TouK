87629b08fee8f1faf959771a34b042e8e469bdd9
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-12-05 07:17:16 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@3696 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/devDocs/scripts/commenter.groovy
+++ RapidCMDB/devDocs/scripts/commenter.groovy
@@ -1,5 +1,24 @@
+/*
+* All content copyright (C) 2004-2008 iFountain, LLC., except as may otherwise be
+* noted in a separate copyright notice. All rights reserved.
+* This file is part of RapidCMDB.
+*
+* RapidCMDB is free software; you can redistribute it and/or modify
+* it under the terms version 2 of the GNU General Public License as
+* published by the Free Software Foundation. This program is distributed
+* in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+* even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+* PARTICULAR PURPOSE. See the GNU General Public License for more
+* details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+* USA.
+*/
+
 baseDir = "D:/tempworkspace/IdeaWorkspace/";
-OODirList = [baseDir+"RapidModules/RcmdbCommons"];
+OODirList = [baseDir+"RapidModules/RapidUiPlugin"];
 
 IFDirList = []
 //OODirList = [baseDir+"Apg/src/groovy", baseDir+"Hyperic"];
@@ -109,11 +128,11 @@ def editFiles(pathStr, flag, pattern){
             else{
                 if(flag == OO_FLAG){
                     println("\t    will add OO license comment to ${fname}");
-                    //fname.write(OOLicenseText + content);
+                    fname.write(OOLicenseText + content);
                 }
                 else{
                     println("\t    will add IF license comment to ${fname}");
-                    //fname.write(IFLicenseText + content);
+                    fname.write(IFLicenseText + content);
                 }
             }
 

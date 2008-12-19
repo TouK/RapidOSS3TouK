25d0f98252a5a46bb689748cd1ce4906d5ee4825
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-12-19 15:11:29 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@4020 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

---
+++ RapidCMDB/devDocs/test/manualTesting/default/RapidServer/RapidSuite/scripts/garbageCollector.groovy
@@ -0,0 +1,8 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: iFountain
+ * Date: Dec 19, 2008
+ * Time: 5:16:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+Runtime.getRuntime().gc();

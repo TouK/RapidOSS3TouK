59c16c24d03933998a188570c513d6149c8e2dd5
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-12-23 12:17:21 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@4090 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidInsight/scripts/emailSender.groovy
+++ RapidInsight/scripts/emailSender.groovy
@@ -6,6 +6,6 @@
  * To change this template use File | Settings | File Templates.
  */
 
- def datasource=EmailDatasource.get(name:"emailds");
+def datasource=EmailDatasource.get(name:"emailds");
 
- datasource.sendMail(from,to,subject,body);
+datasource.sendMail(from,to,subject,body);

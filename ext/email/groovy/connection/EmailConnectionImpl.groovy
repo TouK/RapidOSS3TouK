d30424bc03f526f05d78a303860860a597bb07f1
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-12-24 14:19:56 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@4124 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

---
+++ ext/email/groovy/connection/EmailConnectionImpl.groovy
@@ -0,0 +1,100 @@
+package connection
+
+import com.ifountain.core.connection.BaseConnection
+import com.ifountain.core.connection.ConnectionParam
+import com.ifountain.core.connection.exception.UndefinedConnectionParameterException
+
+
+import javax.mail.Session;
+import com.sun.mail.smtp.SMTPTransport;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: iFountain
+ * Date: Dec 23, 2008
+ * Time: 5:03:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class EmailConnectionImpl extends BaseConnection{
+
+    public static final String SMTPHOST = "SmtpHost";
+    public static final String SMTPPORT = "SmtpPort";
+    public static final String USERNAME = "Username";
+    public static final String PASSWORD = "Password";
+    public static final String PROTOCOL = "Protocol";
+
+    private String smtpHost;
+    private int smtpPort ;
+    private String username ;
+    private String password ;
+    private String protocol ;
+
+    private Session session;
+    private SMTPTransport transport;
+    public EmailConnectionImpl()
+    {
+
+    }
+    
+
+    protected void connect() throws Exception {
+
+        String smtpProtocol = protocol;
+        Properties props = new Properties();
+        props.put("mail."+smtpProtocol+".user", username);
+        props.put("mail."+smtpProtocol+".host", smtpHost);
+        props.put("mail."+smtpProtocol+".port", "" + smtpPort);
+        props.put("mail." + smtpProtocol + ".timeout", "30000");
+        props.put("mail." + smtpProtocol + ".auth", "true");
+        session = Session.getInstance(props);
+        transport = (SMTPTransport) session.getTransport(smtpProtocol);
+        transport.setStartTLS(true);
+        transport.connect(smtpHost, username, password);
+
+    }
+
+    protected void disconnect() {
+        if(transport!=null)
+        {
+            transport.close();
+        }
+        transport=null;
+    }
+    public void init(ConnectionParam param) throws Exception{
+        super.init(param)
+        this.smtpHost = checkParam(SMTPHOST);
+        this.smtpPort = Integer.valueOf(checkParam(SMTPPORT));
+        this.username = checkParam(USERNAME);
+        this.password = checkParam(PASSWORD);
+        this.protocol = checkParam(PROTOCOL);
+    }
+
+    public boolean checkConnection() {
+        boolean result=false;
+        if(transport != null)
+        {
+            try
+            {
+               if(transport.ehlo())
+               {
+                 result=true;
+               }
+            }
+            catch (javax.mail.MessagingException e)
+            {
+                result=false;
+            }
+        }
+        return result;
+
+        
+    }
+    
+     protected String checkParam(String parameterName) throws UndefinedConnectionParameterException {
+        if(!params.getOtherParams().containsKey(parameterName)){
+            throw new UndefinedConnectionParameterException(parameterName);
+        }
+        return (String) params.getOtherParams().get(parameterName);
+    }
+    
+}
---
+++ ext/email/groovy/datasource/EmailAdapter.groovy
@@ -0,0 +1,25 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: iFountain
+ * Date: Dec 23, 2008
+ * Time: 4:45:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+package datasource
+
+import com.ifountain.core.datasource.BaseAdapter
+
+import org.apache.log4j.Logger
+
+
+public class EmailAdapter extends BaseAdapter{
+
+    public EmailAdapter(connectionName, reconnectInterval, logger){
+            super(connectionName, reconnectInterval, logger);
+    }
+    
+     public Map<String, Object> getObject(Map<String, String> ids, List<String> fieldsToBeRetrieved) {
+        return null; //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+}

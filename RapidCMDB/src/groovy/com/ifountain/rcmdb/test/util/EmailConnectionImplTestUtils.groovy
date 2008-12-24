2df1f1cce9275b2b838489ec2a086e19ee6343ef
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-12-24 14:20:52 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@4125 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

---
+++ RapidCMDB/src/groovy/com/ifountain/rcmdb/test/util/EmailConnectionImplTestUtils.groovy
@@ -0,0 +1,24 @@
+package com.ifountain.rcmdb.test.util
+/**
+ * Created by IntelliJ IDEA.
+ * User: iFountain
+ * Date: Dec 24, 2008
+ * Time: 4:07:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+import com.ifountain.comp.test.util.CommonTestUtils
+
+class EmailConnectionImplTestUtils {
+
+   public static Map getConnectionParams(){
+        def paramMap=["SmtpHost":"Smtp.SmtpHost","SmtpPort":"Smtp.SmtpPort","Username":"Smtp.Username","Password":"Smtp.Password","Protocol":"Smtp.Protocol"]
+
+        def params=[:]
+        paramMap.each{ key , propKey ->        
+            params[key]= CommonTestUtils.getTestProperty(propKey);
+        }
+
+        return params;
+    }
+}
---
+++ RapidCMDB/test/unit/connection/EmailConnectionImplTest.groovy
@@ -0,0 +1,40 @@
+package connection
+
+
+import com.ifountain.core.connection.ConnectionParam
+import com.ifountain.rcmdb.test.util.EmailConnectionImplTestUtils
+import com.ifountain.core.test.util.RapidCoreTestCase
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: iFountain
+ * Date: Dec 24, 2008
+ * Time: 3:42:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class EmailConnectionImplTest extends RapidCoreTestCase{
+
+     public void testConnect()
+     {
+        Map params = EmailConnectionImplTestUtils.getConnectionParams();
+        params.SmtpPort=Integer.valueOf(params.SmtpPort)
+        
+         def conParams=new ConnectionParam("EmailConnection","dummy","dummy",params);
+
+
+         def con=new EmailConnectionImpl()
+         con.init(conParams)
+         
+         assertFalse(con.checkConnection());
+         assertFalse(con.isConnected());
+
+         con._connect()
+         assertTrue(con.checkConnection());
+         assertTrue(con.isConnected());
+
+         con._disconnect()
+         assertFalse(con.checkConnection());
+         assertFalse(con.isConnected());
+     }
+
+}

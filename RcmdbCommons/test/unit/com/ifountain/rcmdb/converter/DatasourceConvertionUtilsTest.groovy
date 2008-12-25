74858bf381d49d163f2e8fa450ae785804108436
mustafa <mustafa@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-12-25 11:36:27 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@4148 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

---
+++ RcmdbCommons/test/unit/com/ifountain/rcmdb/converter/DatasourceConvertionUtilsTest.groovy
@@ -0,0 +1,62 @@
+package com.ifountain.rcmdb.converter
+
+import com.ifountain.rcmdb.test.util.RapidCmdbTestCase
+import com.ifountain.rcmdb.domain.converter.DatasourceConvertionUtils
+import com.ifountain.rcmdb.domain.converter.datasource.Converter
+import com.ifountain.rcmdb.domain.converter.datasource.DefaultConverter
+import java.sql.Timestamp
+import java.sql.Time
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mustafa sener
+ * Date: Dec 25, 2008
+ * Time: 10:27:09 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class DatasourceConvertionUtilsTest extends RapidCmdbTestCase
+{
+    public void testDefaultConversionSettings()
+    {
+
+        def objectsToBeConvertedToLong = [new Integer(100), 100, new Short((short)100), (short)100, (long)100, new Long(100), (byte)1, new Byte((byte)1)];
+        def expectedLongValues = [new Long(100), new Long(100), new Long(100), new Long(100), new Long(100), new Long(100), new Long(1), new Long(1)];
+        checkConversion(objectsToBeConvertedToLong, expectedLongValues, DefaultConverter);
+
+        def objectsToBeConvertedToDouble = [new Float(100), (float)100, new Double(100), (double)100];
+        def expectedDoubleValues = [new Double(100),new Double(100),new Double(100),new Double(100)];
+        checkConversion(objectsToBeConvertedToDouble, expectedDoubleValues, DefaultConverter);
+
+        def objectsToBeConvertedBoolean = [new Boolean(false), false];
+        def expectedBooleanValues = [new Boolean(false), false];
+        checkConversion(objectsToBeConvertedBoolean, expectedBooleanValues, DefaultConverter);
+
+        def now  = System.currentTimeMillis();
+        def objectsToBeConvertedDate = [new Date(now), new Timestamp(now), new Time(now)];
+        def expectedDateValues = [new Date(now), new Timestamp(now), new Time(now)];
+        checkConversion(objectsToBeConvertedDate, expectedDateValues, com.ifountain.rcmdb.domain.converter.datasource.NotConvertingConverter);
+    }
+
+    public void testRegisterClosureConverter()
+    {
+        Closure conversionClosure = {objectValue->
+            return String.valueOf(objectValue);
+        }
+        DatasourceConvertionUtils.getInstance().register(Long, conversionClosure);
+    }
+
+
+
+    private void checkConversion(List objectsToCheckConversion, List expectedConverionresults, Class expectedConverter)
+    {
+        for(int i=0; i < objectsToCheckConversion.size(); i++)
+        {
+            Object objToBeConverted = objectsToCheckConversion.get(i);
+            Object expectedConvertedObject = expectedConverionresults.get(i);
+            Converter converter = DatasourceConvertionUtils.getInstance().lookup(objToBeConverted.getClass());
+            assertEquals (expectedConverter, converter.class);
+            assertEquals(expectedConvertedObject.class.name, converter.convert(objToBeConverted).class.name);
+            assertEquals(expectedConvertedObject, converter.convert(objToBeConverted));
+        }
+    }
+}

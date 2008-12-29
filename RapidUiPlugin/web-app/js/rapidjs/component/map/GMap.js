e1f7b241a4f4fd2da1e8f9ac87a4dafb3a82847b
sezgin <sezgin@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2008-12-29 12:18:08 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@4191 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidUiPlugin/web-app/js/rapidjs/component/map/GMap.js
+++ RapidUiPlugin/web-app/js/rapidjs/component/map/GMap.js
@@ -17,22 +17,22 @@
 * USA.
 */
 YAHOO.namespace('rapidjs', 'rapidjs.component');
-YAHOO.rapidjs.component.GMap = function(container, config){
-	YAHOO.rapidjs.component.GMap.superclass.constructor.call(this,container, config);
-	YAHOO.ext.util.Config.apply(this, config);
+YAHOO.rapidjs.component.GMap = function(container, config) {
+    YAHOO.rapidjs.component.GMap.superclass.constructor.call(this, container, config);
+    YAHOO.ext.util.Config.apply(this, config);
 
     var events = {
         'markerClick' : new YAHOO.util.CustomEvent('markerClick')
     };
     YAHOO.ext.util.Config.apply(this.events, events);
     this.configureTimeout(config);
-	this.markers = [];
-	this.locations = {};
-	this.renderingFinished = false;
-	this.pollInterval = 0;
-	YAHOO.util.Event.addListener(window, 'unload', function(){
-		GUnload();
-	});
+    this.markers = [];
+    this.locations = {};
+    this.renderingFinished = false;
+    this.pollInterval = 0;
+    YAHOO.util.Event.addListener(window, 'unload', function() {
+        GUnload();
+    });
 
     this.header = YAHOO.ext.DomHelper.append(this.container, {tag:'div'});
     this.toolbar = new YAHOO.rapidjs.component.tool.ButtonToolBar(this.header, {title: this.title});
@@ -46,112 +46,116 @@ YAHOO.rapidjs.component.GMap = function(container, config){
 
 YAHOO.extend(YAHOO.rapidjs.component.GMap, YAHOO.rapidjs.component.PollingComponentContainer, {
 
-	handleSuccess: function(response){
-		this.clearData();
-		var data = new YAHOO.rapidjs.data.RapidXmlDocument(response);
-		var node = data.rootNode;
-		if(node){
-			var locations = node.getElementsByTagName(this.contentPath);
-			var bounds = new GLatLngBounds();
-			for(var index=0; index<locations.length; index++) {
-				var locationNode = locations[index];
-				this.createLocation(locationNode);
-			}
-			this.putMarkersOnMap();
-		 }
-	},
+    handleSuccess: function(response) {
+        this.clearData();
+        var data = new YAHOO.rapidjs.data.RapidXmlDocument(response);
+        var node = data.rootNode;
+        if (node) {
+            var locations = node.getElementsByTagName(this.contentPath);
+            var bounds = new GLatLngBounds();
+            for (var index = 0; index < locations.length; index++) {
+                var locationNode = locations[index];
+                this.createLocation(locationNode);
+            }
+            this.putMarkersOnMap();
+        }
+    },
 
-	createLocation: function(locationNode){
-		var lat = parseFloat(locationNode.getAttribute(this.latitudeAttributeName));
-		var lng = parseFloat(locationNode.getAttribute(this.longitudeAttributeName));
-		var address = locationNode.getAttribute(this.addressAttributeName);
-		if(!this.locations[address]){
-			var tooltip = "";
-			if(this.tooltipAttributeName){
-				tooltip = locationNode.getAttribute(this.tooltipAttributeName);
-			}
-			var marker = locationNode.getAttribute(this.markerAttributeName);
-			this.locations[address] = {lat:lat, lng: lng, tooltip:tooltip, marker: marker, node:locationNode};
-		}
-	},
+    createLocation: function(locationNode) {
+        var lat = parseFloat(locationNode.getAttribute(this.latitudeAttributeName));
+        var lng = parseFloat(locationNode.getAttribute(this.longitudeAttributeName));
+        var address = locationNode.getAttribute(this.addressAttributeName);
+        if (!this.locations[address]) {
+            var tooltip = "";
+            if (this.tooltipAttributeName) {
+                tooltip = locationNode.getAttribute(this.tooltipAttributeName);
+            }
+            var marker = locationNode.getAttribute(this.markerAttributeName);
+            this.locations[address] = {lat:lat, lng: lng, tooltip:tooltip, marker: marker, node:locationNode};
+        }
+    },
 
-	render: function(){
-		if (GBrowserIsCompatible()) {
-		    this.map = new GMap2(this.body.dom);
-		    this.map.addControl(new GLargeMapControl());
-		    this.map.setCenter(new GLatLng(0,0),1);
+    render: function() {
+        if (GBrowserIsCompatible()) {
+            this.map = new GMap2(this.body.dom);
+            this.map.addControl(new GLargeMapControl());
+            this.map.setCenter(new GLatLng(0, 0), 1);
 
 		    // Create our "tiny" marker icon
-			this.baseIcon = new GIcon();
-/*			this.baseIcon.shadow = "http://labs.google.com/ridefinder/images/mm_30_shadow.png";
-			this.baseIcon.iconSize = new GSize(12, 20);
-			this.baseIcon.shadowSize = new GSize(22, 20);
-			this.baseIcon.iconAnchor = new GPoint(6, 20);
-			this.baseIcon.infoWindowAnchor = new GPoint(5, 1);
-*/
-			this.baseIcon.shadow = "http://www.google.com/mapfiles/shadow50.png";
-			this.baseIcon.iconSize = new GSize(20, 34);
-			this.baseIcon.shadowSize = new GSize(37, 34);
-			this.baseIcon.iconAnchor = new GPoint(9, 34);
-			this.baseIcon.infoWindowAnchor = new GPoint(9, 2);
+            this.baseIcon = new GIcon();
+            /*			this.baseIcon.shadow = "http://labs.google.com/ridefinder/images/mm_30_shadow.png";
+                        this.baseIcon.iconSize = new GSize(12, 20);
+                        this.baseIcon.shadowSize = new GSize(22, 20);
+                        this.baseIcon.iconAnchor = new GPoint(6, 20);
+                        this.baseIcon.infoWindowAnchor = new GPoint(5, 1);
+            */
+            this.baseIcon.shadow = "http://www.google.com/mapfiles/shadow50.png";
+            this.baseIcon.iconSize = new GSize(20, 34);
+            this.baseIcon.shadowSize = new GSize(37, 34);
+            this.baseIcon.iconAnchor = new GPoint(9, 34);
+            this.baseIcon.infoWindowAnchor = new GPoint(9, 2);
 
-      }
-	},
+        }
+    },
 
-	clearData: function(){
-		while(this.markers.length > 0){
-			var marker = this.markers.pop();
-			GEvent.clearInstanceListeners(marker);
-			this.map.removeOverlay(marker);
-		}
-		this.locations = {};
-	},
+    clearData: function() {
+        while (this.markers.length > 0) {
+            var marker = this.markers.pop();
+            GEvent.clearInstanceListeners(marker);
+            this.map.removeOverlay(marker);
+        }
+        this.locations = {};
+    },
 
-	createMarker: function(point, loc) {
-// Do we need to clean up icons created????
-	  var icon = new GIcon(this.baseIcon);
-	  icon.image = loc.marker;
-	  var marker = new GMarker(point, icon);
-	  var component = this;
-      var node = loc.node;
-      var mapEvents = this.events;
-      GEvent.addListener(marker, "click", function(e) {
-            mapEvents['markerClick'].fireDirect( node);
-      });
+    createMarker: function(point, loc) {
+        // Do we need to clean up icons created????
+        var icon = new GIcon(this.baseIcon);
+        icon.image = loc.marker;
+        var marker = new GMarker(point, icon);
+        var component = this;
+        var node = loc.node;
+        var mapEvents = this.events;
+        var component = this;
+        GEvent.addListener(marker, "click", function(e) {
+            if (component.tooltipAttributeName) {
+                marker.openInfoWindowHtml(loc.tooltip);
+            }
+            mapEvents['markerClick'].fireDirect(node);
+        });
 
-	  this.markers.push(marker);
-	  return marker;
-	},
+        this.markers.push(marker);
+        return marker;
+    },
 
 
-	putMarkersOnMap: function(){
-		var bounds = new GLatLngBounds();
-		var index=0;
-		for(var address in this.locations) {
-			var loc = this.locations[address];
-			var point = new GLatLng(loc.lat, loc.lng);
-			var marker = this.createMarker(point, loc);
-			this.map.addOverlay(marker);
-			bounds.extend(point);
-		}
-		var zoom = this.map.getBoundsZoomLevel(bounds);
-		var lng = bounds.getCenter().lng();
-		var lat = bounds.getCenter().lat()
-		if (zoom<6){
-			lat +=1;
-		}
-		var center = new GLatLng(lat, lng);
-		this.map.setZoom(zoom);
-		this.map.setCenter(center);
+    putMarkersOnMap: function() {
+        var bounds = new GLatLngBounds();
+        var index = 0;
+        for (var address in this.locations) {
+            var loc = this.locations[address];
+            var point = new GLatLng(loc.lat, loc.lng);
+            var marker = this.createMarker(point, loc);
+            this.map.addOverlay(marker);
+            bounds.extend(point);
+        }
+        var zoom = this.map.getBoundsZoomLevel(bounds);
+        var lng = bounds.getCenter().lng();
+        var lat = bounds.getCenter().lat()
+        if (zoom < 6) {
+            lat += 1;
+        }
+        var center = new GLatLng(lat, lng);
+        this.map.setZoom(zoom);
+        this.map.setCenter(center);
 //		this.map.setZoom(this.map.getBoundsZoomLevel(bounds));
-//		this.map.setCenter(bounds.getCenter());
-	},
-	resize : function(width, height) {
-	    this.body.setHeight( height - this.header.offsetHeight);
-	    if(this.renderingFinished == false){
-			this.render();
-			this.renderingFinished = true;
-		}
+        //		this.map.setCenter(bounds.getCenter());
+    },
+    resize : function(width, height) {
+        this.body.setHeight(height - this.header.offsetHeight);
+        if (this.renderingFinished == false) {
+            this.render();
+            this.renderingFinished = true;
+        }
 
 //			this.poll();
 

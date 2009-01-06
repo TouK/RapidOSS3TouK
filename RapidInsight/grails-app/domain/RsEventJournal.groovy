9c129b82958b78fdc16ef6659eff5a005eac1d58
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-01-06 09:00:28 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@4258 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidInsight/grails-app/domain/RsEventJournal.groovy
+++ RapidInsight/grails-app/domain/RsEventJournal.groovy
@@ -1,4 +1,4 @@
-/* 
+/*
 * All content copyright (C) 2004-2008 iFountain, LLC., except as may otherwise be
 * noted in a separate copyright notice. All rights reserved.
 * This file is part of RapidCMDB.
@@ -25,64 +25,66 @@ class RsEventJournal {
 
     static searchable = {
         except = ["errors", "__operation_class__", "__is_federated_properties_loaded__"];
-    
-    
+
+
     };
     static datasources = ["RCMDB":["keys":["id":["nameInDs":"id"]]]]
 
-    
-    String eventId ="";
-    
+
+    Long eventId =0;
+
     Date rsTime =new Date(0);
-    
+
     String eventName ="";
-    
+
     String details ="";
-    
+
     Long id ;
-    
+
     Long version ;
-    
+
     org.springframework.validation.Errors errors ;
-    
+
     Object __operation_class__ ;
-    
+
     Object __is_federated_properties_loaded__ ;
-    
-    
-    static relations = [:]    
-    
+
+
+    static relations = [:]
+
     static constraints={
-    eventId(blank:true,nullable:true)
-        
+    eventId(nullable:true)
+
      rsTime(nullable:true)
-        
+
      eventName(blank:true,nullable:true)
-        
+
      details(blank:true,nullable:true)
-        
+
      __operation_class__(nullable:true)
-        
+
      __is_federated_properties_loaded__(nullable:true)
-        
+
      errors(nullable:true)
-        
-     
+
+
     }
 
     static propertyConfiguration= [:]
     static transients = ["errors", "__operation_class__", "__is_federated_properties_loaded__"];
-    
+
     public String toString()
     {
     	return "${getClass().getName()}[id:${getProperty("id")}]";
     }
-    
+
     public boolean equals(Object obj) {
         return obj.id == this.id;
     }
     //AUTO_GENERATED_CODE
 
 
-    
+
+
+
 }

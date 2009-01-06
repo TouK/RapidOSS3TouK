37defb6bc1a1cb8e529f1c44b295596422d081db
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-01-06 13:30:54 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@4267 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidInsight/scripts/emailGenerator.groovy
+++ RapidInsight/scripts/emailGenerator.groovy
@@ -5,14 +5,33 @@
  * Time: 5:29:28 PM
  * To change this template use File | Settings | File Templates.
  */
-def now= (new Date()).getTime()
+RsMessage.removeAll();
 
-def event=RsEvent.get(name:"ev1")
+def delay=6000;
+def lastMaxId=0
 
-def mes=RsMessage.add(eventId:event.id,destination:"abdurrahim",destinationType:"email",action:"create",insertedAt:now,state:1)
+def queries=search.SearchQuery.searchEvery("id:38 OR id:40")
+def eventQuery="(id:0)  "
 
-if(mes.hasErrors())
-{
-	return mes.errors
+queries.each {
+    eventQuery +=  "OR (${it.query})"
 }
-return "success"
+eventQuery = " (${eventQuery}) AND id:[${lastMaxId} TO *]"
+
+logger.info("eventQuery is ${eventQuery}")
+
+def events=RsEvent.searchEvery(eventQuery,[sort:"id", order:"asc"])
+def date=new Date()
+def now=date.getTime();
+
+events.each{ event ->
+    now=date.getTime();
+    RsMessage.add(eventId:event.id,destination:"abdurrahim",destinationType:"email",insertedAt:now,sendAfter:now+delay,state:0,action:"create")
+}
+
+now=date.getTime();
+def delayingMessages=RsMessage.searchEvery("state:0 AND sendAfter:[0 TO ${now}]")
+delayingMessages.each{
+    it.update(state:1)
+    logger.debug("Updated delaying message with id ${it.id}, changed state to 1");
+}

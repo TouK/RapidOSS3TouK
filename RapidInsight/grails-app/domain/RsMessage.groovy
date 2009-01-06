8cdb8b0524cc57080208f868643aa7f8537b7734
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-01-06 09:14:40 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@4259 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidInsight/grails-app/domain/RsMessage.groovy
+++ RapidInsight/grails-app/domain/RsMessage.groovy
@@ -7,41 +7,65 @@ class RsMessage {
 
     static searchable = {
         except = ["errors", "__operation_class__", "__is_federated_properties_loaded__"];
-    
-    
-    };
-    static datasources = ["RCMDB":["keys":["eventId":["nameInDs":"eventId"], "to":["nameInDs":"to"]]]]
 
-    
-    String eventId ="";
-    
-    String to ="";
-    
+
+    };
+    static datasources = ["RCMDB":["keys":["eventId":["nameInDs":"eventId"], "destination":["nameInDs":"destination"], "destinationType":["nameInDs":"destinationType"], "action":["nameInDs":"action"]]]]
+
+
+    Long eventId =0;
+
+    String destination ="";
+
+    String destinationType ="";
+
+    String action ="";
+
+    Long insertedAt =0;
+
+    Long sendAfter =0;
+
+    Long sendAt =0;
+
+    Long state =0;
+
     Long id ;
-    
+
     Long version ;
-    
+
     org.springframework.validation.Errors errors ;
-    
+
     Object __operation_class__ ;
-    
+
     Object __is_federated_properties_loaded__ ;
-    
-    
-    static relations = [:]    
-    
+
+
+    static relations = [:]
+
     static constraints={
-    eventId(blank:false,nullable:false)
-        
-     to(blank:false,nullable:false,key:["eventId"])
-        
+    eventId(nullable:false)
+
+     destination(blank:false,nullable:false)
+
+     destinationType(blank:false,nullable:false)
+
+     action(blank:false,nullable:false,key:["eventId", "destination", "destinationType"])
+
+     insertedAt(nullable:true)
+
+     sendAfter(nullable:true)
+
+     sendAt(nullable:true)
+
+     state(nullable:true)
+
      __operation_class__(nullable:true)
-        
+
      __is_federated_properties_loaded__(nullable:true)
-        
+
      errors(nullable:true)
-        
-     
+
+
     }
 
     static propertyConfiguration= [:]
@@ -49,13 +73,14 @@ class RsMessage {
     
     public String toString()
     {
-    	return "${getClass().getName()}[eventId:${getProperty("eventId")}, to:${getProperty("to")}]";
+    	return "${getClass().getName()}[action:${getProperty("action")}, destination:${getProperty("destination")}, destinationType:${getProperty("destinationType")}, eventId:${getProperty("eventId")}]";
     }
-    
+
     public boolean equals(Object obj) {
         return obj.id == this.id;
     }
     //AUTO_GENERATED_CODE
 
-    
+
+
 }

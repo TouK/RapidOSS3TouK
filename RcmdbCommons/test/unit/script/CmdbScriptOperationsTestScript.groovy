92abaac718fdb7ff75563b8f8a45b92cfd935e7d
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-01-07 09:44:09 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@4276 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RcmdbCommons/test/unit/script/CmdbScriptOperationsTestScript.groovy
+++
@@ -1,8 +0,0 @@
-/**
- * Created by IntelliJ IDEA.
- * User: iFountain
- * Date: Dec 24, 2008
- * Time: 11:12:22 AM
- * To change this template use File | Settings | File Templates.
- */
- println "Hello"

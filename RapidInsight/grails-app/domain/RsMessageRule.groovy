01e844538b81590e847c7f5a1a3f3deb4d323e0a
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-01-07 16:11:34 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@4291 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidInsight/grails-app/domain/RsMessageRule.groovy
+++ RapidInsight/grails-app/domain/RsMessageRule.groovy
@@ -1,76 +1,72 @@
 
 import com.ifountain.core.domain.annotations.*;
 
-class RsMessageRule 
-{
-    
+class RsMessageRule {
+
     //AUTO_GENERATED_CODE
 
     static searchable = {
         except = ["errors", "__operation_class__", "__is_federated_properties_loaded__"];
-    
-    
+
+
     };
     static datasources = ["RCMDB":["keys":["searchQueryId":["nameInDs":"searchQueryId"], "userId":["nameInDs":"userId"], "destinationType":["nameInDs":"destinationType"]]]]
 
-    
+
     Long searchQueryId =0;
-    
+
     Long userId =0;
-    
+
     String destinationType ="";
-    
+
     Long delay =0;
-    
-    Boolean createAction =false;
-    
+
     Boolean clearAction =false;
-    
+
     Long id ;
-    
+
     Long version ;
-    
+
     org.springframework.validation.Errors errors ;
-    
+
     Object __operation_class__ ;
-    
+
     Object __is_federated_properties_loaded__ ;
-    
-    
-    static relations = [:]    
-    
+
+
+    static relations = [:]
+
     static constraints={
     searchQueryId(nullable:false)
-        
+
      userId(nullable:false)
-        
+
      destinationType(blank:false,nullable:false,key:["searchQueryId", "userId"])
-        
+
      delay(nullable:true)
-        
-     createAction(nullable:true)
-        
+
      clearAction(nullable:true)
-        
+
      __operation_class__(nullable:true)
-        
+
      __is_federated_properties_loaded__(nullable:true)
-        
+
      errors(nullable:true)
         
-     
+
     }
 
     static propertyConfiguration= [:]
     static transients = ["errors", "__operation_class__", "__is_federated_properties_loaded__"];
-    
+
     public String toString()
     {
     	return "${getClass().getName()}[destinationType:${getProperty("destinationType")}, searchQueryId:${getProperty("searchQueryId")}, userId:${getProperty("userId")}]";
     }
-    
+
     public boolean equals(Object obj) {
         return obj.id == this.id;
     }
     //AUTO_GENERATED_CODE
+
 }

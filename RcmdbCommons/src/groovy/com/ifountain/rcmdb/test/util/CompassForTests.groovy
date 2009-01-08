88bad9c4d1d8cc97eeee9d4d6fea453b2e35f69f
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-01-08 07:39:44 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@4296 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RcmdbCommons/src/groovy/com/ifountain/rcmdb/test/util/CompassForTests.groovy
+++ RcmdbCommons/src/groovy/com/ifountain/rcmdb/test/util/CompassForTests.groovy
@@ -19,7 +19,8 @@
 package com.ifountain.rcmdb.test.util
 
 import com.ifountain.rcmdb.util.RapidCMDBConstants
-
+import org.codehaus.groovy.grails.commons.DefaultGrailsDomainClass
+import com.ifountain.rcmdb.domain.util.DomainClassUtils
 /**
  * Created by IntelliJ IDEA.
  * User: iFountain
@@ -31,16 +32,19 @@ class CompassForTests {
 //    static List classesToBeInitialized;
     static MockOperationData addOperationData = new MockOperationData();
     static MockOperationData getOperationData = new MockOperationData();
+    static MockOperationData updateOperationData = new MockOperationData();
     static MockOperationData addRelationOperationData = new MockOperationData();
     static MockOperationData removeRelationOperationData = new MockOperationData();
     public static void initialize(List classesToBeInitialized)
     {
+         ExpandoMetaClass.enableGlobally();
          addOperationData.initialize(classesToBeInitialized);
          getOperationData.initialize(classesToBeInitialized);
+         updateOperationData.initialize(classesToBeInitialized);
          addRelationOperationData.initialize(classesToBeInitialized);
          removeRelationOperationData.initialize(classesToBeInitialized);
         classesToBeInitialized.each{Class domainClass->
-
+            def dc=new DefaultGrailsDomainClass(domainClass)
             domainClass.metaClass.static.add = {Map props->
                 return addOperationData.getReturnObject(domainClass, new HashMap(props));
             }
@@ -49,6 +53,41 @@ class CompassForTests {
             }
             domainClass.metaClass.static.get = {Long id->
                 return getOperationData.getReturnObject(domainClass, new HashMap(["id":id]));
+            }
+            domainClass.metaClass.static.update = {Map props->
+               def relations = DomainClassUtils.getRelations(dc);
+               props.each{key,value->
+                    def relation=relations[key];
+                    if(relation==null)
+                    {
+                        try{
+                            delegate[key]=value;
+                        }catch(e){ println e}
+                    }
+                    else
+                    {
+                        value=value instanceof Collection?value:[value]
+                        if(relation.isOneToOne() || relation.isManyToOne())
+                        {
+                            delagate[key]=value[0];
+                        }
+                        else
+                        {
+                            //TODO: we do not preserve old relation list here, we always update
+                            //if a code calls more than one updates with different relations old relations are always gone
+                            if(value[0] != null)
+                            {
+                                delagate[key]=value;
+                            }
+                            else
+                            {
+                                delate[key].clear();
+                            }
+                        }
+                    }
+                }
+
+                return updateOperationData.getReturnObject(domainClass, new HashMap(props));
             }
             domainClass.metaClass.static.addRelation = {Map props->
                 return addRelationOperationData.getReturnObject(domainClass, new HashMap(props));
@@ -59,7 +98,6 @@ class CompassForTests {
             domainClass.metaClass.hasErrors = {
                 return false;
             }
-
         }
     }
 
@@ -148,3 +186,4 @@ class MockOperationData{
         }
     }
 }
+

dc8c54b5249e578cba0d1ea4da5c1db79870ecad
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-01-09 12:55:00 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@4357 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidInsight/scripts/emailSender.groovy
+++ RapidInsight/scripts/emailSender.groovy
@@ -35,55 +35,60 @@ if(emailDs.hasErrors())
 def date=new Date();
 
 def ds=EmailDatasource.get(name:"emailds")
+if(ds!=null)
+{
+    def messages=RsMessage.searchEvery("state:1 AND destinationType:\"${RsMessage.EMAIL}\"", [sort: "id",order:"asc",max:1000]);
+
+    messages.each{ message ->
+        def event=null;
+        if(message.action=="create")
+        {
+            event=RsEvent.get(id:message.eventId);
+        }
+        else
+        {
+            event=RsHistoricalEvent.search("activeId:${message.eventId}").results[0];
+        }
+
+        if(event!=null)
+        {
+
+            def eventParams=event.asMap();
+            logger.debug("Will send email about RsEvent : ${eventParams}");
+
+            def templateParams=[eventParams:eventParams]
+            def emailParams=[:]
+            emailParams.from=from
+            emailParams.to=message.destination
+            emailParams.subject= ( message.action  == "create" ? "Event Created" : "Event Cleared" )
+            emailParams.template=templatePath
+            emailParams.templateParams=templateParams
+            emailParams.contentType="text/html"
+
+            try{
+                ds.sendEmail(emailParams)
+                logger.debug("Sended email about RsEvent: ${eventParams}")
+                message.update(state:3,sendAt:date.getTime());
+                logger.debug("Updated state of message as 3,with eventId ${message.eventId}")
+            }
+            catch(e)
+            {
+                logger.warn("Error occured while sending email.Reason ${e}",e);
+            }
 
 
-def messages=RsMessage.searchEvery("state:1 AND destinationType:\"${RsMessage.EMAIL}\"", [sort: "id",order:"asc",max:1000]);
+        }
+        else
+        {
+            message.update(state:4,sendAt:date.getTime());
+            logger.warn("RsEvent/RsHistoricalEvent with eventId ${message.eventId} does not exist. Will not send email. Updated state of message as 4");
+        }
 
-messages.each{ message ->
-	def event=null;
-	if(message.action=="create")
-    {
-        event=RsEvent.get(id:message.eventId);
     }
-    else
-    {
-        event=RsHistoricalEvent.search("activeId:${message.eventId}").results[0];
-    }
-
-	if(event!=null)
-	{
-
-		def eventParams=event.asMap();
-		logger.debug("Will send email about RsEvent : ${eventParams}");
-
-		def templateParams=[eventParams:eventParams]
-		def emailParams=[:]
-		emailParams.from=from
-		emailParams.to=message.destination
-		emailParams.subject= ( message.action  == "create" ? "Event Created" : "Event Cleared" )
-		emailParams.template=templatePath
-		emailParams.templateParams=templateParams
-		emailParams.contentType="text/html"
-
-		try{
-			ds.sendEmail(emailParams)
-			logger.debug("Sended email about RsEvent: ${eventParams}")
-            message.update(state:3,sendAt:date.getTime());
-            logger.debug("Updated state of message as 3,with eventId ${message.eventId}")
-		}
-		catch(e)
-		{               
-            logger.warn("Error occured while sending email.Reason ${e}",e);
-		}
-
-
-	}
-	else
-	{
-        message.update(state:4,sendAt:date.getTime());
-        logger.warn("RsEvent/RsHistoricalEvent with eventId ${message.eventId} does not exist. Will not send email. Updated state of message as 4");
-	}
-
 }
+else{
+    logger.warn("EmailDatasource specified does not exist");
+}
+
 return "ended"
 

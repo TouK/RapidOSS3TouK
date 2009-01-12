ebc51ff5be6090ea9df5271fb4483be3d9c20cb5
mustafa <mustafa@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-01-12 07:47:29 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@4390 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RcmdbCommons/src/groovy/com/ifountain/rcmdb/domain/util/ControllerUtils.groovy
+++ RcmdbCommons/src/groovy/com/ifountain/rcmdb/domain/util/ControllerUtils.groovy
@@ -20,7 +20,7 @@ package com.ifountain.rcmdb.domain.util
 
 import groovy.xml.MarkupBuilder
 import org.apache.commons.lang.StringUtils
-import org.codehaus.groovy.grails.web.binding.DataBindingUtils
+import org.codehaus.groovy.grails.commons.ApplicationHolder
 
 /**
  * Created by IntelliJ IDEA.
@@ -75,7 +75,7 @@ class ControllerUtils {
             }
         }
         domainObjectpropertyNames = domainObjectpropertyNames.unique ().findAll {domainClass.metaClass.getMetaProperty(it) != null}
-        DataBindingUtils.bindObjectToInstance (instance, clonedMap);
+        ApplicationHolder.getApplication().getClassLoader().loadClass("org.codehaus.groovy.grails.web.binding.DataBindingUtils").bindObjectToInstance (instance, clonedMap);
         domainObjectpropertyNames.each{
             def propValue = instance.getProperty(it);
             if(relations.contains(it) && propValue.id == -1l)

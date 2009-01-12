1845656b85106696d3b3df59aecc9bd9ac94a54d
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-01-12 09:06:31 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@4395 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidInsight/test/unit/message/RsMessageOperationsTest.groovy
+++ RapidInsight/test/unit/message/RsMessageOperationsTest.groovy
@@ -1,6 +1,9 @@
 package message
 
-import com.ifountain.rcmdb.test.util.RapidCmdbTestCase
+
+import com.ifountain.rcmdb.test.util.RapidCmdbWithCompassTestCase
+import org.apache.log4j.Logger
+import com.ifountain.rcmdb.test.util.CompassForTests
 
 /**
 * Created by IntelliJ IDEA.
@@ -9,8 +12,53 @@ import com.ifountain.rcmdb.test.util.RapidCmdbTestCase
 * Time: 9:03:57 AM
 * To change this template use File | Settings | File Templates.
 */
-class RsMessageOperationsTest extends RapidCmdbTestCase{
+class RsMessageOperationsTest extends RapidCmdbWithCompassTestCase{
 
-    
+    public void setUp() {
+        super.setUp();
+
+
+    }
+
+    public void tearDown() {
+        super.tearDown();    
+    }
+
+    public void testProcessDelayedEmails()
+    {
+        initialize([RsMessage], []);
+        CompassForTests.addOperationSupport(RsMessage,RsMessageOperations)
+
+        assertEquals(RsMessage.list().size(),0)
+        def date=new Date();
+        def delay=2000
+        def params=[:]
+        params.eventId=1
+        params.state=0
+        params.destination="xxx"
+        params.destinationType=RsMessage.EMAIL
+        params.action="create"
+        params.sendAfter=date.getTime()+delay
+
+
+        def message=RsMessage.add(params)
+        
+        if(message.hasErrors())
+        {
+            fail("Message should be added successfully")
+        }
+
+        RsMessage.processDelayedEmails(Logger.getLogger("yyy"))
+
+        def mes=RsMessage.get(id:message.id)
+        assertEquals(mes.state,0)
+        Thread.sleep(delay+1000)
+        RsMessage.processDelayedEmails(Logger.getLogger("yyy"))
+        mes=RsMessage.get(id:message.id)
+        assertEquals(mes.state,1)
+
+
+        
+    }
 
 }

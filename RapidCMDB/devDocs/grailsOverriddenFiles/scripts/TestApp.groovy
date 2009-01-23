625a5e48f09d36b0a39fc7f58083febfa9f255e3
mustafa <mustafa@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-01-23 08:50:49 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@4723 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/devDocs/grailsOverriddenFiles/scripts/TestApp.groovy
+++ RapidCMDB/devDocs/grailsOverriddenFiles/scripts/TestApp.groovy
@@ -381,7 +381,7 @@ target(runIntegrationTests: "Runs Grails' tests under the test/integration direc
 
 
                 runTests(suite, result) {test, invocation ->
-                    String name = String.valueOf(t);
+                    String name = String.valueOf(test);
                     name = name.substring(name.indexOf("(")+1)
                     name = name.substring(0, name.indexOf(")"))
                     def stripWord = ["IntegrationTest", "IntegrationTests", "Test", "Tests"];

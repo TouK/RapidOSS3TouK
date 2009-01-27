6784cfe7bc0db597b79eb505e65786e1c44bb118
mustafa <mustafa@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-01-27 07:41:35 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@4828 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/devDocs/scripts/build/RapidBrowserBuild.groovy
+++ RapidCMDB/devDocs/scripts/build/RapidBrowserBuild.groovy
@@ -46,7 +46,7 @@ class RapidBrowserBuild extends Build  {
             ant.fileset(dir: "$env.rapid_browser_svn/webapps", excludes:".svn")
         }
         ant.mkdir(dir:env.rapid_browser_build);
-        ant.javac(srcdir:"${env.rapid_browser_svn}/src", destdir:env.rapid_browser_build){
+        ant.javac(srcdir:"${env.rapid_browser_svn}/src", destdir:env.rapid_browser_build, target:"1.5"){
             ant.classpath(refid : "classpath");
         }
 

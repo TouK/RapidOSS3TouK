58bcf1bfb0beff7e11beb6107ce0364f39108ea4
pinar <pinar@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-01-28 15:05:30 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@4932 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidInsight/operations/RsTicketOperations.groovy
+++ RapidInsight/operations/RsTicketOperations.groovy
@@ -3,58 +3,39 @@
     import connector.JiraConnector;
     import datasource.JiraDatasource;
 
-    /*
-    Status codes
-    1: Open
-    3: In Progress
-    4: Reopened
-    5: Resolved
-    6: Closed
-    Priority codes
-    1: Blocker
-    2: Critical 
-    3: Major
-    4: Minor
-    5: Trivial
-    					Severity From RI: Priority in Jira
-    					case '5' : return "Critical"; => 1
-                        case '4' : return "Major"; => 2
-                        case '3' : return "Minor"; =>3
-                        case '2' : return "Warning"; =>4
-                        case '1' : return "Indeterminate"; =>5
-                        case '0' : return "Clear";
-                        
-                        
-    Issue Type codes
-    1: Bug
-    8: Comp Time
-    4: Improvement
-    2: New Feature
-    3: Task
-    6: Time Off
-    7: Vacation
-    Resolution codes:
-    1: Fixed
-    2: Won't Fix
-    3: Duplicate
-    4: Incomplete
-    5: Cannot Reproduce
-    Actions
-    5: Resolve Issue -> 701: Close Issue
-    2: Close Issue
-
-    */		
+/*
+    Status codes			Priority Codes		Severity From RI
+    1: Open					1: Blocker			5: Critical
+    3: In Progress			2: Critical 		4: Major
+    4: Reopened				3: Major			3: Minor
+    5: Resolved				4: Minor			2: Warning
+    6: Closed				5: Trivial			1: Indeterminate 
+    											0: Clear (NOT MAPPED IN JIRA)
+    Issue Type codes		Resolution codes	Actions
+    1: Bug					1: Fixed			5: Resolve Issue -> 701: Close Issue
+    2: New Feature			2: Won't Fix		2: Close Issue
+    3: Task					3: Duplicate
+    4: Improvement			4: Incomplete
+    						5: Cannot Reproduce
+*/		
     public class RsTicketOperations extends com.ifountain.rcmdb.domain.operation.AbstractDomainOperation {
     	
-    	static createTicket(Map ticketProps){ 
+    	static openTicket(Map ticketProps){ 
+   		// project, type and summary are required to open a ticket.
+   		// project is hard coded as DEMO in this method.
+   		// summary is formed from the event description
+   		// type is assumed to be provided in the ticketProps as we need to support subtasks.  
+    		def severityToPriority = ["5":"1","4":"2","3":"3","2":"4","1":"5"];
     		def connectorName = ticketProps.connectorName;
     		if (connectorName!=null && ticketProps.eventName!=null){
     			def jiraConnector = JiraConnector.get(name:connectorName);
     			if (jiraConnector!=null){
     				def event = RsEvent.get(name:ticketProps.eventName);
     				def jiraDs = jiraConnector.ds;
-    			
     				// Run the create issue code in Jira
+    				ticketProps.project = "DEMO";
+    				ticketProps.summary = event.description;
+    				ticketProps.priority = severityToPriority.get(event.severity.toString())
     				def returnedIssue = jiraDs.openIssue(ticketProps);
     				returnedIssue.rsDatasource = jiraDs.name;
     				def ticket = RsTicket.add(returnedIssue);
@@ -70,7 +51,14 @@
     			}
     		}
     	}
-
+    	
+    	public resolveTicket(){
+    		def jiraDs = JiraDatasource.get(name:rsDatasource);
+    		jiraDs.resolveIssue(name, "1"); // Default resolution is "Fixed"
+    		
+    		status = "5"; //RESOLVED;
+    	}
+    	
     	public closeTicket(String eventName, String elementName){
     		def jiraDs = JiraDatasource.get(name:rsDatasource);
     		jiraDs.closeIssue(name, "1"); // Default resolution is "Fixed"
@@ -85,10 +73,19 @@
     		status = "6"; //CLOSED;
     	}
     	
+    	public updateTicket(Map props){
+    		def jiraDs = JiraDatasource.get(name:rsDatasource);
+    		jiraDs.updateIssue(name, props); 
+    	}
+    	
     	public retrieveDetails(){
     		def jiraDs = JiraDatasource.get(name:rsDatasource);
     	    return jiraDs.retrieveDetails(name);
     	}
 
+    	public addToLog(String comment){
+    		def jiraDs = JiraDatasource.get(name:rsDatasource);
+    	    return jiraDs.addComment(name, comment);
+    	}
     }
         

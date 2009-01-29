6ee9d91a699af0f3122b4ebcfec7532b328a129a
pinar <pinar@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-01-29 12:22:00 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@4950 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

---
+++ RapidCMDB/operations/datasource/JiraDatasourceOperations.groovy
@@ -0,0 +1,47 @@
+package datasource
+
+import datasource.JiraAdapter
+import org.apache.log4j.Logger
+
+class JiraDatasourceOperations extends BaseDatasourceOperations {
+    def adapter;
+    def onLoad() {
+        this.adapter = new JiraAdapter(getProperty("connection").name, reconnectInterval * 1000, Logger.getRootLogger());
+    }
+
+    def getProperty(Map keys, String propName){
+        def props = adapter.getProps(keys.name, [propName]);
+        return convert(props[propName]);
+    }
+
+    def getProperties(Map keys, List properties){
+       def props = adapter.getProps(keys.name, properties);
+       return convert(props);
+    }
+    
+    def openIssue(Map params) {
+    	def issue = this.adapter.openIssue(params)
+        return issue;
+    }
+
+    def closeIssue(String issueId, String resolution) {
+    	def issue = this.adapter.closeIssue(issueId, resolution)
+    }
+    
+    def retrieveDetails(String issueId) {
+    	def issue = this.adapter.retrieveDetails(issueId)
+    	return issue;
+    }
+    
+    def resolveIssue(String issueId, String resolution) {
+    	def issue = this.adapter.resolveIssue(issueId, resolution)
+    }
+    
+    def updateIssue(String issueId, Map params) {
+    	def issue = this.adapter.updateIssue(issueId, params)
+    }
+
+    def addComment(String issueId, String comment) {
+    	def issue = this.adapter.addCommentToIssue(issueId, comment)
+    }
+}

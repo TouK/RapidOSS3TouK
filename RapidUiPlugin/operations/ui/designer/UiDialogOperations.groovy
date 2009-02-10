e6680299a425f786072145cb59f64b665e2db500
sezgin <sezgin@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-02-10 09:04:49 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@5196 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidUiPlugin/operations/ui/designer/UiDialogOperations.groovy
+++ RapidUiPlugin/operations/ui/designer/UiDialogOperations.groovy
@@ -29,6 +29,7 @@ class UiDialogOperations extends UiLayoutUnitOperations{
                     minHeight:[descr:"Minimum height of the dialog"],
                     minWidth:[descr:"Minimum width of the dialog"],
                     maxWidth:[descr:"Maximum width of the dialog"],
+                    resizable:[descr:"Boolean value to represent whether the dialog is resizable or not."],
                     component:[descr:"RapidInsight component that will be displayed as pop up dialog", formatter:{object-> return object.component?object.component.name:""}]
                 ],
                 childrenConfiguration:[]

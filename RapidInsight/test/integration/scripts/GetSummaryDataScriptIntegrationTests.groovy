f9b5b0d1bcbd79799d30eaab5edfd91425240d53
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-02-11 16:26:59 +0000

    Missing Test Implemented
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@5303 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

---
+++ RapidInsight/test/integration/scripts/GetSummaryDataScriptIntegrationTests.groovy
@@ -0,0 +1,223 @@
+package scripts
+
+import com.ifountain.rcmdb.test.util.RapidCmdbIntegrationTestCase
+import script.CmdbScript
+
+import connection.EmailConnection
+import datasource.EmailDatasource
+import connector.EmailConnectorController
+import connector.EmailConnector
+import connection.Connection
+import datasource.BaseDatasource
+import com.ifountain.rcmdb.scripting.ScriptManager
+import org.apache.log4j.Logger
+
+import script.ScriptController
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: admin
+ * Date: Jan 13, 2009
+ * Time: 3:12:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+
+// Warning :  this test assumes that getSummaryData from RapidInsight is in the scripts directory
+// TODO : another version must be written for Smarts getSummaryData
+class GetSummaryDataScriptIntegrationTests extends RapidCmdbIntegrationTestCase {
+    static transactional = false;
+    def classes=[:]
+
+     static String CRITICAL = "Critical";
+     static String MAJOR = "Major";
+     static String MINOR = "Minor";
+     static String WARNING = "Warning";
+     static String INDETERMINATE = "Indeterminate";
+     static String NORMAL = "Normal";
+     static String INVALID = "invalid";
+
+
+     static severityMap=[:];
+
+    void setUp() throws Exception {
+        super.setUp();
+        loadClasses(["RsEvent"])
+        clearAll();
+
+        severityMap[CRITICAL]=5;
+        severityMap[MAJOR]=4;
+        severityMap[MINOR]=3;
+        severityMap[WARNING]=2;
+        severityMap[INDETERMINATE]=1;
+        severityMap[NORMAL]=0;
+        severityMap[INVALID]=50;
+    }
+
+    void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+
+    void clearAll()
+    {
+
+        classes.RsEvent.removeAll();
+
+        CmdbScript.list().each{
+            CmdbScript.deleteScript(it);
+        }
+    }
+     void loadClasses(classList)
+    {
+        classList.each{
+            def loadedClass=this.class.classLoader.loadClass(it)
+            classes[loadedClass.getSimpleName()]=loadedClass
+        }
+    }
+    void testSummaryDataWithNoEvent(){
+        def summaryScript=CmdbScript.addScript([name:"getSummaryData"])
+        assertFalse(summaryScript.hasErrors());
+
+        assertEquals(classes.RsEvent.list().size(),0)
+
+        def params=[name:"TESTDS",nodeType:"Container"]
+        def setMap=getSummaryDataMap(params);
+
+
+        assertEquals(setMap.size(),6);
+        assertEquals(setMap[NORMAL],1);
+        setMap.each{ key , count ->
+        	if(key != NORMAL )
+        	 	assertEquals(count,0)
+        }
+
+    }
+    void testSummaryDataWithInvalidSeverityEvents(){
+        def summaryScript=CmdbScript.addScript([name:"getSummaryData"])
+        assertFalse(summaryScript.hasErrors());
+
+        assertEquals(classes.RsEvent.list().size(),0)
+
+
+        def eventCounts=[:]
+
+        eventCounts[CRITICAL]=5
+        eventCounts[MAJOR]=4
+        eventCounts[MINOR]=1
+        eventCounts[WARNING]=3
+        eventCounts[INDETERMINATE]=7
+        eventCounts[NORMAL]=2
+        eventCounts[INVALID]=10
+
+        eventCounts.each { severity , count ->
+            def severityVal=severityMap[severity];
+
+            count.times{
+                classes.RsEvent.add(name:"event${severity}${it}",rsDatasource:"TESTDS",severity:severityVal)
+            }
+
+        }
+        def totalEventCount=classes.RsEvent.list().size();
+
+
+
+        def params=[name:"TESTDS",nodeType:"Container"]
+        def setMap=getSummaryDataMap(params);
+
+
+
+        assertEquals(setMap.size(),6);
+        def totalEventCountFromSummary=0;
+        assertEquals(setMap[NORMAL],eventCounts[NORMAL]+eventCounts[INVALID])
+
+        eventCounts.each { severity , count ->
+            if(severity!=NORMAL && severity!=INVALID)
+            {
+        	    assertEquals(setMap[severity],count);
+            }
+        	totalEventCountFromSummary+=count;
+        }
+
+
+        assertEquals(totalEventCount,totalEventCountFromSummary);
+
+
+    }
+     void testSummaryDataWithEvents(){
+        def summaryScript=CmdbScript.addScript([name:"getSummaryData"])
+        assertFalse(summaryScript.hasErrors());
+
+        assertEquals(classes.RsEvent.list().size(),0)
+
+
+        def eventCounts=[:]
+
+        eventCounts[CRITICAL]=5
+        eventCounts[MAJOR]=4
+        eventCounts[MINOR]=1
+        eventCounts[WARNING]=3
+        eventCounts[INDETERMINATE]=7
+        eventCounts[NORMAL]=2
+
+        eventCounts.each { severity , count ->
+            def severityVal=severityMap[severity];
+
+            count.times{
+                classes.RsEvent.add(name:"event${severity}${it}",rsDatasource:"TESTDS",severity:severityVal)
+            }
+
+        }
+        def totalEventCount=classes.RsEvent.list().size();
+
+
+
+        def params=[name:"TESTDS",nodeType:"Container"]
+        def setMap=getSummaryDataMap(params);
+
+
+
+        assertEquals(setMap.size(),6);
+        def totalEventCountFromSummary=0;
+        eventCounts.each { severity , count ->
+        	assertEquals(setMap[severity],count);
+        	totalEventCountFromSummary+=count;
+        }
+
+        assertEquals(totalEventCount,totalEventCountFromSummary);
+
+
+    }
+    def getSummaryDataMap(params){
+        params.id="getSummaryData";
+        def controller=runScriptViaController(params);
+
+        def resultXml = new XmlSlurper().parseText(controller.response.contentAsString);
+        def sets=resultXml.set;
+        assertEquals(sets.size(),6);
+
+        def setMap=[:]
+        sets.each {
+            setMap[it.@label.toString()]=it.@value.toInteger();
+        }
+        println "map result parsed from xml ${setMap}"
+        return setMap;
+    }
+
+    def runScriptViaController(externalParams){
+        def params=[:]
+
+        params.putAll(externalParams);
+
+
+        def controller = new ScriptController();
+        params.each{ key , val ->
+            controller.params[key] = val;
+        }
+        controller.run();
+        return controller;
+    }
+
+
+}

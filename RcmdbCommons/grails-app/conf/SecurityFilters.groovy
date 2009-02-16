c313ef5de6db3195de84fa4b6fad29f79a159334
mustafa <mustafa@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-02-16 12:03:14 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@5364 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RcmdbCommons/grails-app/conf/SecurityFilters.groovy
+++ RcmdbCommons/grails-app/conf/SecurityFilters.groovy
@@ -28,13 +28,8 @@ class SecurityFilters {
     def filters = {
         // Ensure that all controllers and actions require an authenticated user,
         // except for the "public" controller
-        authentication(controller: "*", action: "*") {
+        authentication(uri: "/**") {
             before = {
-                // Exclude the "public" controller.
-                if (controllerName == "public") return true
-
-                // This just means that the user must be authenticated. He does
-                // not need any particular role or permission.
                 return accessControl {
                     return role(Role.ADMINISTRATOR) || role(Role.USER)
                 }

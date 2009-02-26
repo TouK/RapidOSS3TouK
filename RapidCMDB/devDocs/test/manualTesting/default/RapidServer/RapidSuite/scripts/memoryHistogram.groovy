72c9bb1c7b9986bc9b7acbe6d4272598be17a65e
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-02-26 12:54:29 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@5605 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/devDocs/test/manualTesting/default/RapidServer/RapidSuite/scripts/memoryHistogram.groovy
+++ RapidCMDB/devDocs/test/manualTesting/default/RapidServer/RapidSuite/scripts/memoryHistogram.groovy
@@ -19,9 +19,10 @@
 import utils.TestingConstants
 
 def pid=java.lang.management.ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
-def javaDir = "C:/Program Files/Java/jdk1.6.0_04/bin/";
-def cmd = "${javaDir}jmap -histo ${pid}"
+def javaDir = System.getenv("JAVA_HOME")+"/bin/";
+def cmd = "\"${javaDir}jmap\" -histo ${pid}"
 
 
 def process = "${cmd}".execute()
-TestingConstants.getHeapHistogramFile().append(process.in.text);
+def file=new File("web-app/${params.fileName}.log");
+file.setText(process.in.text);

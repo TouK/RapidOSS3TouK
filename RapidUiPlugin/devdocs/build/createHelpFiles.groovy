755be5a5407f0cfc5681586ec74ae01c15da083f
sezgin <sezgin@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-03-06 12:20:08 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@5799 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidUiPlugin/devdocs/build/createHelpFiles.groovy
+++ RapidUiPlugin/devdocs/build/createHelpFiles.groovy
@@ -1,11 +1,15 @@
-def inputDir = new File("c:/helpFiles");
-def outputDir = new File("c:/helpFilesOut");
+def inputDir = new File("C:\\Documents and Settings\\admin\\Desktop\\designerdoc");
+def outputDir = new File("C:\\Documents and Settings\\admin\\Desktop\\outdoc");
 def static final String HELP_START_TAG = "<p><!-- DesignerBegin --></p>";
+def static final String HELP_ANOTHER_START_TAG = "<p><!-- DesignerBegin --><br/>";
 def static final String HELP_END_TAG = "<p><!-- DesignerEnd --></p>";
 outputDir.mkdirs();
 inputDir.listFiles().each{File helpFile->
     def helpFileContent = helpFile.getText();
     def startIndex = helpFileContent.indexOf(HELP_START_TAG);
+    if(startIndex < 0){
+        startIndex = helpFileContent.indexOf(HELP_ANOTHER_START_TAG);
+    }
     def endIndex = helpFileContent.indexOf(HELP_END_TAG);
     if(startIndex != -1 && endIndex != -1)
     {

3f806eb8d00c173e059703f9fa9ed615ef88f5af
tugrul <tugrul@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-03-16 18:17:26 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@5899 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidInsight/operations/Constants.groovy
+++ RapidInsight/operations/Constants.groovy
@@ -1,6 +1,8 @@
- static final int CRITICAL = 5
- static final int MAJOR = 4
- static final int INDETERMINATE = 1
- static final int NOTSET = -1
- static final int CRITICAL_PERCENTAGE = 40
- static final int MAJOR_PERCENTAGE = 20
+public class Constants{
+	 public static final int CRITICAL = 5
+	 public static final int MAJOR = 4
+	 public static final int INDETERMINATE = 1
+	 public static final int NOTSET = -1
+	 public static final int CRITICAL_PERCENTAGE = 40
+	 public static final int MAJOR_PERCENTAGE = 20
+}

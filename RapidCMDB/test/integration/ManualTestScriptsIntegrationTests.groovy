919e173dd5be64459252e2a241b7c426eb62aaa2
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-03-25 09:11:54 +0000

    CMDB-1190 - Convert manual implemented tests to automated ( ModelTest, AllDatasourceTest)
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@6026 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/test/integration/ManualTestScriptsIntegrationTests.groovy
+++ RapidCMDB/test/integration/ManualTestScriptsIntegrationTests.groovy
@@ -21,39 +21,31 @@ import com.ifountain.rcmdb.test.util.RapidCmdbIntegrationTestCase
 class ManualTestScriptIntegrationTests extends RapidCmdbIntegrationTestCase
 {
     static transactional = false;
-    /*
 
-    def base_directory="";
     public void setUp() {
         super.setUp();
-          //to run in Hudson
-        base_directory = "../RapidSuite";
-        //def canonicalPath=new File(System.getProperty("base.dir", ".")).getCanonicalPath();
-        def canonicalPath=new File(".").getCanonicalPath();
-        //to run in developer pc
-        if(canonicalPath.endsWith("RapidModules"))
-        {
-            base_directory = "RapidCMDB";
-        }
-        initializeScriptManager();
     }
 
     public void tearDown() {
         super.tearDown();
 
     }
-    void initializeScriptManager()
+    void copyScript(scriptName)
     {
-        def script_base_directory= base_directory+"/test/manualTestScripts";
-        println "script base path is :"+new File(script_base_directory).getCanonicalPath();
 
-        ScriptManager manager = ScriptManager.getInstance();
-        manager.initialize(this.class.getClassLoader(), script_base_directory, [], [:]);
+        def scriptPath= "test/manualTestScripts/scripts/${scriptName}.groovy";
+
+        def ant=new AntBuilder();
+
+        ant.copy(file: scriptPath, toDir: "scripts",overwrite:true);
+        
     }
 
     public void testAllDatasourceTest()
     {
-        def script=CmdbScript.addScript([name:"AllDatasourceTest",scriptFile:"AllDatasourceTest.groovy",type: CmdbScript.ONDEMAND],true)
+        def scriptName="AllDatasourceTest";
+        copyScript(scriptName);
+        def script=CmdbScript.addScript([name:scriptName,type: CmdbScript.ONDEMAND],true)
         println script.errors
         assertFalse(script.hasErrors());
 
@@ -66,6 +58,6 @@ class ManualTestScriptIntegrationTests extends RapidCmdbIntegrationTestCase
             fail("Error in script. Reason ${e}");
         }
     }
-   */
+
 
 }

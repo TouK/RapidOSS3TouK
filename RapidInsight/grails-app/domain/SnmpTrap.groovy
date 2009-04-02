79f1b8401257f9caabdaabc0069cac24b879fa6e
sezgin <sezgin@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-04-02 15:27:54 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@6180 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

---
+++ RapidInsight/grails-app/domain/SnmpTrap.groovy
@@ -0,0 +1,72 @@
+
+import com.ifountain.core.domain.annotations.*;
+
+class SnmpTrap 
+{
+    
+    //AUTO_GENERATED_CODE
+
+    static searchable = {
+        except = ["errors", "__operation_class__", "__is_federated_properties_loaded__"];
+    
+    
+    };
+    static datasources = ["RCMDB":["keys":["name":["nameInDs":"name"]]]]
+
+    
+    String name ="";
+    
+    Long trapVersion =0;
+    
+    String destination ="";
+    
+    Long port =162;
+    
+    String community ="public";
+    
+    org.springframework.validation.Errors errors ;
+    
+    Long id ;
+    
+    Long version ;
+    
+    Object __operation_class__ ;
+    
+    Object __is_federated_properties_loaded__ ;
+    
+    
+    static relations = [:]    
+    
+    static constraints={
+    name(blank:false,nullable:false,key:[])
+        
+     trapVersion(nullable:true)
+        
+     destination(blank:true,nullable:true)
+        
+     port(nullable:true)
+        
+     community(blank:true,nullable:true)
+        
+     errors(nullable:true)
+        
+     __operation_class__(nullable:true)
+        
+     __is_federated_properties_loaded__(nullable:true)
+        
+     
+    }
+
+    static propertyConfiguration= [:]
+    static transients = ["errors", "__operation_class__", "__is_federated_properties_loaded__"];
+    
+    public String toString()
+    {
+    	return "${getClass().getName()}[name:${getProperty("name")}]";
+    }
+    
+    public boolean equals(Object obj) {
+        return obj.id == this.id;
+    }
+    //AUTO_GENERATED_CODE
+}
---
+++ RapidInsight/operations/SnmpTrapOperations.groovy
@@ -0,0 +1,20 @@
+import snmp.SnmpUtils
+
+public class SnmpTrapOperations extends com.ifountain.rcmdb.domain.operation.AbstractDomainOperation
+{
+
+    def send(Map trapProps){
+        def transportAddress = "${destination}/${port}"
+        def varbinds = trapProps["varbinds"] != null ? trapProps["varbinds"]: [];
+        def timestamp = trapProps["timestamp"] != null ? trapProps["timestamp"] : 0
+        def trapCommunity = trapProps["community"] != null ? trapProps["community"] : community;
+        if(trapVersion == SnmpUtils.VERSION_1){
+            return SnmpUtils.sendV1Trap(transportAddress, trapProps["agent"], trapCommunity, trapProps["enterprise"], timestamp, trapProps["generic"], trapProps["specific"], varbinds)
+        }
+        else{
+            return SnmpUtils.sendV2cTrap(transportAddress, trapProps["agent"], trapCommunity, timestamp,  trapProps["trapOid"], varbinds)
+        }
+
+    }
+}
+    

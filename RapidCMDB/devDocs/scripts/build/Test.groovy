c93f1000ee49829bb172176deb348dadc0f418de
sezgin <sezgin@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-04-16 15:19:34 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@6382 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/devDocs/scripts/build/Test.groovy
+++ RapidCMDB/devDocs/scripts/build/Test.groovy
@@ -18,80 +18,63 @@
 */
 package build;
 
-class Test extends Parent{
-	
-	void run(args){
-		if(args.size() >0){
-			println "Running target " + args[0];
-			invokeMethod(args[0], null);
-		}
-		else{
-			println "Running default target";
-			runAllTests();
-		}
-		println "Done";
-	}
-	
-	def runTests(String testClass, String outputXmlDir, String outputXmlFile, String htmlDir){
-		ant.echo(message : "Running all tests for test class " + testClass + " and will output xml results to " + outputXmlDir + "/" + outputXmlFile);
-		ant.mkdir(dir :  outputXmlDir);
-		ant.junit(printsummary : "yes", haltonfailure : "no", fork : "yes", showoutput : "true"){
-			ant.classpath(refid : "testexecutionclasspath");
-			ant.jvmarg(value : "-Xmx512m");
-			ant.jvmarg(value : "-DWORKSPACE=.");
-			ant.formatter(type :  "xml");
-			ant.test(name : testClass, haltonfailure : "no",  outfile : outputXmlDir + "/" + outputXmlFile){
-				ant.formatter(type : "xml");
-			}
-		}
-//        def testSuiteString = "</testsuite>";
-//        def xmlFiles = new File(outputXmlDir).listFiles();
-//        xmlFiles.each{File f->
-//            if(f.getName().endsWith(".xml"))
-//            {
-//                def lines = f.readLines();
-//                def strBuf = new StringBuffer();
-//                lines.each{String line->
-//                    if(line.indexOf("</testsuite>") < 0)
-//                    {
-//                        strBuf.append (line).append("\n");
-//                    }
-//                    else
-//                    {
-//                        strBuf.append (line.substring(0, line.indexOf(testSuiteString)+testSuiteString.length())).append("\n");
-//                        return;
-//                    }
-//                }
-//                println "new content for ${f.getCanonicalPath()} is ${strBuf.toString()}"
-//                f.setText(strBuf.toString());
-//            }
-//        }
-//		ant.mkdir(dir :  htmlDir);
-//		ant.junitreport(todir : htmlDir ){
-//			ant.fileset(dir : outputXmlDir){
-//				ant.include(name : "*.xml");
-//			}
-//			ant.report(format : "frames", todir : htmlDir);
-//		}
-	}
-	
-	def Properties getLibJars(){
-		Properties libJars = new Properties();
-		File file1 = new File(env.dist_rapid_suite_lib);
-		File file2 = new File(env.dist_rapid_server_lib);
-		env.getLibProperties(libJars, file1);
-		env.getLibProperties(libJars, file2);
-		return libJars;
-	}
-	
-	def addProductionLibJarsToClasspath(){
-		Properties libJars = getLibJars();
-		Enumeration jarKeys = libJars.keys();
-		ant.path(id : "libJars"){
-			while(jarKeys.hasMoreElements()){
+class Test extends Parent {
+
+    void run(args) {
+        if (args.size() > 0) {
+            println "Running target " + args[0];
+            invokeMethod(args[0], null);
+        }
+        else {
+            println "Running default target";
+            runAllTests();
+        }
+        println "Done";
+    }
+
+    def runTests(String testClass, String outputXmlDir, String outputXmlFile, String htmlDir) {
+        ant.echo(message: "Running all tests for test class " + testClass + " and will output xml results to " + outputXmlDir + "/" + outputXmlFile);
+        ant.mkdir(dir: outputXmlDir);
+        ant.junit(printsummary: "yes", haltonfailure: "no", fork: "yes", showoutput: "true") {
+            ant.classpath(refid: "testexecutionclasspath");
+            ant.jvmarg(value: "-Xmx512m");
+            ant.jvmarg(value: "-DWORKSPACE=.");
+            ant.formatter(type: "xml");
+            ant.test(name: testClass, haltonfailure: "no", outfile: outputXmlDir + "/" + outputXmlFile) {
+                ant.formatter(type: "xml");
+            }
+        }
+        def testResultFile = new File("${outputXmlDir}/${outputXmlFile}");
+        def xmlText = testResultFile.getText();
+        try {
+            new XmlSlurper().parseText(xmlText)
+        }
+        catch (e) {
+            def testSuiteString = "</testsuite>"
+            if (xmlText.indexOf(testSuiteString)) {
+                xmlText = xmlText.substring(0, xmlText.indexOf(testSuiteString) + testSuiteString.length());
+                testResultFile.setText(xmlText);
+            }
+        }
+    }
+
+    def Properties getLibJars() {
+        Properties libJars = new Properties();
+        File file1 = new File(env.dist_rapid_suite_lib);
+        File file2 = new File(env.dist_rapid_server_lib);
+        env.getLibProperties(libJars, file1);
+        env.getLibProperties(libJars, file2);
+        return libJars;
+    }
+
+    def addProductionLibJarsToClasspath() {
+        Properties libJars = getLibJars();
+        Enumeration jarKeys = libJars.keys();
+        ant.path(id: "libJars") {
+            while (jarKeys.hasMoreElements()) {
                 String jarName = (String) jarKeys.nextElement();
-				ant.pathelement(location : libJars.getProperty(jarName));
-			}
-		}
-	}	
+                ant.pathelement(location: libJars.getProperty(jarName));
+            }
+        }
+    }
 }

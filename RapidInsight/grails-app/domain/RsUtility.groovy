d003cee9233e01e22de686561c1b87fe9db3c5aa
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-04-21 14:21:59 +0000

    CMDB-1245 - Configuration for state calculation mechanism ( simply move all state operations to RsTopologyObjectOperations) OR  ( with configuration will prevent duplicate operations file , will be easy to change , no need to modify multiple places  )
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@6397 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

---
+++ RapidInsight/grails-app/domain/RsUtility.groovy
@@ -0,0 +1,65 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: admin
+ * Date: Apr 16, 2009
+ * Time: 2:37:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+import com.ifountain.core.domain.annotations.*;
+
+class RsUtility {
+
+    //AUTO_GENERATED_CODE
+
+    static searchable = {
+        except = ["errors", "__operation_class__", "__is_federated_properties_loaded__"];
+
+
+    };
+    static datasources = ["RCMDB":["keys":["name":["nameInDs":"name"]]]]
+
+
+    String name ="";
+
+    String value ="";
+
+    org.springframework.validation.Errors errors ;
+
+    Long id ;
+
+    Long version ;
+
+    Object __operation_class__ ;
+
+    Object __is_federated_properties_loaded__ ;
+
+
+    static relations = [:]
+
+    static constraints={
+    name(blank:false,nullable:false,key:[])
+
+     value(blank:true,nullable:true)
+
+     errors(nullable:true)
+
+     __operation_class__(nullable:true)
+
+     __is_federated_properties_loaded__(nullable:true)
+
+
+    }
+
+    static propertyConfiguration= [:]
+    static transients = ["errors", "__operation_class__", "__is_federated_properties_loaded__"];
+
+    public String toString()
+    {
+    	return "${getClass().getName()}[name:${getProperty("name")}]";
+    }
+
+    public boolean equals(Object obj) {
+        return obj.id == this.id;
+    }
+    //AUTO_GENERATED_CODE
+}

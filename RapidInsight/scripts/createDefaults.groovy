1bb93c9c14b7d787267b78aaab1adc8eff07edb2
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-05-05 07:40:45 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@6583 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidInsight/scripts/createDefaults.groovy
+++ RapidInsight/scripts/createDefaults.groovy
@@ -71,7 +71,7 @@ scriptsToAdd.each{  scriptParams ->
     }
     catch(e)
     {
-       logger.warn("createDefaults: Could not add script with params : ${scriptParams}.Reason:${e}",e)
+       logger.warn("createDefaults: Could not add script with params : ${scriptParams}.Reason:${e}")
     }
 }
 

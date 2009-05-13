3a7a0fefabaa8f5b90f851cb65232f7fc292afed
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-05-13 11:28:24 +0000

    CMDB-1373 - fullExport / fullImport
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@6663 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/scripts/fullExportConfiguration.groovy
+++ RapidCMDB/scripts/fullExportConfiguration.groovy
@@ -1,20 +1,3 @@
-def getExportConfiguration()
-{
-    def CONFIG=[:];
-    CONFIG.backupDir="backup";
-    CONFIG.exportDir="export";
-    CONFIG.objectPerFile=1000;
-
-    CONFIG.MODELS=[];
-    CONFIG.MODELS.add([model:"all"]);
-    //CONFIG.MODELS.add([model:"conf"]);
-    //CONFIG.MODELS.add([model:"RsTopologyObject"]);
-    //CONFIG.MODELS.add([model:"RsGroup",childModels:false]);
-    //CONFIG.MODELS.add([model:"auth.RsUser"]);
-
-    return CONFIG;
-}
-
 def getImportConfiguration()
 {
    def CONFIG=[:];

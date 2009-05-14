5a0c2a81e3ddfe27792db7e7afbba7826a1e254a
sezgin <sezgin@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-05-14 07:39:28 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@6676 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidInsight/test/manualTestScripts/stateCalculation/findMaxTest.groovy
+++ RapidInsight/test/manualTestScripts/stateCalculation/findMaxTest.groovy
@@ -1,3 +1,6 @@
+import com.ifountain.comp.utils.SmartWait
+import com.ifountain.rcmdb.util.ClosureWaitAction
+
 logger.warn("Starting..............")
 
 // RI- FINDMAX
@@ -19,33 +22,34 @@ RsCustomer.removeAll()
 RsService.removeAll()
 RsTopologyObject.removeAll()
 RsEvent.removeAll()
+RsObjectState.removeAll();
 
 
-def cust1 = RsCustomer.add([name:"Cust1"])
-def cust2 = RsCustomer.add([name:"Cust2"])
+def cust1 = RsCustomer.add([name: "Cust1"])
+def cust2 = RsCustomer.add([name: "Cust2"])
 
-def serv11 = RsService.add([name:"Service11"])
-def serv12 = RsService.add([name:"Service12"])
-def serv21 = RsService.add([name:"Service21"])
-def serv22 = RsService.add([name:"Service22"])
+def serv11 = RsService.add([name: "Service11"])
+def serv12 = RsService.add([name: "Service12"])
+def serv21 = RsService.add([name: "Service21"])
+def serv22 = RsService.add([name: "Service22"])
 
-def device111 = RsTopologyObject.add([name:"Device111"])
-def device112 = RsTopologyObject.add([name:"Device112"])
-def device121 = RsTopologyObject.add([name:"Device121"])
-def device122 = RsTopologyObject.add([name:"Device122"])
-def device211 = RsTopologyObject.add([name:"Device211"])
-def device212 = RsTopologyObject.add([name:"Device212"])
-def device221 = RsTopologyObject.add([name:"Device221"])
-def device222 = RsTopologyObject.add([name:"Device222"])
+def device111 = RsTopologyObject.add([name: "Device111"])
+def device112 = RsTopologyObject.add([name: "Device112"])
+def device121 = RsTopologyObject.add([name: "Device121"])
+def device122 = RsTopologyObject.add([name: "Device122"])
+def device211 = RsTopologyObject.add([name: "Device211"])
+def device212 = RsTopologyObject.add([name: "Device212"])
+def device221 = RsTopologyObject.add([name: "Device221"])
+def device222 = RsTopologyObject.add([name: "Device222"])
 
 
-cust1.addRelation(childObjects:[serv11,serv12])
-cust2.addRelation(childObjects:[serv21,serv22])
+cust1.addRelation(childObjects: [serv11, serv12])
+cust2.addRelation(childObjects: [serv21, serv22])
 
-serv11.addRelation(childObjects:[device111,device112])
-serv12.addRelation(childObjects:[device121,device122])
-serv21.addRelation(childObjects:[device211,device212])
-serv22.addRelation(childObjects:[device221,device222])
+serv11.addRelation(childObjects: [device111, device112])
+serv12.addRelation(childObjects: [device121, device122])
+serv21.addRelation(childObjects: [device211, device212])
+serv22.addRelation(childObjects: [device221, device222])
 
 
 
@@ -54,65 +58,89 @@ assert serv11.currentState() == NOTSET
 assert cust1.currentState() == NOTSET
 
 
-def event1111 = RsEvent.add(name:"Event1111", elementName:device111.name, severity:NORMAL)
+def event1111 = RsEvent.add(name: "Event1111", elementName: device111.name, severity: NORMAL)
 
 
-assert device111.currentState() == NORMAL
-assert serv11.currentState() == NORMAL
-assert cust1.currentState() == NORMAL
-
-def event1112 = RsEvent.add(name:"Event1112", elementName:device111.name,severity:MAJOR)
-
-assert device111.currentState() == MAJOR
-assert serv11.currentState() == MAJOR
-assert cust1.currentState() == MAJOR
+SmartWait.waitFor(new ClosureWaitAction({
+    assert device111.currentState() == NORMAL
+    assert serv11.currentState() == NORMAL
+    assert cust1.currentState() == NORMAL
+}))
 
 
+def event1112 = RsEvent.add(name: "Event1112", elementName: device111.name, severity: MAJOR)
+SmartWait.waitFor(new ClosureWaitAction({
+    assert device111.currentState() == MAJOR
+    assert serv11.currentState() == MAJOR
+    assert cust1.currentState() == MAJOR
+}))
 
-def event1113 = RsEvent.add(name:"Event1113", elementName:device111.name,severity:CRITICAL)
+
+
+
+def event1113 = RsEvent.add(name: "Event1113", elementName: device111.name, severity: CRITICAL)
 assert !event1113.hasErrors()
 
-assert device111.currentState() == CRITICAL
-assert serv11.currentState() == CRITICAL
-assert cust1.currentState() == CRITICAL
+SmartWait.waitFor(new ClosureWaitAction({
+    assert device111.currentState() == CRITICAL
+    assert serv11.currentState() == CRITICAL
+    assert cust1.currentState() == CRITICAL
+}))
 
 event1112.severity = CRITICAL
 assert !event1112.hasErrors()
 
-assert device111.currentState() == CRITICAL
-assert serv11.currentState() == CRITICAL
-assert cust1.currentState() == CRITICAL
+SmartWait.waitFor(new ClosureWaitAction({
+    assert device111.currentState() == CRITICAL
+    assert serv11.currentState() == CRITICAL
+    assert cust1.currentState() == CRITICAL
+}))
 
 event1112.severity = NORMAL
 assert !event1112.hasErrors()
+SmartWait.waitFor(new ClosureWaitAction({
+    assert device111.currentState() == CRITICAL
+    assert serv11.currentState() == CRITICAL
+    assert cust1.currentState() == CRITICAL
+}))
 
-assert device111.currentState() == CRITICAL
-assert serv11.currentState() == CRITICAL
-assert cust1.currentState() == CRITICAL
 
 event1113.severity = NORMAL
 assert !event1113.hasErrors()
+SmartWait.waitFor(new ClosureWaitAction({
+    assert device111.currentState() == NORMAL
+    assert serv11.currentState() == NORMAL
+    assert cust1.currentState() == NORMAL
+}))
 
-assert device111.currentState() == NORMAL
-assert serv11.currentState() == NORMAL
-assert cust1.currentState() == NORMAL
 
-def event1221 = RsEvent.add(name:"Event1221", elementName:device122.name,severity:WARNING)
+def event1221 = RsEvent.add(name: "Event1221", elementName: device122.name, severity: WARNING)
+SmartWait.waitFor(new ClosureWaitAction({
+    assert device122.currentState() == WARNING
+    assert serv12.currentState() == WARNING
+    assert cust1.currentState() == WARNING
+}))
 
-assert device122.currentState() == WARNING
-assert serv12.currentState() == WARNING
-assert cust1.currentState() == WARNING
 
 event1221.clear()
-assert device122.currentState() == NORMAL
-assert serv12.currentState() == NORMAL
-assert cust1.currentState() == NORMAL
+SmartWait.waitFor(new ClosureWaitAction({
+    assert device122.currentState() == NORMAL
+    assert serv12.currentState() == NORMAL
+    assert cust1.currentState() == NORMAL
+}))
 
 
 //test RsObjectState removed when object is removed
-def objectId=device122.id;
-assert RsObjectState.get(objectId:objectId).state == NORMAL
+def objectId = device122.id;
+SmartWait.waitFor(new ClosureWaitAction({
+    assert RsObjectState.get(objectId: objectId).state == NORMAL
+}));
+
 device122.remove();
-assert RsObjectState.get(objectId:objectId)==null
+
+SmartWait.waitFor(new ClosureWaitAction({
+    assert RsObjectState.get(objectId: objectId) == null
+}));
+
 
 return "success"

5af07cb81b240930b880adb82dc532bf8968d30b
mustafa <mustafa@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-05-20 07:16:19 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@6729 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RcmdbCommons/plugins/rapid-domain-class-0.1/src/groovy/com/ifountain/rcmdb/domain/operation/DomainOperationManager.groovy
+++ RcmdbCommons/plugins/rapid-domain-class-0.1/src/groovy/com/ifountain/rcmdb/domain/operation/DomainOperationManager.groovy
@@ -73,9 +73,6 @@ class DomainOperationManager {
             GroovyClassLoader gcl = new GroovyClassLoader(parentClassLoader);
             gcl.addClasspath (operationsDirectory);
             gcl.setShouldRecompile (true);
-            println ("ADDED CLASS PATH:${gcl.getParent().classPath } of parent classloader${gcl.getParent().getParent()}")
-            println ("ADDED CLASS PATH:"+operationsDirectory)
-            println ("CLASS LOADER CLASS PATH:"+gcl.classPath)
             Class cls = null;
             try
             {

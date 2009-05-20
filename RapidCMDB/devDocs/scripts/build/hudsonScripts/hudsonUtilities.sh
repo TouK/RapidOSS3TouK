637d1ac97384618ddf428d812ab2397bfbaeff27
mustafa <mustafa@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-05-20 14:56:22 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@6737 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/devDocs/scripts/build/hudsonScripts/hudsonUtilities.sh
+++ RapidCMDB/devDocs/scripts/build/hudsonScripts/hudsonUtilities.sh
@@ -128,8 +128,46 @@ runTestBuildAndJavaTestsForRCMDB() {
 
 runGrailsTests() {
     cp $WORKSPACE/RapidModules/RapidCMDB/devDocs/groovy-starter-for-unit-tests.conf  $WORKSPACE/Distribution/RapidServer/conf/groovy-starter.conf
+    cd $WORKSPACE/Distribution/RapidServer/Modeler/
+    cp $WORKSPACE/RapidModules/RapidCMDB/devDocs/RCMDBTest.properties .
+    chmod +x *.sh
+    rm -rf $WORKSPACE/Distribution/RapidServer/temp
+    ./rsmodeler.sh -testUnit
+    sleep 5
+    rm -r test/reports/TESTS-TestSuites.xml
+    if [ ! -d $WORKSPACE/TestResults/Modeler ]
+      then
+        mkdir $WORKSPACE/TestResults/Modeler
+    fi
+    mv test/reports/*.xml $WORKSPACE/TestResults/Modeler
 
-    cd RapidSuite
+    sed -i "s/MAX_MEMORY_SIZE="512"/MAX_MEMORY_SIZE="1024"/g" rsmodeler.sh
+    cp $WORKSPACE/RapidModules/RapidCMDB/devDocs/groovy-starter-for-integration-tests.conf  $WORKSPACE/Distribution/RapidServer/conf/groovy-starter.conf
+    rm -rf $WORKSPACE/Distribution/RapidServer/temp
+    ./rsmodeler.sh -testIntegration
+
+    sleep 5
+    rm -r test/reports/TESTS-TestSuites.xml
+    if [ ! -d $WORKSPACE/TestResults/Modeler ]
+      then
+        mkdir $WORKSPACE/TestResults/Modeler
+    fi
+    mv test/reports/*.xml $WORKSPACE/TestResults/Modeler
+
+    cd ../RapidSuite
+    cp $WORKSPACE/RapidModules/RapidCMDB/devDocs/RCMDBTest.properties .
+    chmod +x rs.sh
+
+    sed -i "s/-Xmx512m/-Xmx1024m/g" rs.sh
+    cp $WORKSPACE/RapidModules/RapidCMDB/devDocs/groovy-starter-for-unit-tests.conf  $WORKSPACE/Distribution/RapidServer/conf/groovy-starter.conf
+    rm -rf $WORKSPACE/Distribution/RapidServer/temp
+    ./rs.sh -testUnit
+    rm -r test/reports/TESTS-TestSuites.xml
+    if [ ! -d $WORKSPACE/TestResults/RapidSuite ]
+      then
+        mkdir $WORKSPACE/TestResults/RapidSuite
+    fi
+    mv test/reports/*.xml  $WORKSPACE/TestResults/RapidSuite
     cp $WORKSPACE/RapidModules/RapidCMDB/devDocs/groovy-starter-for-integration-tests.conf  $WORKSPACE/Distribution/RapidServer/conf/groovy-starter.conf
     rm -rf $WORKSPACE/Distribution/RapidServer/temp
     ./rs.sh -testIntegration

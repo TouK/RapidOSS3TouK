ca79be03a8bc09592d6e312eb1f8a8235637def8
sezgin <sezgin@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-05-21 08:51:33 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@6746 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

---
+++ RapidCMDB/src/groovy/com/ifountain/rcmdb/sametime/connection/SametimeConnectionImpl.groovy
@@ -0,0 +1,396 @@
+package com.ifountain.rcmdb.sametime.connection
+
+import com.ifountain.core.connection.BaseConnection
+import com.ifountain.core.connection.ConnectionParam
+import com.ifountain.core.connection.exception.UndefinedConnectionParameterException
+import com.lotus.sametime.core.comparch.STSession
+import com.lotus.sametime.community.CommunityService
+import com.lotus.sametime.lookup.LookupService
+import com.lotus.sametime.im.InstantMessagingService
+import com.lotus.sametime.lookup.Resolver
+import com.lotus.sametime.community.LoginListener
+import com.lotus.sametime.community.LoginEvent
+import com.lotus.sametime.core.constants.ImTypes
+import org.apache.log4j.Logger
+import com.lotus.sametime.im.ImServiceListener
+import com.lotus.sametime.im.ImEvent
+import com.lotus.sametime.lookup.ResolveListener
+import com.lotus.sametime.lookup.ResolveEvent
+import com.lotus.sametime.core.constants.STError
+import com.lotus.sametime.im.Im
+import com.lotus.sametime.core.types.STUser
+import com.ifountain.core.connection.exception.ConnectionException
+import com.lotus.sametime.im.ImListener
+import com.lotus.sametime.core.constants.EncLevel
+
+/**
+* Created by IntelliJ IDEA.
+* User: Sezgin Kucukkaraaslan
+* Date: May 20, 2009
+* Time: 3:13:34 PM
+*/
+class SametimeConnectionImpl extends BaseConnection implements LoginListener, ResolveListener, ImServiceListener, ImListener {
+    Logger logger = Logger.getLogger(SametimeConnectionImpl.class);
+    public static final String HOST = "Host"
+    public static final String USERNAME = "Username"
+    public static final String PASSWORD = "Password"
+    public static final String COMMUNITY = "Community"
+    private String host;
+    private String username;
+    private String password;
+    private String community;
+    private STSession stsession;
+    private CommunityService commservice;
+    private LookupService lookupService;
+    private InstantMessagingService imService;
+    private Resolver resolver;
+
+    private Map<String, String> targets = new HashMap<String, String>(); //userId-target map
+    private Map<String, String> userIds = new HashMap<String, String>(); //target-userId map
+    private Map<String, Im> ims = new HashMap<String, Im>();
+    private Map<String, STUser> resolvedUsers = new HashMap<String, STUser>();
+
+    private static final int NOT_RECEIVED = 0;
+    private static final int FAILED = 1;
+    private static final int SUCCESSFUL = 2;
+    private int resolveResult;
+    private int imOpenResult;
+    private int loginResult;
+    private String targetToBeResolved;
+
+    private Object connectionLock = new Object();
+    private Object loginLock = new Object();
+    private Object imLock = new Object();
+    private Object userResolverLock = new Object();
+    private Exception exceptionToBeThrown;
+
+    private Closure textReceivedCallback;
+
+    protected void connect() throws Exception {
+        synchronized (connectionLock) {
+            stsession = new STSession("RapidCMDB" + this);
+            logger.info(getLogPrefix() + "Sametime session created.");
+            stsession.loadSemanticComponents();
+            logger.info(getLogPrefix() + "Sametime components loaded.");
+            stsession.start();
+            logger.info(getLogPrefix() + "Sametime session started.");
+            login();
+        }
+    }
+
+    private void login() throws InterruptedException {
+        commservice = (CommunityService) stsession.getCompApi(CommunityService.COMP_NAME);
+        commservice.enableAutomaticReconnect(Integer.MAX_VALUE, 5000);
+        commservice.addLoginListener(this);
+        loginResult = NOT_RECEIVED;
+        logger.debug(getLogPrefix() + "Trying to login to server: " + host + " with user: " + username);
+        commservice.loginByPassword(host, username, password, community);
+        synchronized (loginLock) {
+            loginLock.wait(getTimeout());
+        }
+        if (loginResult == FAILED) {
+            throw exceptionToBeThrown
+        }
+        else if (loginResult == NOT_RECEIVED) {
+            throw new ConnectionException("Login request to the host ${host} timed out.");
+        }
+
+    }
+
+    protected void disconnect() {
+        synchronized (connectionLock)
+        {
+            logger.debug(getLogPrefix() + "Closing instant message sessions.");
+            for (Iterator iter = ims.values().iterator(); iter.hasNext();) {
+                Im im = (Im) iter.next();
+                im.removeImListener(this);
+                im.close(0);
+            }
+            logger.info(getLogPrefix() + "All instant message sessions closed.");
+            ims.clear();
+            resolvedUsers.clear();
+            targets.clear();
+            userIds.clear();
+            if (imService != null) {
+                imService.removeImServiceListener(this);
+            }
+            if (resolver != null) {
+                resolver.removeResolveListener(this);
+            }
+            if (commservice != null) {
+                commservice.logout();
+                commservice.removeLoginListener(this);
+                logger.info(getLogPrefix() + "Successfully logged out.");
+            }
+            if (stsession != null) {
+                stsession.stop();
+                logger.info(getLogPrefix() + "Sametime session stopped.");
+                stsession.unloadSession();
+                logger.info(getLogPrefix() + "Sametime session unloaded.");
+            }
+        }
+    }
+
+    public boolean checkConnection() {
+        try {
+            return resolveUser(username) == SUCCESSFUL
+        }
+        catch (e) {
+            return false;
+        }
+    }
+
+    public boolean isConnectionException(Throwable t) {
+        return false;
+    }
+
+    public void init(ConnectionParam param) {
+        super.init(param);
+        host = checkParam(HOST);
+        username = checkParam(USERNAME)
+        password = checkParam(PASSWORD)
+        community = params.getOtherParams().get(COMMUNITY);
+    }
+
+    private String checkParam(String parameterName) throws UndefinedConnectionParameterException {
+        if (!params.getOtherParams().containsKey(parameterName)) {
+            throw new UndefinedConnectionParameterException(parameterName);
+        }
+        return (String) params.getOtherParams().get(parameterName);
+    }
+
+    public void loggedOut(LoginEvent event) {
+        loginResult = FAILED;
+        String errorMessage = getErrorMessage(event.getReason())
+        logger.warn(getLogPrefix() + "Logged out. Reason: " + errorMessage);
+        exceptionToBeThrown = new Exception(errorMessage);
+        ims.clear();
+        synchronized (loginLock) {
+            loginLock.notifyAll();
+        }
+    }
+
+    public void loggedIn(LoginEvent event) {
+        logger.info(getLogPrefix() + "Successfully logged in.");
+        loginResult = SUCCESSFUL;
+        imService = (InstantMessagingService) stsession.getCompApi(InstantMessagingService.COMP_NAME);
+        imService.registerImType(ImTypes.IM_TYPE_CHAT);
+        imService.addImServiceListener(this);
+        lookupService = (LookupService) stsession.getCompApi(LookupService.COMP_NAME);
+        resolver = lookupService.createResolver(true, false, true, false);
+        resolver.addResolveListener(this);
+        synchronized (loginLock) {
+            loginLock.notifyAll();
+        }
+    }
+
+    protected synchronized int resolveUser(String target) throws InterruptedException
+    {
+        synchronized (connectionLock) {
+            if (!isConnected()) {
+                return NOT_RECEIVED
+            }
+        }
+        resolveResult = NOT_RECEIVED;
+        targetToBeResolved = target;
+        logger.debug(getLogPrefix() + "Resolving user " + target);
+        resolver.resolve(target);
+        synchronized (userResolverLock) {
+            userResolverLock.wait(getTimeout());
+        }
+        return resolveResult;
+    }
+
+    public void resolveConflict(ResolveEvent event) {
+        logger.debug(getLogPrefix() + "Resolve conflict.");
+        resolveResult = FAILED;
+        synchronized (userResolverLock) {
+            userResolverLock.notifyAll();
+        }
+    }
+
+    public void resolveFailed(ResolveEvent event) {
+        logger.debug(getLogPrefix() + "Resolve failed.");
+        resolveResult = FAILED;
+        synchronized (userResolverLock) {
+            userResolverLock.notifyAll();
+        }
+    }
+
+    public void resolved(ResolveEvent event) {
+        resolveResult = SUCCESSFUL;
+        STUser user = (STUser) event.getResolved();
+        String userId = user.getId().getId();
+        targets.put(userId, targetToBeResolved);
+        userIds.put(targetToBeResolved, userId);
+        resolvedUsers.put(userId, user);
+        logger.debug(getLogPrefix() + "Sametime user <" + targetToBeResolved + "> is resolved.");
+        synchronized (userResolverLock) {
+            userResolverLock.notifyAll();
+        }
+    }
+
+    public void sendImMessage(String to, String messageText) throws Exception {
+        String userId = userIds.get(to);
+        Im im;
+        if (userId == null) {
+            def resResult = resolveUser(to);
+            switch (resResult) {
+                case NOT_RECEIVED: throw new ConnectionException("Sametime user <" + to + "> resolve request timed out.");
+                case FAILED: throw new Exception("Sametime user <" + to + "> could not be resolved.");
+                case SUCCESSFUL: break;
+            }
+            userId = userIds.get(to);
+            im = getImFromResolvedUser(userId, to);
+        }
+        else {
+            im = ims.get(userId);
+            if (im == null) {
+                im = getImFromResolvedUser(userId, to);
+            }
+        }
+        im.sendText(false, messageText);
+    }
+
+    private void openIm(Im im, String to) throws Exception
+    {
+        logger.debug(getLogPrefix() + "Opening im.");
+        imOpenResult = NOT_RECEIVED;
+        im.open();
+        synchronized (imLock) {
+            imLock.wait(getTimeout());
+        }
+        if (imOpenResult == NOT_RECEIVED)
+        {
+            throw new ConnectionException("Sametime instant message session request with user <" + to + "> timed out.");
+        }
+        if (imOpenResult == FAILED)
+        {
+            throw new Exception("Sametime instant message session with user <" + to + "> cannot be created.");
+        }
+    }
+
+    private Im getImFromResolvedUser(String userId, String target) throws Exception {
+        STUser user = resolvedUsers.get(userId);
+        logger.debug(getLogPrefix() + "Creating instant message session with: " + user.getName());
+        Im im = imService.createIm(user, EncLevel.ENC_LEVEL_NONE, ImTypes.IM_TYPE_CHAT);
+        im.addImListener(this);
+        openIm(im, target);
+        im = ims.get(userId);
+        return im;
+    }
+
+
+    public void imReceived(ImEvent event) {
+        Im im = event.getIm();
+        STUser user = im.getPartner();
+        String userId = user.getId().getId();
+        if (targets.containsKey(userId)) {
+            logger.debug(getLogPrefix() + "Im received from " + targets.get(userId));
+            if (!resolvedUsers.containsKey(userId)) {
+                resolvedUsers.put(userId, user);
+            }
+        }
+        else {
+            logger.debug(getLogPrefix() + "Im received from " + userId);
+        }
+        if (!ims.containsKey(userId)) {
+            im.addImListener(this);
+            ims.put(userId, im);
+        }
+    }
+
+    public void dataReceived(ImEvent event) {
+    }
+
+    public void textReceived(ImEvent event) {
+        if (textReceivedCallback != null) {
+            Im im = event.getIm();
+            STUser user = im.getPartner();
+            String userId = user.getId().getId();
+            String target;
+            if (targets.containsKey(userId)) {
+                target = targets.get(userId);
+            }
+            else {
+                target = userId;
+            }
+            String eventText = event.getText();
+            textReceivedCallback(target, eventText);
+        }
+    }
+
+    public void imClosed(ImEvent event) {
+        Im im = event.getIm();
+        String userId = im.getPartner().getId().getId();
+        im.removeImListener(this);
+        ims.remove(userId);
+        logger.info(getLogPrefix() + "IM closed");
+    }
+
+    public void imOpened(ImEvent event) {
+        imOpenResult = SUCCESSFUL;
+        Im im = event.getIm();
+        String userId = im.getPartner().getId().getId();
+        ims.put(userId, im);
+        logger.debug(getLogPrefix() + "Sametime instant message session with <" + targets.get(userId) + "> created.");
+        synchronized (imLock) {
+            imLock.notifyAll();
+        }
+    }
+
+    public void openImFailed(ImEvent event) {
+        logger.debug(getLogPrefix() + "Instant message session creation failed.");
+        imOpenResult = FAILED;
+        synchronized (imLock) {
+            imLock.notifyAll();
+        }
+    }
+
+
+    private String getLogPrefix() {
+        return "[SametimeConnection]: "
+    }
+    public String getErrorMessage(int reason) {
+        if (reason == STError.ST_CONNECT_HOST_UNREACHABLE) {
+            return "Host " + host + " is unreachable";
+        }
+        else if (reason == STError.ST_CONNECT_BAD_LOGIN) {
+            return "Bad login for user " + username + ".";
+        }
+        else {
+            return STError.getMessageString(reason);
+        }
+    }
+    public CommunityService getCommservice() {
+        return commservice;
+    }
+
+    public InstantMessagingService getImService() {
+        return imService;
+    }
+
+
+    public LookupService getLookupService() {
+        return lookupService;
+    }
+
+    public STSession getStsession() {
+        return stsession;
+    }
+
+    protected Map getTargets() {
+        return targets;
+    }
+    protected Map getUserIds() {
+        return userIds;
+    }
+
+    public Map<String, STUser> getResolvedUsers() {
+        return resolvedUsers;
+    }
+
+    public setTextReceivedCallback(Closure c) {
+        textReceivedCallback = c;
+    }
+}
---
+++ RapidCMDB/test/unit/com/ifountain/rcmdb/sametime/connection/SametimeConnectionImplTests.groovy
@@ -0,0 +1,240 @@
+package com.ifountain.rcmdb.sametime.connection
+
+import com.ifountain.core.connection.ConnectionParam
+import com.ifountain.core.test.util.RapidCoreTestCase
+import com.ifountain.rcmdb.test.util.SametimeTestConstants
+import com.ifountain.core.connection.exception.UndefinedConnectionParameterException
+import com.ifountain.comp.test.util.CommonTestUtils
+import com.lotus.sametime.core.constants.STError
+import com.ifountain.rcmdb.test.util.ClosureWaitAction
+import com.ifountain.core.connection.exception.ConnectionException
+import junit.framework.TestSuite
+
+/**
+* Created by IntelliJ IDEA.
+* User: Sezgin Kucukkaraaslan
+* Date: May 20, 2009
+* Time: 3:15:15 PM
+*/
+class SametimeConnectionImplTests extends RapidCoreTestCase {
+    SametimeConnectionImpl connection;
+    String receiverUsername;
+    protected void setUp() {
+        super.setUp();
+        connection = new SametimeConnectionImpl();
+        receiverUsername = CommonTestUtils.getTestProperty(SametimeTestConstants.SAMETIME_SECONDARY_USERNAME);
+    }
+
+    protected void tearDown() {
+        if (connection.isConnected()) {
+            connection._disconnect();
+        }
+        super.tearDown();
+    }
+
+    public void testInit() throws Exception {
+        ConnectionParam connParam = getConnectionParam();
+        try {
+            connection.init(connParam);
+        }
+        catch (e) {
+            fail("should not throw exception")
+        }
+        assertSame(connParam, connection.getParameters());
+
+        connParam.getOtherParams().remove(SametimeConnectionImpl.HOST);
+        try {
+            connection.init(connParam);
+            fail("should throw exception")
+        }
+        catch (UndefinedConnectionParameterException e) {}
+
+        connParam = getConnectionParam();
+        connParam.getOtherParams().remove(SametimeConnectionImpl.USERNAME);
+        try {
+            connection.init(connParam);
+            fail("should throw exception")
+        }
+        catch (UndefinedConnectionParameterException e) {}
+
+        connParam = getConnectionParam();
+        connParam.getOtherParams().remove(SametimeConnectionImpl.PASSWORD);
+        try {
+            connection.init(connParam);
+            fail("should throw exception")
+        }
+        catch (UndefinedConnectionParameterException e) {}
+
+        //community is optional
+        connParam = getConnectionParam();
+        connParam.getOtherParams().remove(SametimeConnectionImpl.COMMUNITY);
+        try {
+            connection.init(connParam);
+        }
+        catch (e) {
+            fail("should not throw exception")
+        }
+    }
+
+    public void testConnect() throws Exception {
+        ConnectionParam param = getConnectionParam();
+        connection.init(param);
+        connection._connect();
+        assertTrue(connection.checkConnection());
+        assertTrue(connection.getStsession().isActive())
+        assertTrue(connection.getCommservice().isLoggedIn())
+    }
+
+    public void testServerIsInvalid() throws Exception {
+        ConnectionParam param = getConnectionParam();
+        param.getOtherParams().put(SametimeConnectionImpl.HOST, "HostDoesNotExist")
+        connection.init(param);
+        try {
+            connection._connect();
+            fail("should throw exception")
+        }
+        catch (e) {
+            assertEquals(connection.getErrorMessage(STError.ST_CONNECT_HOST_UNREACHABLE), e.getMessage())
+        }
+    }
+
+    public void testUsernameIsInvalid() throws Exception {
+        ConnectionParam param = getConnectionParam();
+        param.getOtherParams().put(SametimeConnectionImpl.USERNAME, "Invalid user")
+        connection.init(param);
+        try {
+            connection._connect();
+            fail("should throw exception")
+        }
+        catch (e) {
+            assertEquals(connection.getErrorMessage(STError.ST_CONNECT_BAD_LOGIN), e.getMessage())
+        }
+    }
+    public void testPasswordIsInvalid() throws Exception {
+        ConnectionParam param = getConnectionParam();
+        param.getOtherParams().put(SametimeConnectionImpl.PASSWORD, "InvalidPass")
+        connection.init(param);
+        try {
+            connection._connect();
+            fail("should throw exception")
+        }
+        catch (e) {
+            assertEquals(connection.getErrorMessage(STError.ST_CONNECT_BAD_LOGIN), e.getMessage())
+        }
+    }
+
+    public void testResolveUser() {
+        ConnectionParam param = getConnectionParam();
+        connection.init(param);
+        connection._connect();
+        connection.resolveUser(receiverUsername);
+        assertEquals(1, connection.getTargets().size());
+        assertEquals(1, connection.getUserIds().size());
+        String userId = connection.getUserIds().get(receiverUsername);
+        assertNotNull(userId);
+        assertEquals(receiverUsername, connection.getTargets().get(userId));
+        assertEquals(1, connection.getResolvedUsers().size());
+        assertTrue(connection.getResolvedUsers().containsKey(userId));
+    }
+
+    public void testDisconnect() throws Exception {
+        ConnectionParam param = getConnectionParam();
+        connection.init(param);
+        connection._connect();
+        assertTrue(connection.checkConnection());
+        connection._disconnect();
+        assertFalse(connection.checkConnection());
+        assertFalse(connection.getStsession().isActive())
+        CommonTestUtils.waitFor(new ClosureWaitAction({
+            assertFalse(connection.getCommservice().isLoggedIn())    
+        }))
+    }
+
+    public void testSendMessage() throws Exception {
+        ConnectionParam param = getConnectionParam();
+        connection.init(param);
+        connection._connect();
+
+        SametimeConnectionImpl receiverConnection = new SametimeConnectionImpl();
+        param.getOtherParams().put(SametimeConnectionImpl.USERNAME, receiverUsername);
+        param.getOtherParams().put(SametimeConnectionImpl.PASSWORD, CommonTestUtils.getTestProperty(SametimeTestConstants.SAMETIME_SECONDARY_PASSWORD));
+        receiverConnection.init(param);
+        receiverConnection._connect();
+        try {
+            def receivedMessages = [];
+            def textReceived = {from, message ->
+                receivedMessages.add(message);
+            }
+            receiverConnection.setTextReceivedCallback(textReceived)
+            def messageText = "Hello World";
+            connection.sendImMessage(receiverUsername, messageText);
+
+            CommonTestUtils.waitFor(new ClosureWaitAction({
+                assertEquals(1, receivedMessages.size())
+                assertEquals(messageText, receivedMessages[0])
+            }))
+        }
+        finally {
+            receiverConnection._disconnect();
+        }
+    }
+
+    public void testSendMessageThrowsExceptionIfNotConnected() {
+        ConnectionParam param = getConnectionParam();
+        connection.init(param);
+        def messageText = "Hello World";
+
+        try {
+            connection.sendImMessage(receiverUsername, messageText);
+            fail("should throw exception")
+        }
+        catch (ConnectionException e) {
+            assertEquals("Sametime user <" + receiverUsername + "> resolve request timed out.", e.getMessage())
+        }
+    }
+
+    public void testCannotSendOfflineMessages() {
+        ConnectionParam param = getConnectionParam();
+        connection.init(param);
+        connection._connect();
+
+        def messageText = "Hello World";
+        try {
+            connection.sendImMessage(receiverUsername, messageText);
+            fail("should throw exception")
+        }
+        catch (Exception e) {
+            assertEquals("Sametime instant message session with user <" + receiverUsername + "> cannot be created.", e.getMessage());
+        }
+    }
+
+    public void testSendingMessageToANonexistingUser() {
+        ConnectionParam param = getConnectionParam();
+        connection.init(param);
+        connection._connect();
+
+        def messageText = "Hello World";
+        def invalidUser = "invalidUser"
+        try {
+            connection.sendImMessage(invalidUser, messageText);
+            fail("should throw exception")
+        }
+        catch (Exception e) {
+            assertEquals("Sametime user <" + invalidUser + "> could not be resolved.", e.getMessage());
+        }
+    }
+
+    def getConnectionParam() {
+        Map otherParams = new HashMap();
+        otherParams.put(SametimeConnectionImpl.HOST, CommonTestUtils.getTestProperty(SametimeTestConstants.SAMETIME_HOST))
+        otherParams.put(SametimeConnectionImpl.USERNAME, CommonTestUtils.getTestProperty(SametimeTestConstants.SAMETIME_USERNAME))
+        otherParams.put(SametimeConnectionImpl.PASSWORD, CommonTestUtils.getTestProperty(SametimeTestConstants.SAMETIME_PASSWORD))
+        otherParams.put(SametimeConnectionImpl.COMMUNITY, CommonTestUtils.getTestProperty(SametimeTestConstants.SAMETIME_COMMUNITY))
+        ConnectionParam param = new ConnectionParam("SametimeConnection", SametimeTestConstants.SAMETIME_TEST_CONNECTION, SametimeConnectionImpl.class.getName(), otherParams);
+        param.setMinTimeout(30000);
+        param.setMaxTimeout(30000);
+        TestSuite
+        return param;
+
+    }
+}

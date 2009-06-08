4ed4b6d14ecbd300ac3035c77cd87a1b2cacc2f5
sezgin <sezgin@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-06-08 11:07:50 +0000

    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@6916 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/src/groovy/com/ifountain/rcmdb/sms/datasource/SmsAdapter.groovy
+++ RapidCMDB/src/groovy/com/ifountain/rcmdb/sms/datasource/SmsAdapter.groovy
@@ -1,6 +1,7 @@
 package com.ifountain.rcmdb.sms.datasource
 
 import com.ifountain.core.datasource.BaseAdapter
+import org.apache.log4j.Logger
 
 /**
 * Created by IntelliJ IDEA.
@@ -9,14 +10,16 @@ import com.ifountain.core.datasource.BaseAdapter
 * Time: 2:24:25 PM
 */
 class SmsAdapter extends BaseAdapter {
-
+    public SmsAdapter(String connConfigName, long reconnectInterval, Logger logger) {
+        super(connConfigName, reconnectInterval, logger)
+    }
     public Map<String, Object> getObject(Map<String, String> ids, List<String> fieldsToBeRetrieved) {
         return null;
     }
 
     public void sendMessage(String smsNumber, String message) {
         SendMessageAction action = new SendMessageAction(smsNumber, message)
-        action.execute();
+        executeAction(action);
     }
 
 }

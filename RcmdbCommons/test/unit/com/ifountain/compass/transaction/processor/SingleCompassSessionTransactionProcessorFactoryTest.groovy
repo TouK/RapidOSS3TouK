a528ed79160fb1d5d2ed91366cc3f095fd4adfb9
mustafa <mustafa@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-06-09 06:48:00 +0000

    CMDB-1431 - Implement compass transactionprocessor to process compasstransaction at background
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@6932 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RcmdbCommons/test/unit/com/ifountain/compass/transaction/processor/SingleCompassSessionTransactionProcessorFactoryTest.groovy
+++ RcmdbCommons/test/unit/com/ifountain/compass/transaction/processor/SingleCompassSessionTransactionProcessorFactoryTest.groovy
@@ -23,7 +23,7 @@ import org.compass.core.spi.InternalCompass
 * Time: 4:54:05 PM
 * To change this template use File | Settings | File Templates.
 */
-class SingleCompassSessionTransactionProcessorFactoryTest extends AbstractSearchableCompassTests{
+class SingleCompassSessionTransactionProcessorFactoryTest extends AbstractSearchableCompassTests {
     InternalCompass compass;
     SingleCompassSessionTransactionProcessorFactory factory;
     Map additionalCompassSettings;
@@ -35,99 +35,99 @@ class SingleCompassSessionTransactionProcessorFactoryTest extends AbstractSearch
     public void tearDown() {
         super.tearDown(); //To change body of overridden methods use File | Settings | File Templates.
     }
-    
+
     public void testTransactionProcessorFactoryConfigurationMethods()
     {
         initialize();
         def compassSettings = compass.getSettings();
         def isConcurrent = new Boolean(compassSettings.getSetting(LuceneEnvironment.Transaction.Processor.ReadCommitted.CONCURRENT_OPERATIONS));
-        assertFalse ("concurrent commit should be set to false", isConcurrent);
-        assertEquals (1000, factory.getCommitInterval());
+        assertFalse("concurrent commit should be set to false", isConcurrent);
+        assertEquals(1000, factory.getCommitInterval());
         compass.close();
 
 
         def newCommitInterval = 10000;
-        addCommitIntervalSetting (newCommitInterval)
+        addCommitIntervalSetting(newCommitInterval)
         initialize();
-        assertEquals (newCommitInterval, factory.getCommitInterval());
+        assertEquals(newCommitInterval, factory.getCommitInterval());
     }
 
     public void testCommitsTransactionAtSpecifiedIntervals()
     {
-        addCommitIntervalSetting (250);
+        addCommitIntervalSetting(250);
         initializeWithMockFactory();
         def commitIntervals = [];
         RadCommittedTransactionProcessorWrapper wrapper = TransactionProcessorFactoryMock.wrapperToBeReturned;
-        wrapper.closureMap.commit = {boolean onePhase->
+        wrapper.closureMap.commit = {boolean onePhase ->
             commitIntervals.add(System.nanoTime());
         }
-        Thread.sleep (1000);
+        Thread.sleep(1000);
         assertTrue(commitIntervals.size() >= 3);
         compass.close();
         def commitIntervalCountAfterClose = commitIntervals.size();
-        assertTrue ("Warpped factory close method should be called", TransactionProcessorFactoryMock.isCloseCalled);
-        for(int i=commitIntervals.size()-2; i >= 0 ; i--)
+        assertTrue("Warpped factory close method should be called", TransactionProcessorFactoryMock.isCloseCalled);
+        for (int i = commitIntervals.size() - 2; i >= 0; i--)
         {
-            assertTrue (commitIntervals[i+1] - commitIntervals[i] < 275*Math.pow(10,6))
+            assertTrue(commitIntervals[i + 1] - commitIntervals[i] < 275 * Math.pow(10, 6))
         }
 
-        Thread.sleep (1000);
-        assertEquals ("after close committer thread should be destroyed", commitIntervalCountAfterClose, commitIntervals.size());
+        Thread.sleep(1000);
+        assertEquals("after close committer thread should be destroyed", commitIntervalCountAfterClose, commitIntervals.size());
 
         def newCommitInterval = 800000;
-        addCommitIntervalSetting (newCommitInterval)
+        addCommitIntervalSetting(newCommitInterval)
         commitIntervals.clear();
         initializeWithMockFactory();
         wrapper = TransactionProcessorFactoryMock.wrapperToBeReturned;
-        wrapper.closureMap.commit = {boolean onePhase->
+        wrapper.closureMap.commit = {boolean onePhase ->
             commitIntervals.add(System.nanoTime());
         }
-        Thread.sleep (1000);
+        Thread.sleep(1000);
 
         assertTrue("Expected 0 but was ${commitIntervals.size()}", commitIntervals.size() == 0);
     }
 
     public void testSearchOperationAfterCommitWillNotCauseAnyProblem()
     {
-        addCommitIntervalSetting (250)
+        addCommitIntervalSetting(250)
         initialize();
-        
+
         RadCommittedTransactionProcessorWrapper wrapper = TransactionProcessorFactoryMock.wrapperToBeReturned;
-        CompassTestObject objToBeSaved1 = new CompassTestObject(id:1);
-        CompassTestObject objToBeSaved2 = new CompassTestObject(id:2);
-        withCompassQueryBuilder (){CompassQueryBuilder builder->
-            withCompassSession (){session->
-                session.save (objToBeSaved1);
+        CompassTestObject objToBeSaved1 = new CompassTestObject(id: 1);
+        CompassTestObject objToBeSaved2 = new CompassTestObject(id: 2);
+        withCompassQueryBuilder() {CompassQueryBuilder builder ->
+            withCompassSession() {session ->
+                session.save(objToBeSaved1);
             }
-            CompassHits hits = builder.queryString ("alias:*").toQuery().hits();
-            Thread.sleep (1000);
-            withCompassSession (){session->
-                session.save (objToBeSaved2);
+            CompassHits hits = builder.queryString("alias:*").toQuery().hits();
+            Thread.sleep(1000);
+            withCompassSession() {session ->
+                session.save(objToBeSaved2);
             }
-            assertEquals (1, hits.length());
-            Object objReturnedFromCompass = hits.data (0);
-            assertEquals (objToBeSaved1.id, objReturnedFromCompass.id);
+            assertEquals(1, hits.length());
+            Object objReturnedFromCompass = hits.data(0);
+            assertEquals(objToBeSaved1.id, objReturnedFromCompass.id);
         }
     }
 
     public void testSearchOperationWillSeeChangesWhichAreNotCommittedYet()
     {
-        addCommitIntervalSetting (250000)
+        addCommitIntervalSetting(250000)
         initialize();
         RadCommittedTransactionProcessorWrapper wrapper = TransactionProcessorFactoryMock.wrapperToBeReturned;
-        withCompassQueryBuilder (){CompassQueryBuilder builder->
+        withCompassQueryBuilder() {CompassQueryBuilder builder ->
             def savedObjects = []
-            withCompassSession (){session->
-                for(int i=0; i < 100; i++)
+            withCompassSession() {session ->
+                for (int i = 0; i < 100; i++)
                 {
-                    savedObjects.add(new CompassTestObject(id:i))
-                    session.save (savedObjects[savedObjects.size()-1]);
+                    savedObjects.add(new CompassTestObject(id: i))
+                    session.save(savedObjects[savedObjects.size() - 1]);
                 }
             }
-            CompassHits hits = builder.queryString ("alias:*").toQuery().hits();
-            assertEquals (savedObjects.size(), hits.length());
-            savedObjects.each{addedObject->
-                def objectFromCompass = builder.queryString ("id:${addedObject.id}".toString()).toQuery().hits().data (0)
+            CompassHits hits = builder.queryString("alias:*").toQuery().hits();
+            assertEquals(savedObjects.size(), hits.length());
+            savedObjects.each {addedObject ->
+                def objectFromCompass = builder.queryString("id:${addedObject.id}".toString()).toQuery().hits().data(0)
                 assertEquals(addedObject.id, objectFromCompass.id);
             }
         }
@@ -135,26 +135,26 @@ class SingleCompassSessionTransactionProcessorFactoryTest extends AbstractSearch
 
     public void testDelete()
     {
-        addCommitIntervalSetting (250000)
+        addCommitIntervalSetting(250000)
         initialize();
         RadCommittedTransactionProcessorWrapper wrapper = TransactionProcessorFactoryMock.wrapperToBeReturned;
-        withCompassQueryBuilder (){CompassQueryBuilder builder->
+        withCompassQueryBuilder() {CompassQueryBuilder builder ->
             def savedObjects = []
-            withCompassSession (){CompassSession session->
-                for(int i=0; i < 100; i++)
+            withCompassSession() {CompassSession session ->
+                for (int i = 0; i < 100; i++)
                 {
-                    def objectToBeDeleted = new CompassTestObject(id:i);
-                    session.save (objectToBeDeleted);
+                    def objectToBeDeleted = new CompassTestObject(id: i);
+                    session.save(objectToBeDeleted);
                     session.delete(objectToBeDeleted);
                 }
-                def object = new CompassTestObject(id:1);
-                session.save (object);
+                def object = new CompassTestObject(id: 1);
+                session.save(object);
                 savedObjects.add(object)
             }
-            CompassHits hits = builder.queryString ("alias:*").toQuery().hits();
-            assertEquals (1, hits.length());
-            savedObjects.each{addedObject->
-                def objectFromCompass = builder.queryString ("id:${addedObject.id}".toString()).toQuery().hits().data (0)
+            CompassHits hits = builder.queryString("alias:*").toQuery().hits();
+            assertEquals(1, hits.length());
+            savedObjects.each {addedObject ->
+                def objectFromCompass = builder.queryString("id:${addedObject.id}".toString()).toQuery().hits().data(0)
                 assertEquals(addedObject.id, objectFromCompass.id);
             }
         }
@@ -162,40 +162,91 @@ class SingleCompassSessionTransactionProcessorFactoryTest extends AbstractSearch
 
     public void testUpdate()
     {
-        addCommitIntervalSetting (250000)
+        addCommitIntervalSetting(250000)
         initialize();
         RadCommittedTransactionProcessorWrapper wrapper = TransactionProcessorFactoryMock.wrapperToBeReturned;
         def updatedPropValue = "updatedPropValue"
-        withCompassQueryBuilder (){CompassQueryBuilder builder->
+        withCompassQueryBuilder() {CompassQueryBuilder builder ->
             def savedObjects = []
-            withCompassSession (){CompassSession session->
-                for(int i=0; i < 100; i++)
+            withCompassSession() {CompassSession session ->
+                for (int i = 0; i < 100; i++)
                 {
-                    def objectToBeDeleted = new CompassTestObject(id:i);
-                    session.save (objectToBeDeleted);
+                    def objectToBeDeleted = new CompassTestObject(id: i);
+                    session.save(objectToBeDeleted);
                     objectToBeDeleted.prop1 = updatedPropValue
                     session.save(objectToBeDeleted);
                     savedObjects.add(objectToBeDeleted);
                 }
             }
-            CompassHits hits = builder.queryString ("alias:*").toQuery().hits();
-            assertEquals (savedObjects.size(), hits.length());
-            savedObjects.each{addedObject->
-                def objectFromCompass = builder.queryString ("id:${addedObject.id}".toString()).toQuery().hits().data (0)
+            CompassHits hits = builder.queryString("alias:*").toQuery().hits();
+            assertEquals(savedObjects.size(), hits.length());
+            savedObjects.each {addedObject ->
+                def objectFromCompass = builder.queryString("id:${addedObject.id}".toString()).toQuery().hits().data(0)
                 assertEquals(addedObject.id, objectFromCompass.id);
                 assertEquals(updatedPropValue, objectFromCompass.prop1);
             }
         }
     }
 
+
+    public void testSearchWhileUpdating()
+    {
+        addCommitIntervalSetting(300)
+        initialize();
+        def updatedPropValue = "updatedPropValue"
+        def numberOfObjects = 100;
+        withCompassSession() {CompassSession session ->
+            for (int i = 0; i < numberOfObjects; i++)
+            {
+                def objectToBeDeleted = new CompassTestObject(id: i);
+                session.save(objectToBeDeleted);
+            }
+        }
+        withCompassQueryBuilder() {CompassQueryBuilder builder ->
+            CompassHits hits = builder.queryString("alias:*").toQuery().hits();
+            assertEquals(numberOfObjects, hits.length());
+        }
+
+        def willStopThread = false;
+        Thread t = Thread.start {
+            while (!willStopThread)
+            {
+                withCompassSession() {CompassSession session ->
+
+                    for (int i = 0; i < numberOfObjects; i++)
+                    {
+                        def objectToBeUpdated = new CompassTestObject(id: i);
+                        objectToBeUpdated.prop1 = updatedPropValue;
+                        session.save(objectToBeUpdated);
+                    }
+
+                }
+            }
+        }
+        Thread.sleep (3000);
+        try {
+            for(int i=0; i < 1000; i++)
+            {
+                withCompassQueryBuilder (){CompassQueryBuilder builder->
+                    CompassHits hits = builder.queryString ("alias:*").toQuery().hits();
+                    assertEquals (numberOfObjects, hits.length());
+                }
+            }
+        } finally {
+            willStopThread = true;
+            t.join();
+        }
+
+    }
+
     private void addCommitIntervalSetting(long commitInterval)
     {
-        additionalCompassSettings.put (SingleCompassSessionTransactionProcessorFactory.COMMIT_INTERVAL_SETTING_KEY, ""+commitInterval);
+        additionalCompassSettings.put(SingleCompassSessionTransactionProcessorFactory.COMMIT_INTERVAL_SETTING_KEY, "" + commitInterval);
     }
 
     private void addSingleSessionFactory()
     {
-        additionalCompassSettings.put(LuceneEnvironment.Transaction.Processor.PREFIX+SingleCompassSessionTransactionProcessor.NAME+"."+LuceneEnvironment.Transaction.Processor.CONFIG_TYPE, SingleCompassSessionTransactionProcessorFactory.class.name);
+        additionalCompassSettings.put(LuceneEnvironment.Transaction.Processor.PREFIX + SingleCompassSessionTransactionProcessor.NAME + "." + LuceneEnvironment.Transaction.Processor.CONFIG_TYPE, SingleCompassSessionTransactionProcessorFactory.class.name);
         additionalCompassSettings.put(LuceneEnvironment.Transaction.Processor.TYPE, SingleCompassSessionTransactionProcessor.NAME);
     }
 
@@ -203,7 +254,7 @@ class SingleCompassSessionTransactionProcessorFactoryTest extends AbstractSearch
     {
         addSingleSessionFactory();
         compass = TestCompassFactory.getCompass([CompassTestObject], [], false, additionalCompassSettings);
-        factory = ((LuceneSearchEngineFactory)compass.getSearchEngineFactory()).getTransactionProcessorManager().getProcessorFactory (SingleCompassSessionTransactionProcessor.NAME);
+        factory = ((LuceneSearchEngineFactory) compass.getSearchEngineFactory()).getTransactionProcessorManager().getProcessorFactory(SingleCompassSessionTransactionProcessor.NAME);
     }
 
 
@@ -211,17 +262,16 @@ class SingleCompassSessionTransactionProcessorFactoryTest extends AbstractSearch
     {
         addSingleSessionFactory();
         compass = TestCompassFactory.getCompass([CompassTestObject], [], false, additionalCompassSettings);
-        factory = ((LuceneSearchEngineFactory)compass.getSearchEngineFactory()).getTransactionProcessorManager().getProcessorFactory (SingleCompassSessionTransactionProcessor.NAME);
+        factory = ((LuceneSearchEngineFactory) compass.getSearchEngineFactory()).getTransactionProcessorManager().getProcessorFactory(SingleCompassSessionTransactionProcessor.NAME);
         TransactionProcessorFactoryMock mockFactory = new TransactionProcessorFactoryMock();
-        factory.setWrappedTransactionProcessorFactory (mockFactory);
-        withCompassQueryBuilder(){builder->
-            builder.queryString("alias:*").toQuery().hits()            
+        factory.setWrappedTransactionProcessorFactory(mockFactory);
+        withCompassQueryBuilder() {builder ->
+            builder.queryString("alias:*").toQuery().hits()
         }
-        CommonTestUtils.waitFor (new ClosureWaitAction(){
-            assertNotNull (TransactionProcessorFactoryMock.wrapperToBeReturned);
+        CommonTestUtils.waitFor(new ClosureWaitAction() {
+            assertNotNull(TransactionProcessorFactoryMock.wrapperToBeReturned);
         })
     }
-
 
 }
 
@@ -230,9 +280,9 @@ class TransactionProcessorFactoryMock implements TransactionProcessorFactory
     public static isCloseCalled = false;
     public static TransactionProcessor wrapperToBeReturned;
     public TransactionProcessor create(LuceneSearchEngine searchEngine) {
-        if(wrapperToBeReturned == null)
+        if (wrapperToBeReturned == null)
         {
-            wrapperToBeReturned = new  RadCommittedTransactionProcessorWrapper(searchEngine);
+            wrapperToBeReturned = new RadCommittedTransactionProcessorWrapper(searchEngine);
         }
         return wrapperToBeReturned;
     }
@@ -256,7 +306,7 @@ class RadCommittedTransactionProcessorWrapper extends ReadCommittedTransactionPr
     }
 
     public void commit(boolean onePhase) {
-        if(closureMap.commit)
+        if (closureMap.commit)
         {
             closureMap.commit(onePhase);
         }

af8ccca51dbb35a812eadeddf72c0550fe2dbb13
fadime <fadime@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-06-15 11:33:07 +0000

    CMDB-1450 - Implement automated selenium test to test a scheduled cron script
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@7000 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/devDocs/scripts/build/RapidInsightUiTestBuild.groovy
+++ RapidCMDB/devDocs/scripts/build/RapidInsightUiTestBuild.groovy
@@ -13,25 +13,25 @@ class RapidInsightUiTestBuild extends Build {
 
 
 
-    static def getTestOptions(){
-	   Properties options = new Properties();
-	   options.put("RI_UNIX", "true")
-	   options.put("RI_WINDOWS", "false")
-	   options.put("RCMDB_UNIX", "true")
-	   options.put("RCMDB_WINDOWS", "false")
-	   options.put("MODELER", "false")
-	   options.put("OPENNMS", "false")
-	   options.put("JIRA", "false")
-	   options.put("APG", "false")
-	   options.put("NETCOOL", "false")
-	   options.put("SMARTS", "false")
-	   options.put("HYPERIC", "false")
-	   options.put("E_WINDOWS", "false")
-	   options.put("E_UNIX", "false")
-	   options.put("ZIP", "true")
-	   options.put("TEST", "false")
-	   options.put("JREDIR", "C:/Program Files/Java/jdk1.6.0_04/jre")
-	   return options;
+    static def getTestOptions() {
+        Properties options = new Properties();
+        options.put("RI_UNIX", "true")
+        options.put("RI_WINDOWS", "false")
+        options.put("RCMDB_UNIX", "true")
+        options.put("RCMDB_WINDOWS", "false")
+        options.put("MODELER", "false")
+        options.put("OPENNMS", "false")
+        options.put("JIRA", "false")
+        options.put("APG", "false")
+        options.put("NETCOOL", "false")
+        options.put("SMARTS", "false")
+        options.put("HYPERIC", "false")
+        options.put("E_WINDOWS", "false")
+        options.put("E_UNIX", "false")
+        options.put("ZIP", "true")
+        options.put("TEST", "false")
+        options.put("JREDIR", "C:/Program Files/Java/jdk1.6.0_04/jre")
+        return options;
     }
 
     static void main(String[] args) {
@@ -43,62 +43,62 @@ class RapidInsightUiTestBuild extends Build {
     {
         try {
 
-          //startSeleniumServer();
-          // buildDependentProjects()
-         //  clean();
-         // setupRi();
-          compileUiTestClasses();
+            //startSeleniumServer();
+            // buildDependentProjects()
+            //  clean();
+            // setupRi();
+            compileUiTestClasses();
 
-         //stopSeleniumServer();
-         //startt()
-          startRI();
+            //stopSeleniumServer();
+            //startt()
+            startRI();
 
-      // def testClassPaths = ["${env.distribution}/uiTestClasses/testUtils"]
-       // runTest("${env.distribution}/uiTestClasses/tests", testClassPaths, "${env.distribution}/uiTestResults","testResults")
+            // def testClassPaths = ["${env.distribution}/uiTestClasses/testUtils"]
+            // runTest("${env.distribution}/uiTestClasses/tests", testClassPaths, "${env.distribution}/uiTestResults","testResults")
         }
-        finally{
-           // stopRI()
-          //  stopSeleniumServer()
+        finally {
+            // stopRI()
+            //  stopSeleniumServer()
         }
     }
 
-     def startSeleniumServer()
-     {
-           ant.java(jar:"${env.third_party}/lib/selenium/selenium-server.jar", fork:"true",
-                   spawn:"true",jvm:"/usr/java/jdk1.6.0_04/jre/bin/java")
-     }
+    def startSeleniumServer()
+    {
+        ant.java(jar: "${env.third_party}/lib/selenium/selenium-server.jar", fork: "true",
+                spawn: "true", jvm: "/usr/java/jdk1.6.0_04/jre/bin/java")
+    }
 
-      def stopSeleniumServer()
-      {
-          ant.target( name:"stop-server") {
-              ant.get(taskname:"selenium-shutdown", src:"http://localhost:4444/selenium-server/driver/?cmd=shutDown",
-                      dest:"result.txt", ignoreerrors:"true")
-              ant.echo(taskname:"selenium-shutdown", message:"DGF Errors during shutdown are expected")
-          }
-     }
+    def stopSeleniumServer()
+    {
+        ant.target(name: "stop-server") {
+            ant.get(taskname: "selenium-shutdown", src: "http://localhost:4444/selenium-server/driver/?cmd=shutDown",
+                    dest: "result.txt", ignoreerrors: "true")
+            ant.echo(taskname: "selenium-shutdown", message: "DGF Errors during shutdown are expected")
+        }
+    }
 
 
     def startt()
     {
-      //  Runtime.getRuntime().exec("C:/Documents and Settings/fadime/Desktop/RapidServer/RapidSuite/rs.exe -start");
-         // String[] test = new String[1]
-      //   test[0]  ="C:/Documents and Settings/fadime/Desktop/RapidServer/RapidSuite"
-            def list = ["./${env.distribution}/RapidServer"]
-       Process  p = "C:/Documents and Settings/fadime/Desktop/RapidServer/RapidSuite/rs.exe -start".execute(list,null);
+        //  Runtime.getRuntime().exec("C:/Documents and Settings/fadime/Desktop/RapidServer/RapidSuite/rs.exe -start");
+        // String[] test = new String[1]
+        //   test[0]  ="C:/Documents and Settings/fadime/Desktop/RapidServer/RapidSuite"
+        def list = ["./${env.distribution}/RapidServer"]
+        Process p = "C:/Documents and Settings/fadime/Desktop/RapidServer/RapidSuite/rs.exe -start".execute(list, null);
         p.waitFor()
 
-        for(int i=0; i <0; i++)
+        for (int i = 0; i < 0; i++)
         {
             println("u")
-            try{
-               def url = new URL("http://localhost:12222/RapidSuite")
+            try {
+                def url = new URL("http://localhost:12222/RapidSuite")
                 p.sleep(60000)
-              def  content = url.getText()
+                def content = url.getText()
                 break;
-             }
-            catch(ConnectException e)
+            }
+            catch (ConnectException e)
             {
-                if(i == 5)
+                if (i == 5)
                 {
                     throw e;
                 }
@@ -106,44 +106,43 @@ class RapidInsightUiTestBuild extends Build {
         }
 
     }
-      
 
-     def startRI()
-     {
+
+    def startRI()
+    {
         Process p = "chmod +x ${env.distribution}/RapidServer/RapidSuite/rs.sh".execute();
-       p.consumeProcessOutput(System.out, System.out);
-         p.waitFor();
-         String[] test = new String[1]
-         test[0]  ="/RapidServer"
-         def list = ["./${env.distribution}/RapidServer"]
-         File dir = new File("RapidSuite")
-         p = "./${env.distribution}/RapidServer/RapidSuite/rs.sh -start".execute(list,dir);
         p.consumeProcessOutput(System.out, System.out);
         p.waitFor();
 
-        for(int i=0; i <0; i++)
+        List list = [];
+        list.add("./${env.distribution}/RapidServer".toString());
+        File dir = new File("RapidSuite")
+        p = "./${env.distribution}/RapidServer/RapidSuite/rs.exe -start".execute(list, dir);
+        p.consumeProcessOutput(System.out, System.out);
+        p.waitFor();
+
+        for (int i = 0; i < 0; i++)
         {
             println("u")
-            try{
-               def url = new URL("http://localhost:12222/RapidSuite")
+            try {
+                def url = new URL("http://localhost:12222/RapidSuite")
                 p.sleep(60000)
-              def  content = url.getText()
+                def content = url.getText()
                 break;
-             }
-            catch(ConnectException e)
+            }
+            catch (ConnectException e)
             {
-                if(i == 0)
+                if (i == 0)
                 {
                     throw e;
                 }
             }
         }
 
-       
-     }
+    }
 
-     def stopRI()
-     {
+    def stopRI()
+    {
         Process p = "chmod +x ${env.distribution}/RapidServer/RapidSuite/rs.sh".execute();
         p.consumeProcessOutput();
         p.consumeProcessErrorStream(System.out);
@@ -152,39 +151,38 @@ class RapidInsightUiTestBuild extends Build {
         p.consumeProcessOutput();
         p.consumeProcessErrorStream(System.out);
         p.waitFor();
-     }
+    }
 
     def compileUiTestClasses()
     {
         ant.taskdef(name: "groovyc", classname: "org.codehaus.groovy.ant.Groovyc");
         def destDir = "${env.distribution}/uiTestClasses";
-        ant.delete(dir:destDir);
-        ant.mkdir(dir:destDir);
-        ant.mkdir(dir:"${destDir}/testUtils");
-        ant.mkdir(dir:"${destDir}/tests");
+        ant.delete(dir: destDir);
+        ant.mkdir(dir: destDir);
+        ant.mkdir(dir: "${destDir}/testUtils");
+        ant.mkdir(dir: "${destDir}/tests");
 
-       ant.copy(file:"${env.rapid_cmdb_commons_cvs}/src/groovy/com/ifountain/rcmdb/test/util/SeleniumTestCase.groovy",
-               todir:"${env.distribution}/case")
+        ant.copy(file: "${env.rapid_cmdb_commons_cvs}/src/groovy/com/ifountain/rcmdb/test/util/SeleniumTestCase.groovy",
+                todir: "${env.distribution}/case")
 
         ant.groovyc(destdir: "${env.distribution}/uiTestClasses/testUtils",
-              classpathref: "classpath",
-                 srcdir:"${env.distribution}/case");
+                classpathref: "classpath",
+                srcdir: "${env.distribution}/case");
 
 
-        ant.groovyc(destdir: "${env.distribution}/uiTestClasses/tests", srcdir: "${env.rapid_cmdb_commons_cvs}/test/ui"){
-            ant.classpath{
-                ant.path(refid:"classpath")
-                ant.path(location:"${env.distribution}/uiTestClasses/testUtils")
+        ant.groovyc(destdir: "${env.distribution}/uiTestClasses/tests", srcdir: "${env.rapid_cmdb_commons_cvs}/test/ui") {
+            ant.classpath {
+                ant.path(refid: "classpath")
+                ant.path(location: "${env.distribution}/uiTestClasses/testUtils")
             }
         }
-        ant.delete(dir:"${env.distribution}/case")
-
+        ant.delete(dir: "${env.distribution}/case")
 
     }
 
     def setupRi()
     {
-        ant.unzip(src:"${env.distribution}/${riZipFileName}",  dest:"${env.distribution}");
+        ant.unzip(src: "${env.distribution}/${riZipFileName}", dest: "${env.distribution}");
     }
 
     def buildDependentProjects()
@@ -201,49 +199,46 @@ class RapidInsightUiTestBuild extends Build {
 
     def clean()
     {
-        ant.delete(dir:"${env.distribution}/RapidServer");
+        ant.delete(dir: "${env.distribution}/RapidServer");
     }
 
     def runTest(String testClassDir, List classPaths, String outputXmlDir, String outputXmlFile) {
         //ant.echo(message: "Running all tests for test class " + testClass + " and will output xml results to " + outputXmlDir + "/" + outputXmlFile);
-       // ant.delete(dir: outputXmlDir);
+        // ant.delete(dir: outputXmlDir);
         ant.mkdir(dir: outputXmlDir);
         ant.junit(printsummary: "yes", haltonfailure: "no", fork: "false", showoutput: "true") {
             ant.classpath
             {
-                ant.path(refid:"classpath");
-                ant.path(location:"${testClassDir}");
-                classPaths.each{classPathEntry->
-                    ant.path(location:classPathEntry);    
+                ant.path(refid: "classpath");
+                ant.path(location: "${testClassDir}");
+                classPaths.each {classPathEntry ->
+                    ant.path(location: classPathEntry);
                 }
             }
             ant.formatter(type: "xml");
-            ant.batchtest(fork:"false", todir:outputXmlDir) {
-                ant.fileset(dir:"${testClassDir}")
-                {
-                    ant.include(name:"**/*Test.class");
-                }
+            ant.batchtest(fork: "false", todir: outputXmlDir) {
+                ant.fileset(dir: "${testClassDir}")
+                        {
+                            ant.include(name: "**/*Test.class");
+                        }
             }
 
             ant.formatter(type: "xml");
         }
         //   ant.junitreport( todir:"${env.distribution}/uiTestResults"){
-       //         ant.fileset(dir:"${env.distribution}/uiTestResults") {
-       //                       ant.include(name:"TEST-*.xml");
-       //                  }
+        //         ant.fileset(dir:"${env.distribution}/uiTestResults") {
+        //                       ant.include(name:"TEST-*.xml");
+        //                  }
         //          ant.report(format:"frames",todir:"${env.distribution}/uiTestResults/html")
         // }
-         ant.junit(printsummary: "yes", haltonfailure: "no"){
-        ant.batchtest(fork:"yes",todir:"${env.distribution}/uiTestResults"){
-            ant.fileset(dir:"${env.distribution}/uiTestResults"){
-                  ant.include(name:"TEST-*.xml");
+        ant.junit(printsummary: "yes", haltonfailure: "no") {
+            ant.batchtest(fork: "yes", todir: "${env.distribution}/uiTestResults") {
+                ant.fileset(dir: "${env.distribution}/uiTestResults") {
+                    ant.include(name: "TEST-*.xml");
+                }
             }
         }
-         }
-
 
     }
-
-
 
 }

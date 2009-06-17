90f1a45d215990ef07083748411ead03b7131a1b
fadime <fadime@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-06-17 13:36:19 +0000

    CMDB-1450 - Implement automated selenium test to test a scheduled cron script
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@7104 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RcmdbCommons/src/groovy/com/ifountain/rcmdb/test/util/SeleniumTestCase.groovy
+++ RcmdbCommons/src/groovy/com/ifountain/rcmdb/test/util/SeleniumTestCase.groovy
@@ -22,8 +22,7 @@ class SeleniumTestCase extends SeleneseTestCase {
 
     public static void suiteSetUp(url, browser) {
 
-       // selenium = new DefaultSelenium(SeleniumTestUtils.getSeleniumServerHost(),
-        selenium = new DefaultSelenium("192.168.1.111",
+        selenium = new DefaultSelenium(SeleniumTestUtils.getSeleniumServerHost(),
                 Integer.parseInt(SeleniumTestUtils.getSeleniumServerPort()), browser, url);
         selenium.start();
         selenium.setTimeout("30000");

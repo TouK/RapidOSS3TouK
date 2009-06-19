54df61254d81847958e77d01dd9513499f117f8d
ender <ender@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-06-19 07:43:53 +0000

    RrdUtils directory: supplies rrd database utilities
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@7148 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

---
+++ RapidCMDB/src/groovy/com/ifountain/rcmdb/rrd/Grapher.groovy
@@ -0,0 +1,161 @@
+package com.ifountain.rcmdb.rrd
+
+import org.jrobin.core.RrdDef
+import org.jrobin.graph.RrdGraphDef
+import java.awt.Color
+import org.jrobin.graph.RrdGraph
+
+/**
+* Created by IntelliJ IDEA.
+* User: ifountain
+* Date: Jun 18, 2009
+* Time: 9:24:26 AM
+*/
+class Grapher {
+    public static final String DATABASE_NAME = "dbname";
+    public static final String DATASOURCE = "datasource";
+    public static final String START_TIME = "startTime";
+    public static final String END_TIME = "endTime";
+
+    public static final String FUNCTION = "function";
+    public static final String VERTICAL_LABEL = "vlabel";
+    public static final String HORIZONTAL_LABEL = "hlabel";
+    public static final String LINE = "line";
+    public static final String AREA = "area";
+    public static final String HRULE = "hrule";
+    public static final String VRULE = "vrule";
+    public static final String RPN = "rpn";
+    public static final String DSNAME = "dsname";
+    public static final String NAME = "name";
+    public static final String COLOR = "color";
+    public static final String DESCRIPTION = "description";
+    public static final String THICKNESS = "thickness";
+
+    public static byte[] graph(Map config){
+        
+        if (!config.containsKey(DATASOURCE)) {
+            throw new Exception("No Datasource specified");
+        }
+        if (!config.containsKey(START_TIME)) {
+            throw new Exception("Start time is not specified");
+        }
+        if (!config.containsKey(END_TIME)) {
+            throw new Exception("End time is not specified");
+        }
+
+        RrdGraphDef graphDef = new RrdGraphDef()
+
+        try{
+            graphDef.setStartTime(config.get(START_TIME))
+        }
+        catch(MissingMethodException e)
+        {
+            throw new Exception("Invalid timestamps specified")
+        }
+
+        try{
+            graphDef.setEndTime(config.get(END_TIME));
+        }
+        catch(MissingMethodException e)
+        {
+             throw new Exception("Invalid timestamps specified")
+        }
+
+
+        addDataSource(graphDef,config.get(DATASOURCE));
+
+        if(config.containsKey(AREA) ){
+            addArea(graphDef,config.get(AREA));
+        }
+
+        if(config.containsKey(LINE) ){
+            addLine(graphDef,config.get(LINE));
+        }
+
+        RrdGraph graph = new RrdGraph(graphDef);
+
+        return graph.getRrdGraphInfo().getBytes()
+                
+    }
+    public static void addDataSource(RrdGraphDef rdef, dslist){
+        boolean isSourceExist = false;
+        dslist.each{
+            if(!it.containsKey(NAME)){
+                 throw new Exception("Datasource distorted: Name of datasource is not specified");    
+            }
+            try{
+                rdef.datasource(it.get(NAME),it.get(RPN) )
+            }
+            catch(Exception ex){
+                if(!(it.containsKey(DSNAME) &&
+                        it.containsKey(DATABASE_NAME) &&   it.containsKey(FUNCTION))){
+                    throw new Exception("Datasource distorted");
+                }
+
+                try{
+                   rdef.datasource(it.get(NAME),it.get(DATABASE_NAME),it.get(DSNAME),it.get(FUNCTION) )
+                   isSourceExist = true;
+                }
+                catch(Exception e){
+                    e.printStackTrace()
+                    throw new Exception("Datasource distorted: " + e.getMessage())
+                }
+            }
+        }
+        if(!isSourceExist){
+            throw new Exception("There is no database selected")
+        }
+    }
+    public static void addArea(RrdGraphDef rdef, alist){
+         //sample area: graphDef.area("good", new Color(0, 0xFF, 0), "Good speed");
+         alist.each{
+            try{
+                String colorStr = it.get(COLOR);
+                if(colorStr.length()!=6){
+                    throw new Exception("Invalid color: "+colorStr);
+                }
+                Color color;
+                try{
+                    int r = Integer.parseInt(colorStr.substring(0,2),16);
+                    int g = Integer.parseInt(colorStr.substring(2,4),16);
+                    int b = Integer.parseInt(colorStr.substring(4,6),16);
+                    color = new Color(r,g,b );
+                }catch(Exception e){
+                    throw new Exception("Invalid color: "+colorStr);
+                }
+                rdef.area(it.get(NAME),color,it.get(DESCRIPTION) )
+            }
+            catch(Exception ex){
+                throw new Exception("Area distorted: " + ex.getMessage()) ;
+            }
+        }
+    }
+    public static void addLine(RrdGraphDef rdef, llist){
+         //sample line: graphDef.line("lineb", Color.GREEN, "Line B", 3);
+         llist.each{
+            try{
+                String colorStr = it.get(COLOR);
+                if(colorStr.length()!=6){
+                    throw new Exception("Invalid color: "+colorStr);
+                }
+                Color color;
+                try{
+                    int r = Integer.parseInt(colorStr.substring(0,2),16);
+                    int g = Integer.parseInt(colorStr.substring(2,4),16);
+                    int b = Integer.parseInt(colorStr.substring(4,6),16);
+                    color = new Color(r,g,b );
+                }catch(Exception e){
+                    throw new Exception("Invalid color: "+colorStr);
+                }
+                if(it.containsKey(THICKNESS) ){
+                    rdef.line(it.get(NAME),color,it.get(DESCRIPTION),it.get(THICKNESS) );
+                }else{
+                    rdef.line(it.get(NAME),color,it.get(DESCRIPTION) );
+                }
+            }
+            catch(Exception ex){
+                throw new Exception("Line distorted: " + ex.getMessage()) ;
+            }
+        }
+    }
+}
---
+++ RapidCMDB/src/groovy/com/ifountain/rcmdb/rrd/RrdUtils.groovy
@@ -0,0 +1,169 @@
+package com.ifountain.rcmdb.rrd
+
+import org.jrobin.core.RrdDef
+import org.jrobin.core.RrdDb
+import org.jrobin.core.DsDef
+import org.jrobin.core.ArcDef
+import org.jrobin.core.Sample
+
+/**
+* Created by IntelliJ IDEA.
+* User: ifountain
+* Date: Jun 17, 2009
+* Time: 9:37:31 AM
+*/
+class RrdUtils {
+    public static final String DATABASE_NAME = "databaseName";
+    public static final String DATASOURCE = "datasource";
+    public static final String START_TIME = "startTime";
+    public static final String ARCHIVE = "archive";
+    public static final String STEP = "step";
+
+    public static final String NAME = "name";
+    public static final String TYPE = "type";
+    public static final String HEARTBEAT= "heartbeat";
+    public static final String MAX = "max";
+    public static final String MIN = "min";
+
+    public static final String FUNCTION = "function";
+    public static final String XFF = "xff"
+    public static final String STEPS = "steps"
+    public static final String ROWS = "rows"
+
+
+    public static RrdDef rrdDef;
+    public static RrdDb rrdDb;
+
+    public static void createDatabase(Map config) {
+        if (!config.containsKey(DATABASE_NAME)) {
+            throw new Exception("Database name is not specified");
+        }
+
+        if (!config.containsKey(DATASOURCE)) {
+            throw new Exception("No Datasource specified");
+        }
+        if (!config.containsKey(ARCHIVE)) {
+            throw new Exception("No archive specified");
+        }
+
+        rrdDef = new RrdDef(config.get(DATABASE_NAME));
+
+        if (config.containsKey(START_TIME) || config.containsKey("${START_TIME}"))
+        {
+            try {
+                rrdDef.setStartTime(config.get(START_TIME))
+            }
+            catch (MissingMethodException e) {
+                throw new Exception("Start time is not valid");
+            }
+        }
+
+        if (config.containsKey(STEP))
+        {
+            try {
+                rrdDef.setStep(config.get(STEP))
+            }
+            catch (MissingMethodException e) {
+                throw new Exception("Step is not valid");
+            }
+        }
+
+        try {
+            rrdDef.addDatasource(getDsDefs(config.get(DATASOURCE)));
+        } catch (Exception ex) {
+            throw new Exception("At least one Datasource is distorted: " + ex.getMessage())
+        }
+        try {
+            rrdDef.addArchive(getArcDefs(config.get(ARCHIVE)));
+        } catch (Exception ex) {
+            throw new Exception("At least one Archive is distorted: " + ex.getMessage())
+        }
+
+        rrdDb = new RrdDb(rrdDef);
+        rrdDb.close();
+    }
+
+    public static DsDef[] getDsDefs(list){
+        def dsList = [];
+        list.each{
+           double min = Double.NaN
+           double max = Double.NaN
+           if(it.containsKey(MIN) ){
+               min = it.get(MIN)
+           }
+           if(it.containsKey(MAX)){
+               max = it.get(MAX)
+           }
+           DsDef dsTemp = new DsDef(it.get(NAME),it.get(TYPE),it.get(HEARTBEAT), min, max)
+           dsList.add(dsTemp);
+       }
+       return dsList as DsDef[]
+    }
+
+    public static ArcDef[] getArcDefs(list){
+        def arcList = [];
+        list.each{
+           ArcDef arcTemp = new ArcDef(it.get(FUNCTION),it.get(XFF),it.get(STEPS), it.get(ROWS))
+           arcList.add(arcTemp);
+       }
+       return arcList as ArcDef[]
+    }
+
+    public static void updateData(String dbname, String data){
+        RrdDb rrdDb = new RrdDb(dbname);
+
+        Sample sample = rrdDb.createSample();
+        sample.setAndUpdate(data);
+        rrdDb.close();
+    }
+    public static void updateData(String dbname, String[] data){
+        RrdDb rrdDb = new RrdDb(dbname);
+
+        Sample sample = rrdDb.createSample();
+        for(int i=0; i<data.length; i++){
+            sample.setAndUpdate(data[i]);
+        }
+        rrdDb.close();
+    }
+
+    public static RrdDef getRrdDef() {
+        return rrdDef
+    }
+    public static RrdDb getRrdDb(){ 
+        return rrdDb;
+    }
+    public static byte[] graph(Map config){
+        return Grapher.graph(config);
+    }
+    
+    /*
+    public static Calendar getFormattedStartTime(String str){
+        def date = str.trim().split(",");
+        Calendar cal = Calendar.getInstance();
+        cal.clear()
+        int year,month,day,hour=0,min=0,sec=0;
+        if(date.length==1){
+           def ymd = date[0].split("-");
+           if(ymd.length!=3){
+               throw new Exception("date is not valid")
+           }
+           year = Integer.parseInt(ymd[0]);
+           month = Integer.parseInt(ymd[1]);
+           day = Integer.parseInt(ymd[2])
+        }
+        if(date.length==2){
+           def hms = date[1].split(":");
+           if(hms.length!=3){
+               throw new Exception("time is not valid")
+           }
+           hour = Integer.parseInt(hms[0].trim());
+           min = Integer.parseInt(hms[1]);
+           sec = Integer.parseInt(hms[2])
+        }
+        println hour +" "+min+" "+sec
+        cal.set(year, month ,day,hour,min,sec);
+
+        return cal;
+    }
+    */
+}

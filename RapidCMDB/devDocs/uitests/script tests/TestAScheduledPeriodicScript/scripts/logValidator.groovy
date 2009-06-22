822c11bbb6279a28ab8e6573dab4ed2b367392cd
fadime <fadime@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-06-22 05:51:10 +0000

    CMDB-1450 - Implement automated selenium test to test a scheduled cron script
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@7178 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/devDocs/uitests/script tests/TestAScheduledPeriodicScript/scripts/logValidator.groovy
+++
@@ -1,6 +0,0 @@
-
- import org.apache.commons.lang.StringUtils
-
-  def logFile = new File(params.file);
-  def log = logFile.getText();
-  return StringUtils.countMatches (log, "WARN: Hello from periodic")
--- RapidCMDB/devDocs/uitests/script tests/testAScheduledCronScriptFiles/scripts/logValidator.groovy
+++
@@ -1,8 +0,0 @@
-
- import org.apache.commons.lang.StringUtils
-
-
-  def logFile = new File(params.file);
-  def log = logFile.getText();
-
-  return StringUtils.countMatches (log, "Hello from cron")

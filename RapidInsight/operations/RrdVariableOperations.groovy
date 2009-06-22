c0f619e7098d705f05ad5cb61590614e58ce1f00
mustafag <mustafag@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-06-22 15:33:32 +0000

    CMDB-1484 - Time Series Graphs With Rrd &  JRobin RrdVariableOperations has two methods and  "createDBConfig()" successfully passed tests on RrdVariableOperationsTest
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@7201 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidInsight/operations/RrdVariableOperations.groovy
+++ RapidInsight/operations/RrdVariableOperations.groovy
@@ -1,6 +1,54 @@
 
-    
-    public class RrdVariableOperations extends com.ifountain.rcmdb.domain.operation.AbstractDomainOperation
+import com.ifountain.rcmdb.rrd.RrdUtils;
+
+public class RrdVariableOperations extends com.ifountain.rcmdb.domain.operation.AbstractDomainOperation
+{
+    def createDB()
     {
+        RrdUtils.createDatabase (createDBConfig);
     }
+
+    private def Map createDBConfig()
+    {
+        def dbConfig = [:]
+
+        //there could be change in file name
+        dbConfig[RrdUtils.DATABASE_NAME] = file
+        dbConfig[RrdUtils.START_TIME] = startTime
+        dbConfig[RrdUtils.STEP] = step
+
+        def datapoint = [:]
+
+        datapoint[RrdUtils.NAME] = name
+        datapoint[RrdUtils.TYPE] = type
+        datapoint[RrdUtils.HEARTBEAT] = heartbeat
+        datapoint[RrdUtils.MAX] = max
+        datapoint[RrdUtils.MIN] = min
+
+        dbConfig[RrdUtils.DATASOURCE] = [datapoint]
+
+        def archiveList = []
+
+        archives.each
+        {
+            def archive = [:]
+            archive[RrdUtils.FUNCTION] = it.function
+            archive[RrdUtils.XFF] = it.xff
+            archive[RrdUtils.STEPS] = it.step
+            archive[RrdUtils.ROWS] = it.row
+
+            archiveList.add(archive)
+        }
+
+        dbConfig[RrdUtils.ARCHIVE] = archiveList
+
+        return dbConfig
+    }
+
+
+
+    
+    
+
+}
     

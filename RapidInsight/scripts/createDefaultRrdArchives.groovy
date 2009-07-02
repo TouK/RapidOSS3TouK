831955ea484ab1600dc1e260466aa92e71a9be1f
ender <ender@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-07-02 10:34:45 +0000

    CMDB-1484 - Time Series Graphs With Rrd &  JRobin
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@7408 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

---
+++ RapidInsight/scripts/createDefaultRrdArchives.groovy
@@ -0,0 +1,5 @@
+
+RrdArchive.add(name:"1hour",function: "AVERAGE",xff: 0.5,step:1,row:60);
+RrdArchive.add(name:"6hours",function:"AVERAGE",xff:0.5,step:4,row:90);
+RrdArchive.add(name:"12hours",function:"AVERAGE",xff:0.5,step:6,row:120);
+RrdArchive.add(name:"24hours",function:"AVERAGE",xff:0.5,step:9,row:160);

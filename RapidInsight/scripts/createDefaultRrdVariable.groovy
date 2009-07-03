f61011c72ce105f96d7b15b044dbfdc86ca1d698
mustafag <mustafag@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-07-03 08:24:15 +0000

    Sample RrdVariable creation and data update script is added and tested
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@7434 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

---
+++ RapidInsight/scripts/createDefaultRrdVariable.groovy
@@ -0,0 +1,36 @@
+/**
+ * User: ifountain
+ * Date: Jul 3, 2009
+ * Time: 10:36:17 AM
+ */
+
+/*
+ * Sample RrdVariable
+ *		It uses default archives, one hour, twelve hours, and one day.
+ *
+ */
+
+//predefined RrdArchives
+def oneHour = RrdArchive.get(name:"1hour")
+def twelveHours = RrdArchive.get(name:"12hours")
+def oneDay = RrdArchive.get(name:"1day")
+
+long ntime = Date.now() - 50 * 300000
+
+def sampleRrdVariable = RrdVariable.add(
+								name:"sampleRrdVariable",					//name of RrdVariable
+								resource:"default",							//Resource of this variable. Mostly for groupping
+								type:"GAUGE",								//Type of this variable. See documentation for further understanding
+								heartbeat:600,								//heartbeat of this variable. See documentation for further understanding
+								startTime: ntime,							//start time of this variable
+								step:300,									//step interval between data updating points
+								archives: [oneHour, twelveHours, oneDay],	//archives of this variable
+						        file: "sampleRrdDB.rrd"                     //file name of rrd db
+                        )
+
+sampleRrdVariable.createDB()
+
+50.times
+{
+	sampleRrdVariable.updateDB(time: (ntime + (it+1) * 300000) , value : new Random().nextInt(1000))
+}
---
+++ RapidInsight/test/unit/scriptTests/CreateDefaultRrdVariableTest.groovy
@@ -0,0 +1,51 @@
+package scriptTests
+
+import com.ifountain.rcmdb.test.util.RapidCmdbWithCompassTestCase
+import com.ifountain.rcmdb.test.util.scripting.ScriptManagerForTest
+import com.ifountain.rcmdb.test.util.CompassForTests
+
+/**
+* User: ifountain
+* Date: Jul 3, 2009
+* Time: 10:39:03 AM
+*/
+class CreateDefaultRrdVariableTest extends RapidCmdbWithCompassTestCase {
+
+    def rrdVariable;
+
+    public void setUp() {
+        super.setUp();
+        def rrdArchive = this.class.classLoader.loadClass("RrdArchive");
+        rrdVariable = this.class.classLoader.loadClass("RrdVariable");
+        def rrdVariableOperations = this.class.classLoader.loadClass("RrdVariableOperations")
+        initialize([rrdArchive, rrdVariable], []);
+        CompassForTests.addOperationSupport(rrdVariable, rrdVariableOperations);
+        initializeScriptManager();
+    }
+
+    public void tearDown() {
+        new File("sampleRrdDB.rrd").delete();
+        super.tearDown();
+    }
+
+    void initializeScriptManager() {
+        def base_directory = getWorkspacePath()+"/RapidModules/RapidInsight/scripts"
+        println "base path is :"+new File(base_directory).getCanonicalPath();
+        ScriptManagerForTest.initialize(gcl,base_directory);
+        ScriptManagerForTest.addScript('createDefaultRrdArchives');
+        ScriptManagerForTest.addScript('createDefaultRrdVariable');
+    }
+
+    public void testVariableCreatedSuccessfully(){
+        //default archives must be available in RI to create default rrd variable
+        def archiveResult = ScriptManagerForTest.runScript("createDefaultRrdArchives",[:]);
+
+        def result = ScriptManagerForTest.runScript("createDefaultRrdVariable",[:]);
+
+        def variable = rrdVariable.get(name:"sampleRrdVariable");
+
+        assertTrue("Sample rrd variable is not created", variable!=null);
+        assertTrue("Sample rrd db file is not created", new File("sampleRrdDB.rrd").exists());
+    }
+
+}

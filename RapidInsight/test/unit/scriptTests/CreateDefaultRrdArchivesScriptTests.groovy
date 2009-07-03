3f845bdac4b2eb17d30e0959f817619a6539c4f2
mustafag <mustafag@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-07-03 07:15:50 +0000

    Assertion failure is resolved
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@7433 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidInsight/test/unit/scriptTests/CreateDefaultRrdArchivesScriptTests.groovy
+++ RapidInsight/test/unit/scriptTests/CreateDefaultRrdArchivesScriptTests.groovy
@@ -9,38 +9,50 @@ import com.ifountain.rcmdb.test.util.scripting.ScriptManagerForTest
  * To change this template use File | Settings | File Templates.
  */
 class CreateDefaultRrdArchivesScriptTests  extends RapidCmdbWithCompassTestCase  {
+
     def rrdArchive;
-   public void setUp() {
+
+    public void setUp() {
         super.setUp();
         rrdArchive = this.class.classLoader.loadClass("RrdArchive");
-       initialize([rrdArchive], []);
-       initializeScriptManager();
+        initialize([rrdArchive], []);
+        initializeScriptManager();
     }
 
     public void tearDown() {
         super.tearDown();
     }
-    void initializeScriptManager()
-    {
+    
+    void initializeScriptManager() {
         def base_directory = getWorkspacePath()+"/RapidModules/RapidInsight/scripts"
         println "base path is :"+new File(base_directory).getCanonicalPath();
         ScriptManagerForTest.initialize(gcl,base_directory);
         ScriptManagerForTest.addScript('createDefaultRrdArchives');
-
     }
-    public void testArchivesCreated(){
+
+    public void testArchivesCreatedSuccessfully(){
         def result=ScriptManagerForTest.runScript("createDefaultRrdArchives",[:]);
 
-        //assertEquals(4,rrdArchive.count());
-        
         def arc1 = rrdArchive.get(name:"1hour");
         def arc2 = rrdArchive.get(name:"6hours");
         def arc3 = rrdArchive.get(name:"12hours");
-        def arc4 = rrdArchive.get(name:"24hours");
+        def arc4 = rrdArchive.get(name:"1day");
+        def arc5 = rrdArchive.get(name:"1week");
+        def arc6 = rrdArchive.get(name:"2weeks");
+        def arc7 = rrdArchive.get(name:"1month");
+        def arc8 = rrdArchive.get(name:"3months");
+        def arc9 = rrdArchive.get(name:"6months");
+        def arc10 = rrdArchive.get(name:"1year");
 
         assertTrue("Archive for 1 hour is not created.",arc1!=null);
         assertTrue("Archive for 6 hours is not created.",arc2!=null);
         assertTrue("Archive for 12 hours is not created.",arc3!=null);
-        assertTrue("Archive for 24 hours is not created.",arc4!=null);
+        assertTrue("Archive for 1 day is not created.",arc4!=null);
+        assertTrue("Archive for 1 week is not created.",arc5!=null);
+        assertTrue("Archive for 2 weeks is not created.",arc6!=null);
+        assertTrue("Archive for 1 month is not created.",arc7!=null);
+        assertTrue("Archive for 3 months is not created.",arc8!=null);
+        assertTrue("Archive for 6 months is not created.",arc9!=null);
+        assertTrue("Archive for 1 year is not created.",arc10!=null);
     }
 }

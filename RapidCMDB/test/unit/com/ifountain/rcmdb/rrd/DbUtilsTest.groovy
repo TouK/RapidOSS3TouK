320eb107c4cd402afdcb122813aa298691615c81
ender <ender@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-07-16 08:17:19 +0000

    CMDB-1484 - Time Series Graphs With Rrd &  JRobin
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@7593 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/test/unit/com/ifountain/rcmdb/rrd/DbUtilsTest.groovy
+++ RapidCMDB/test/unit/com/ifountain/rcmdb/rrd/DbUtilsTest.groovy
@@ -797,6 +797,11 @@ class DbUtilsTests extends RapidCoreTestCase {
         String xmlFile = rrdFileName+".xml";
         DbUtils.createXml(result,xmlFile);
     }
+    public void testXml() throws Exception{
+//        RrdDb rrddb = new RrdDb("D:/RapidServer/RapidSuite/rrdFiles/sampleRrdDB.rrd");
+        DbUtils.fetchData("D:/RapidServer/RapidSuite/rrdFiles/sampleRrdDB.rrd");
+//        rrddb.exportXml ("D:/RapidServer/RapidSuite/rrdFiles/sampleRrdDB.rrd.xml");
+    }
 
     public void testCreateXmlForMultipleDataSources() throws Exception{
         createDatabase();

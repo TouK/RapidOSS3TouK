199baddf0fa64c4698235c328975aa1d70d502d4
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-07-24 10:02:45 +0000

    CMDB-1592 - Smarts topology connector bug -topologyConfiguration exceptions goes to RapidServer.log
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@7728 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RcmdbCommons/src/groovy/com/ifountain/rcmdb/test/util/LoggerForTest.groovy
+++ RcmdbCommons/src/groovy/com/ifountain/rcmdb/test/util/LoggerForTest.groovy
@@ -1,36 +1,45 @@
 package com.ifountain.rcmdb.test.util
+
+import org.apache.log4j.Logger
+
 /**
- * Created by IntelliJ IDEA.
- * User: admin
- * Date: Jul 24, 2009
- * Time: 11:52:03 AM
- * To change this template use File | Settings | File Templates.
- */
+* Created by IntelliJ IDEA.
+* User: admin
+* Date: Jul 24, 2009
+* Time: 11:52:03 AM
+* To change this template use File | Settings | File Templates.
+*/
 
 
-class LoggerForTest
+class LoggerForTest  extends Logger
 {
     def logHistory=null;
     public LoggerForTest()
     {
+        super("logger.for.test");
         clear();
     }
-    def info(message)
+    public void info(Object message)
     {
         logHistory["INFO"].add(message);
     }
-    def debug(message)
+    public void debug(Object message)
     {
         logHistory["DEBUG"].add(message);
     }
-    def warn(message)
+    public void warn(Object message)
     {
         logHistory["WARN"].add(message);
     }
-    def warn(message,exception)
+    public void warn(Object message,exception)
     {
         logHistory["WARN"].add([message:message,exception:exception]);
     }
+    public boolean isDebugEnabled()
+    {
+        return true;
+    }
+
     def clear()
     {
         logHistory=[:];
@@ -39,9 +48,6 @@ class LoggerForTest
         logHistory["WARN"]=[];
     }
 
-    def isDebugEnabled()
-    {
-        return true;
-    }
+
 
 }

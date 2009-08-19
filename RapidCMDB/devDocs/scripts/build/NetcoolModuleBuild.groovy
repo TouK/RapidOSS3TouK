d49aa68325f3aa1f7dabbca9ffe314edf55a5aa6
mustafa <mustafa@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-08-19 08:38:30 +0000

    CMDB-1537 - Netcool Severity & Escalate conversions are not imported, the audit log does not have string values of severit etc. UI components have hardcoded mappings , these mappings should be generated from imported netcool conversions
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@7891 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidCMDB/devDocs/scripts/build/NetcoolModuleBuild.groovy
+++ RapidCMDB/devDocs/scripts/build/NetcoolModuleBuild.groovy
@@ -22,31 +22,31 @@ package build;
  * Time: 4:45:18 PM
  */
 class NetcoolModuleBuild extends Build {
-	def version = "$env.rapid_netcool/RINetcoolVersion.txt";
-	 
+    def version = "$env.rapid_netcool/RINetcoolVersion.txt";
+
     static void main(String[] args) {
         NetcoolModuleBuild netcoolModuleBuild = new NetcoolModuleBuild();
         netcoolModuleBuild.build();
     }
     def clean(distDir) {
-    	if(distDir.equals(env.dist_modules)){
-	        ant.delete(dir: env.dist_modules);
-	        ant.mkdir(dir: env.dist_modules);
-    	}
+        if (distDir.equals(env.dist_modules)) {
+            ant.delete(dir: env.dist_modules);
+            ant.mkdir(dir: env.dist_modules);
+        }
     }
-    
+
     def build() {
-    	build(env.dist_modules);
+        build(env.dist_modules);
     }
-    
+
     def build(distDir) {
-    	def rapidSuiteDir = "${distDir}/RapidSuite";
-    	def versionInBuild = "${rapidSuiteDir}/RINetcoolVersion.txt";
+        def rapidSuiteDir = "${distDir}/RapidSuite";
+        def versionInBuild = "${rapidSuiteDir}/RINetcoolVersion.txt";
         clean(distDir);
         ant.copy(file: version, tofile: versionInBuild);
         setVersionAndBuildNumber(versionInBuild);
         def versionDate = getVersionWithDate();
-        ant.copy(todir: "$rapidSuiteDir/grails-app",overwrite:true) {
+        ant.copy(todir: "$rapidSuiteDir/grails-app", overwrite: true) {
             ant.fileset(dir: "$env.rapid_netcool/grails-app")
         }
         ant.copy(todir: "$rapidSuiteDir/operations") {
@@ -54,8 +54,14 @@ class NetcoolModuleBuild extends Build {
         }
         ant.copy(todir: "$rapidSuiteDir/src/groovy") {
             ant.fileset(dir: "$env.rapid_netcool/src/groovy")
+            {
+                if (!TEST) {
+                    ant.exclude(name: "**/test/**")
+                    ant.exclude(name: "**/*Test*")
+                }
+            }
         }
-        if(TEST)
+        if (TEST)
         {
             //for token definition files to be found , should be changed
             ant.copy(todir: "$rapidSuiteDir/conversion") {
@@ -70,44 +76,44 @@ class NetcoolModuleBuild extends Build {
             }
         }
         copyDependentJars("$rapidSuiteDir/lib");
-        
+
         ant.copy(toDir: "${rapidSuiteDir}/generatedModels/grails-app/domain") {
             ant.fileset(file: "${env.rapid_netcool}/applications/RapidInsightForNetcool/grails-app/domain/*.groovy");
         }
         ant.copy(todir: rapidSuiteDir) {
-            ant.fileset(dir: "$env.rapid_netcool/applications/RapidInsightForNetcool"){
+            ant.fileset(dir: "$env.rapid_netcool/applications/RapidInsightForNetcool") {
                 ant.exclude(name: "**/netcoolDataGenerator.groovy")
                 ant.exclude(name: "**/NetcoolDemoValues.groovy")
                 ant.exclude(name: "**/NetcoolRealValues.groovy")
                 ant.exclude(name: "**/demoGenerator.groovy")
             }
         }
-        ant.copy(toDir: "${rapidSuiteDir}/../solutions",overwrite:true) {
+        ant.copy(toDir: "${rapidSuiteDir}/../solutions", overwrite: true) {
             ant.fileset(file: "${env.rapid_insight}/solutions/**");
         }
-        ant.copy(toDir: "${rapidSuiteDir}/../solutions",overwrite:true) {
+        ant.copy(toDir: "${rapidSuiteDir}/../solutions", overwrite: true) {
             ant.fileset(file: "${env.rapid_netcool}/applications/solutions/**");
         }
-//        replaceJavascriptAndCss("${rapidSuiteDir}/grails-app/views/layouts/indexLayout.gsp", "/RapidSuite/indexLayout_${buildNo}.js", "/RapidSuite/indexLayout_${buildNo}.css")
-        if(distDir.equals(env.dist_modules)){
-	        ant.zip(destfile: "$env.distribution/NetcoolPlugin$versionDate" + ".zip") {
-	            ant.zipfileset(dir: "$env.dist_modules")
-	        }
+        //        replaceJavascriptAndCss("${rapidSuiteDir}/grails-app/views/layouts/indexLayout.gsp", "/RapidSuite/indexLayout_${buildNo}.js", "/RapidSuite/indexLayout_${buildNo}.css")
+        if (distDir.equals(env.dist_modules)) {
+            ant.zip(destfile: "$env.distribution/NetcoolPlugin$versionDate" + ".zip") {
+                ant.zipfileset(dir: "$env.dist_modules")
+            }
         }
         if (TEST) {
             ant.copy(todir: "${rapidSuiteDir}/test") {
                 ant.fileset(dir: "$env.rapid_netcool/test")
-            }            
+            }
         }
         println "Netcool Build Done";
     }
 
-     def copyDependentJars(dirToCopyTo) {
-        if(TEST)
+    def copyDependentJars(dirToCopyTo) {
+        if (TEST)
         {
             ant.copy(file: (String) classpath.getProperty("antlr-3_1_1-runtime_jar"), toDir: dirToCopyTo);
         }
-        
+
     }
 
 }

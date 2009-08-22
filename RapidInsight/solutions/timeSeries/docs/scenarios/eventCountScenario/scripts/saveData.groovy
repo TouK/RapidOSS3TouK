a4c191b47015ef3ca547f6d4751ab3b79bbcd368
mustafa <mustafa@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-08-22 12:20:00 +0000

    timeseries scenario fix
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@7920 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidInsight/solutions/timeSeries/docs/scenarios/eventCountScenario/scripts/saveData.groovy
+++ RapidInsight/solutions/timeSeries/docs/scenarios/eventCountScenario/scripts/saveData.groovy
@@ -7,7 +7,7 @@ def allEvents = RrdVariable.get(name:"allEvents")
 def criticalEvents = RrdVariable.get(name:"criticalEvents")
 def majorEvents = RrdVariable.get(name:"majorEvents")
 
-//calculate data to be inserted. In this scenario, we will search event counts according to their severity
+//calculate data to be inserted. In this scenario, we will search event counts according to their severity      
 def allEventCount = RsEvent.countHits("alias:*");
 def criticalEventCount = RsEvent.countHits("severity:5");
 def majorEventCount = RsEvent.countHits("severity:4");
@@ -15,5 +15,5 @@ def majorEventCount = RsEvent.countHits("severity:4");
 //Time series database will be updated by found event counts
 allEvents.updateDB(allEventCount)
 // time parameter can be specified in updateDB optionally. If it is not specified, it will be assigned to current time
-criticalEventCount.updateDB(criticalEventCount, Date.now())
-majorEventCount.updateDB(majorEventCount)
+criticalEvents.updateDB(criticalEventCount, Date.now())
+majorEvents.updateDB(majorEventCount)

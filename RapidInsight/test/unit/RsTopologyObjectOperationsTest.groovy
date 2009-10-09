dee84f9e827109584e429ca27a779a727e4a1835
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-10-09 14:11:44 +0000

    test fix to remove old overrides
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@8203 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidInsight/test/unit/RsTopologyObjectOperationsTest.groovy
+++ RapidInsight/test/unit/RsTopologyObjectOperationsTest.groovy
@@ -14,6 +14,7 @@ class RsTopologyObjectOperationsTest extends RapidCmdbWithCompassTestCase {
 
     public void setUp() {
         super.setUp();
+        clearMetaClasses();
         initialize([RsTopologyObject,RsApplication], []);
         CompassForTests.addOperationSupport(RsTopologyObject,RsTopologyObjectOperations);
         RsApplicationTestUtils.initializeRsApplicationOperations (RsApplication);
@@ -21,9 +22,15 @@ class RsTopologyObjectOperationsTest extends RapidCmdbWithCompassTestCase {
     }
 
     public void tearDown() {
-       super.tearDown();
+       super.tearDown();       
     }
-
+    public void clearMetaClasses()
+    {
+        ExpandoMetaClass.disableGlobally();
+        GroovySystem.metaClassRegistry.removeMetaClass(RsTopologyObject);
+        GroovySystem.metaClassRegistry.removeMetaClass(RsTopologyObjectOperations);
+        ExpandoMetaClass.enableGlobally();
+    }
     public void testGetState()
     {
         def object=RsTopologyObject.add(name:"testobj");

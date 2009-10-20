4e4b7841981039e1d562991cd1dded865f7f8df9
sezgin <sezgin@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-10-20 08:10:01 +0000

    CMDB-1706 - Implement Mobile UI's to support browsers used mobile phones other than Iphone
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@8252 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RcmdbCommons/src/groovy/com/ifountain/rcmdb/mobile/MobileUtils.groovy
+++ RcmdbCommons/src/groovy/com/ifountain/rcmdb/mobile/MobileUtils.groovy
@@ -10,16 +10,18 @@ class MobileUtils {
 
     public static def isMobile(request) {
         String userAgent = String.valueOf(request.getHeader("user-agent")).toLowerCase();
-        if (userAgent.indexOf("mobile") > -1
-                || userAgent.indexOf("ipod") > -1
-                || userAgent.indexOf("iphone") > -1
-                || userAgent.indexOf("blackberry") > -1
-                || userAgent.indexOf("iemobile") > -1
-                || userAgent.indexOf("opera mini") > -1
-                || userAgent.indexOf("opera mobi") > -1) {
-            return true;
+        def userAgents = [
+                "mobile", "ipod", "iphone", "blackberry", "iemobile", "opera mini", "opera mobi",
+                "sonyericsson", "nokia", "panasonic", "lg-", "lge-", "lg/", "mot-", "android", "sie-",
+                "windows ce", "htc-", "symbian", "palmos", "palmsource", "opwv", "docomo", "samsung", "sec-",
+                "up.b", "up/"
+        ];
+        for(int i = 0; i < userAgents.size(); i ++){
+            def uA = userAgents[i]
+            if(userAgent.indexOf(uA) > -1){
+                return true;
+            }
         }
-
         return false;
     }
 

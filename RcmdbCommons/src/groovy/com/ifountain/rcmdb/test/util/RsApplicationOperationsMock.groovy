962cb11b73359e871f0cf64197ef6b781769d9e5
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-11-24 07:44:27 +0000

    CMDB-1754 - Notification system should use the searchClass of searchQuery if exists , if not exist should use RsEvent
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@8457 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RcmdbCommons/src/groovy/com/ifountain/rcmdb/test/util/RsApplicationOperationsMock.groovy
+++ RcmdbCommons/src/groovy/com/ifountain/rcmdb/test/util/RsApplicationOperationsMock.groovy
@@ -1,6 +1,6 @@
 package com.ifountain.rcmdb.test.util
 
-import application.RsApplicationOperations
+import org.codehaus.groovy.grails.commons.ApplicationHolder
 
 /**
 * Created by IntelliJ IDEA.
@@ -9,10 +9,23 @@ import application.RsApplicationOperations
 * Time: 5:46:51 PM
 * To change this template use File | Settings | File Templates.
 */
-class RsApplicationOperationsMock extends RsApplicationOperations{
+class RsApplicationOperationsMock extends com.ifountain.rcmdb.domain.operation.AbstractDomainOperation{
 
     public static def getUtility(utilityName)
     {
        return RsApplicationTestUtils.loadUtility(utilityName).newInstance();
+    }
+
+    public static def getModelClass(String modelName)
+    {
+        def modelClass=ApplicationHolder.application.getDomainClass(modelName);
+        if(modelClass)
+        {
+            return modelClass.clazz;
+        }
+        else
+        {
+            throw new Exception("Model Class ${modelName} does not exist");
+        }
     }
 }

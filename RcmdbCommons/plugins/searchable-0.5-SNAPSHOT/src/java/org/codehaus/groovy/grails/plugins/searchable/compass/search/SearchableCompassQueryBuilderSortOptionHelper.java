638bbfc26f681adbb5327258353a4da832f6b0be
sezgin <sezgin@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-12-17 11:41:30 +0000

    CMDB-1782 - Search with sorting on multiple fields
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@8568 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RcmdbCommons/plugins/searchable-0.5-SNAPSHOT/src/java/org/codehaus/groovy/grails/plugins/searchable/compass/search/SearchableCompassQueryBuilderSortOptionHelper.java
+++ RcmdbCommons/plugins/searchable-0.5-SNAPSHOT/src/java/org/codehaus/groovy/grails/plugins/searchable/compass/search/SearchableCompassQueryBuilderSortOptionHelper.java
@@ -20,10 +20,12 @@ import org.compass.core.Compass;
 import org.compass.core.CompassQuery;
 import org.compass.core.CompassSession;
 import org.springframework.util.Assert;
+import org.apache.commons.lang.StringUtils;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.ArrayList;
 
 import com.ifountain.compass.CompassConstants;
 
@@ -36,83 +38,98 @@ import com.ifountain.compass.CompassConstants;
 public class SearchableCompassQueryBuilderSortOptionHelper implements SearchableCompassQueryBuilderOptionsHelper {
     public static final String DIRECTION = "direction";
     public static final String ORDER = "order";
-    public static final List VALID_SORT_DIRECTION_VALUES = Arrays.asList(new String[] { "asc", "desc", "auto", "reverse" });
+    public static final List VALID_SORT_DIRECTION_VALUES = Arrays.asList(new String[]{"asc", "desc", "auto", "reverse"});
 
     public CompassQuery applyOptions(GrailsApplication grailsApplication, Compass compass, CompassSession compassSession, CompassQuery compassQuery, Map options) {
         return addSort(compassQuery, options);
     }
-
+    //TODO: Added on Searchable Plugin: Search with sorting on multiple fields
+    
     public CompassQuery addSort(CompassQuery compassQuery, Map options) {
-        String sort = (String) options.get("sort");
+        Object sort = options.get("sort");
         if (sort == null) {
             return compassQuery;
         }
-        Object sortProperty = getSortProperty(sort);
-        CompassQuery.SortDirection direction = getSortDirection(sortProperty, options);
-
-        if (sortProperty instanceof CompassQuery.SortImplicitType) {
-            compassQuery = compassQuery.addSort((CompassQuery.SortImplicitType) sortProperty, direction);
+        Assert.isTrue(sort instanceof String || sort instanceof List, "sort option should be given as string or list");
+        List<String> sorts = null;
+        List<String> orders = null;
+        if (sort instanceof String) {
+            sorts = Arrays.asList(StringUtils.split((String) sort, ','));
         } else {
-            Assert.isInstanceOf(String.class, sortProperty, "Expected string");
-            CompassQuery.SortPropertyType sortType = getSortType((String)options.get("sortType"));
-            compassQuery = compassQuery.addSort((String) sortProperty, sortType, direction);
+            sorts = (List) sort;
+        }
+        if (!options.containsKey(ORDER) && !options.containsKey(DIRECTION)) {
+            orders = new ArrayList<String>();
+        } else {
+            Assert.isTrue((options.containsKey(ORDER) && !options.containsKey(DIRECTION)) || (!options.containsKey(ORDER) && options.containsKey(DIRECTION)), "Either specify a sort '" + ORDER + "' or '" + DIRECTION + "' or neither but not both");
+            Object order = options.get(DIRECTION);
+            if (order == null) {
+                order = options.get(ORDER);
+            }
+            Assert.isTrue(order instanceof String || order instanceof List, "order/direction option should be given as string or list");
+            if (order instanceof String) {
+                orders = Arrays.asList(StringUtils.split((String) order, ','));
+            } else {
+                orders = (List) order;
+            }
+        }
+        if (orders.size() < sorts.size()) {
+            while (orders.size() != sorts.size()) {
+                orders.add("auto");
+            }
+        }
+        for (int i = 0; i < sorts.size(); i++) {
+            String s = sorts.get(i);
+            String o = orders.get(i);
+            Object sortProperty = getSortProperty(s);
+            CompassQuery.SortDirection direction = getSortDirection(sortProperty, o);
+            if (sortProperty instanceof CompassQuery.SortImplicitType) {
+                compassQuery = compassQuery.addSort((CompassQuery.SortImplicitType) sortProperty, direction);
+            } else {
+                Assert.isInstanceOf(String.class, sortProperty, "Expected string");
+                CompassQuery.SortPropertyType sortType = getSortType((String) options.get("sortType"));
+                compassQuery = compassQuery.addSort((String) sortProperty, sortType, direction);
+            }
         }
         return compassQuery;
     }
 
     private Object getSortProperty(String sort) {
         Assert.notNull(sort, "sort cannot be null");
+        sort = sort.trim();
         if (sort.equals("SCORE")) {
             return CompassQuery.SortImplicitType.SCORE;
         }
-        return CompassConstants.UN_TOKENIZED_FIELD_PREFIX+sort;
+        return CompassConstants.UN_TOKENIZED_FIELD_PREFIX + sort;
     }
 
     private CompassQuery.SortPropertyType getSortType(String sortType) {
-        if(sortType == null)
-        {
-            return CompassQuery.SortPropertyType.AUTO;    
-        }
-        else if(sortType.equalsIgnoreCase("string"))
-        {
+        if (sortType == null) {
+            return CompassQuery.SortPropertyType.AUTO;
+        } else if (sortType.equalsIgnoreCase("string")) {
             return CompassQuery.SortPropertyType.STRING;
-        }
-        else if(sortType.equalsIgnoreCase("int") || sortType.equalsIgnoreCase("int"))
-        {
+        } else if (sortType.equalsIgnoreCase("int") || sortType.equalsIgnoreCase("int")) {
             return CompassQuery.SortPropertyType.INT;
-        }
-
-        else if(sortType.equalsIgnoreCase("float"))
-        {
+        } else if (sortType.equalsIgnoreCase("float")) {
             return CompassQuery.SortPropertyType.FLOAT;
-        }
-        else
-        {
+        } else {
             return CompassQuery.SortPropertyType.AUTO;
         }
     }
 
     /**
      * Get the CompassQuery.SortDirection for the given property and optional order/direction Map entry
+     *
      * @param property either CompassQuery.SortImplicitType.SCORE or a class property name (String)
-     * @param options a Map containg
+     * @param order 
      * @return
      */
-    public CompassQuery.SortDirection getSortDirection(Object property, Map options) {
+    private CompassQuery.SortDirection getSortDirection(Object property, String order) {
+        order = order.trim();
         Assert.notNull(property, "sort property cannot be null");
-        Assert.notNull(options, "options Map cannot be null");
-        if (!options.containsKey(ORDER) && !options.containsKey(DIRECTION)) {
-            return CompassQuery.SortDirection.AUTO;
-        }
-
-        Assert.isTrue((options.containsKey(ORDER) && !options.containsKey(DIRECTION)) || (!options.containsKey(ORDER) && options.containsKey(DIRECTION)), "Either specify a sort '" + ORDER + "' or '" + DIRECTION + "' or neither but not both");
-        String value = (String) options.get(DIRECTION);
-        if (value == null) {
-            value = (String) options.get(ORDER);
-        }
-        Assert.isTrue(VALID_SORT_DIRECTION_VALUES.contains(value), "The sort order/direction '" + value + "' is not a valid value");
+        Assert.isTrue(VALID_SORT_DIRECTION_VALUES.contains(order), "The sort order/direction '" + order + "' is not a valid value");
         return property.equals(CompassQuery.SortImplicitType.SCORE) ?
-                value.equals("asc") || value.equals("reverse") ? CompassQuery.SortDirection.REVERSE : CompassQuery.SortDirection.AUTO :
-                value.equals("asc") || value.equals("auto") ? CompassQuery.SortDirection.AUTO : CompassQuery.SortDirection.REVERSE;
+                order.equals("asc") || order.equals("reverse") ? CompassQuery.SortDirection.REVERSE : CompassQuery.SortDirection.AUTO :
+                order.equals("asc") || order.equals("auto") ? CompassQuery.SortDirection.AUTO : CompassQuery.SortDirection.REVERSE;
     }
 }
--- RcmdbCommons/test/unit/com/ifountain/compass/integration/CompassSortingTest.groovy
+++ RcmdbCommons/test/unit/com/ifountain/compass/integration/CompassSortingTest.groovy
@@ -1,11 +1,8 @@
 package com.ifountain.compass.integration
 
-import com.ifountain.rcmdb.test.util.RapidCmdbWithCompassTestCase
 import com.ifountain.compass.CompassTestObject
 import com.ifountain.compass.CompositeDirectoryWrapperProvider
-import application.RsApplication
-import com.ifountain.rcmdb.test.util.CompassForTests
-import application.RsApplicationOperations
+import com.ifountain.rcmdb.test.util.RapidCmdbWithCompassTestCase
 
 /**
 * Created by IntelliJ IDEA.
@@ -14,7 +11,7 @@ import application.RsApplicationOperations
 * Time: 1:36:57 PM
 * To change this template use File | Settings | File Templates.
 */
-class CompassSortingTest extends RapidCmdbWithCompassTestCase{
+class CompassSortingTest extends RapidCmdbWithCompassTestCase {
 
     public void setUp() {
         super.setUp(); //To change body of overridden methods use File | Settings | File Templates.
@@ -26,115 +23,178 @@ class CompassSortingTest extends RapidCmdbWithCompassTestCase{
 
     public void testSortingWithUnTokenizedFields()
     {
-        initialize ([CompassTestObject], [], false);
-        def insts =[CompassTestObject.add(prop1: "propertytoken1 aprop bprop cprop"),
-        CompassTestObject.add(prop1: "propertytoken1 bprop cprop"),
-        CompassTestObject.add(prop1: "propertytoken1 cprop"),
-        CompassTestObject.add(prop1: "Propertytoken1")]
-        assertEquals (insts.size(), CompassTestObject.count());
-        def returnedObjects = CompassTestObject.search("alias:*", [sort:"prop1"]).results
-        insts = insts.sort {obj1, obj2->
+        initialize([CompassTestObject], [], false);
+        def insts = [CompassTestObject.add(prop1: "propertytoken1 aprop bprop cprop"),
+                CompassTestObject.add(prop1: "propertytoken1 bprop cprop"),
+                CompassTestObject.add(prop1: "propertytoken1 cprop"),
+                CompassTestObject.add(prop1: "Propertytoken1")]
+        assertEquals(insts.size(), CompassTestObject.count());
+        def returnedObjects = CompassTestObject.search("alias:*", [sort: "prop1"]).results
+        insts = insts.sort {obj1, obj2 ->
             return obj1.prop1.toLowerCase().compareTo(obj2.prop1.toLowerCase());
         }
-        for(int i=0; i < insts.size(); i++)
+        for (int i = 0; i < insts.size(); i++)
         {
             def expectedInstance = insts[i]
-            assertEquals (expectedInstance.id, returnedObjects[i].id)
+            assertEquals(expectedInstance.id, returnedObjects[i].id)
         }
     }
 
     public void testSortingWithObjectsExtendingFromSameParentButOneOfThemDoesNotHaveSortProperty()
     {
-        initialize ([SortTestParentObject, SortTestoLevel2Child1, SortTestoLevel2Child2, SortTestoLevel1Child1,SortTestoLevel1Child2], [], false);
-        def insts =[
+        initialize([SortTestParentObject, SortTestoLevel2Child1, SortTestoLevel2Child2, SortTestoLevel1Child1, SortTestoLevel1Child2], [], false);
+        def insts = [
                 SortTestoLevel2Child1.add(prop3: "prop1 property value1"),
                 SortTestoLevel2Child1.add(prop3: "prop1 property value2"),
                 SortTestoLevel2Child1.add(prop3: "prop1 property value3"),
                 SortTestoLevel2Child2.add(prop4: "prop1 property value")
         ]
-        assertEquals (insts.size(), SortTestParentObject.count());
-        def returnedObjects = SortTestParentObject.search("alias:*", [sort:"prop3", order:"asc"]).results
-        assertEquals (insts[3].id, returnedObjects[0].id);
-        assertEquals (insts[0].id, returnedObjects[1].id);
-        assertEquals (insts[1].id, returnedObjects[2].id);
-        assertEquals (insts[2].id, returnedObjects[3].id);
+        assertEquals(insts.size(), SortTestParentObject.count());
+        def returnedObjects = SortTestParentObject.search("alias:*", [sort: "prop3", order: "asc"]).results
+        assertEquals(insts[3].id, returnedObjects[0].id);
+        assertEquals(insts[0].id, returnedObjects[1].id);
+        assertEquals(insts[1].id, returnedObjects[2].id);
+        assertEquals(insts[2].id, returnedObjects[3].id);
 
-        returnedObjects = SortTestParentObject.search("alias:*", [sort:"prop3", order:"desc"]).results
-        assertEquals (insts[2].id, returnedObjects[0].id);
-        assertEquals (insts[1].id, returnedObjects[1].id);
-        assertEquals (insts[0].id, returnedObjects[2].id);
-        assertEquals (insts[3].id, returnedObjects[3].id);
+        returnedObjects = SortTestParentObject.search("alias:*", [sort: "prop3", order: "desc"]).results
+        assertEquals(insts[2].id, returnedObjects[0].id);
+        assertEquals(insts[1].id, returnedObjects[1].id);
+        assertEquals(insts[0].id, returnedObjects[2].id);
+        assertEquals(insts[3].id, returnedObjects[3].id);
     }
 
     public void testSortingWithObjectsExtendingFromSameParentButOneOfThemDoesNotHaveLongSortProperty()
     {
-        initialize ([SortTestParentObject, SortTestoLevel2Child1, SortTestoLevel2Child2, SortTestoLevel1Child1,SortTestoLevel1Child2], [], false);
-        def insts =[
+        initialize([SortTestParentObject, SortTestoLevel2Child1, SortTestoLevel2Child2, SortTestoLevel1Child1, SortTestoLevel1Child2], [], false);
+        def insts = [
                 SortTestoLevel2Child1.add(propInt: 1),
                 SortTestoLevel2Child1.add(propInt: 2),
                 SortTestoLevel2Child1.add(propInt: 3)
         ]
-        assertEquals (insts.size(), SortTestParentObject.count());
-        def returnedObjects = SortTestParentObject.search("alias:*", [sort:"propInt", order:"asc"]).results
-        assertEquals (insts[0].id, returnedObjects[0].id);
-        assertEquals (insts[1].id, returnedObjects[1].id);
-        assertEquals (insts[2].id, returnedObjects[2].id);
+        assertEquals(insts.size(), SortTestParentObject.count());
+        def returnedObjects = SortTestParentObject.search("alias:*", [sort: "propInt", order: "asc"]).results
+        assertEquals(insts[0].id, returnedObjects[0].id);
+        assertEquals(insts[1].id, returnedObjects[1].id);
+        assertEquals(insts[2].id, returnedObjects[2].id);
 
-        returnedObjects = SortTestParentObject.search("alias:*", [sort:"propInt", order:"desc"]).results
-        assertEquals (insts[2].id, returnedObjects[0].id);
-        assertEquals (insts[1].id, returnedObjects[1].id);
-        assertEquals (insts[0].id, returnedObjects[2].id);
+        returnedObjects = SortTestParentObject.search("alias:*", [sort: "propInt", order: "desc"]).results
+        assertEquals(insts[2].id, returnedObjects[0].id);
+        assertEquals(insts[1].id, returnedObjects[1].id);
+        assertEquals(insts[0].id, returnedObjects[2].id);
     }
 
     public void testSortingWithNumberProperty()
     {
-        initialize ([SortTestParentObject, SortTestoLevel2Child1, SortTestoLevel2Child2, SortTestoLevel1Child1,SortTestoLevel1Child2], [], true);
-        def insts =[
+        initialize([SortTestParentObject, SortTestoLevel2Child1, SortTestoLevel2Child2, SortTestoLevel1Child1, SortTestoLevel1Child2], [], true);
+        def insts = [
                 SortTestoLevel2Child1.add(propInt: 1111),
                 SortTestoLevel2Child1.add(propInt: 112),
                 SortTestoLevel2Child1.add(propInt: 12),
         ]
-        assertEquals (insts.size(), SortTestParentObject.count());
-        def returnedObjects = SortTestParentObject.search("alias:*", [sort:"propInt", order:"asc"]).results
-        assertEquals (insts[2].id, returnedObjects[0].id);
-        assertEquals (insts[1].id, returnedObjects[1].id);
-        assertEquals (insts[0].id, returnedObjects[2].id);
+        assertEquals(insts.size(), SortTestParentObject.count());
+        def returnedObjects = SortTestParentObject.search("alias:*", [sort: "propInt", order: "asc"]).results
+        assertEquals(insts[2].id, returnedObjects[0].id);
+        assertEquals(insts[1].id, returnedObjects[1].id);
+        assertEquals(insts[0].id, returnedObjects[2].id);
 
-        returnedObjects = SortTestParentObject.search("alias:*", [sort:"propInt", order:"desc"]).results
-        assertEquals (insts[0].id, returnedObjects[0].id);
-        assertEquals (insts[1].id, returnedObjects[1].id);
-        assertEquals (insts[2].id, returnedObjects[2].id);
+        returnedObjects = SortTestParentObject.search("alias:*", [sort: "propInt", order: "desc"]).results
+        assertEquals(insts[0].id, returnedObjects[0].id);
+        assertEquals(insts[1].id, returnedObjects[1].id);
+        assertEquals(insts[2].id, returnedObjects[2].id);
     }
 
     public void testSortingWithNonExistingProperty()
     {
-        initialize ([SortTestParentObject, SortTestoLevel2Child1, SortTestoLevel2Child2, SortTestoLevel1Child1,SortTestoLevel1Child2], [], false);
-        def returnedObjects = SortTestoLevel2Child1.search("alias:*", [sort:"undefinedProp", order:"asc"]).results
-        assertEquals (0, returnedObjects.size())
+        initialize([SortTestParentObject, SortTestoLevel2Child1, SortTestoLevel2Child2, SortTestoLevel1Child1, SortTestoLevel1Child2], [], false);
+        def returnedObjects = SortTestoLevel2Child1.search("alias:*", [sort: "undefinedProp", order: "asc"]).results
+        assertEquals(0, returnedObjects.size())
     }
 
     public void testSortingWithNoInstance()
     {
-        initialize ([SortTestParentObject, SortTestoLevel2Child1, SortTestoLevel2Child2, SortTestoLevel1Child1,SortTestoLevel1Child2], [], false);
-        def returnedObjects = SortTestoLevel2Child1.search("alias:*", [sort:"prop3", order:"asc"]).results
-        assertEquals (0, returnedObjects.size())
-        returnedObjects = SortTestParentObject.search("alias:*", [sort:"prop3", order:"asc"]).results
-        assertEquals (0, returnedObjects.size())
-        returnedObjects = SortTestParentObject.search("alias:*", [sort:"propInt", order:"asc"]).results
-        assertEquals (0, returnedObjects.size())
+        initialize([SortTestParentObject, SortTestoLevel2Child1, SortTestoLevel2Child2, SortTestoLevel1Child1, SortTestoLevel1Child2], [], false);
+        def returnedObjects = SortTestoLevel2Child1.search("alias:*", [sort: "prop3", order: "asc"]).results
+        assertEquals(0, returnedObjects.size())
+        returnedObjects = SortTestParentObject.search("alias:*", [sort: "prop3", order: "asc"]).results
+        assertEquals(0, returnedObjects.size())
+        returnedObjects = SortTestParentObject.search("alias:*", [sort: "propInt", order: "asc"]).results
+        assertEquals(0, returnedObjects.size())
     }
 
     public void testSortingCastExceptionDoesNotOccur()
     {
-        initialize ([SortTestParentObject, SortTestoLevel2Child1, SortTestoLevel2Child2, SortTestoLevel1Child1,SortTestoLevel1Child2], [], false);
+        initialize([SortTestParentObject, SortTestoLevel2Child1, SortTestoLevel2Child2, SortTestoLevel1Child1, SortTestoLevel1Child2], [], false);
 
-        SortTestoLevel2Child1.add(d:System.currentTimeMillis());
+        SortTestoLevel2Child1.add(d: System.currentTimeMillis());
         SortTestParentObject.add([:]);
 
         def returnedObjects = SortTestParentObject.search("alias:*").total
-        assertEquals (2, returnedObjects)
-        returnedObjects = SortTestParentObject.search("alias:*", [sort:"d"]).total
-        assertEquals (2, returnedObjects)
+        assertEquals(2, returnedObjects)
+        returnedObjects = SortTestParentObject.search("alias:*", [sort: "d"]).total
+        assertEquals(2, returnedObjects)
+    }
+
+    public void testSortingWithMultipleFields()
+    {
+        initialize([CompassTestObject], [], false);
+        def insts = [
+                CompassTestObject.add(prop1: "prop1value3", prop2: "prop2value2"),
+                CompassTestObject.add(prop1: "prop1value3", prop2: "prop2value1"),
+                CompassTestObject.add(prop1: "prop1value3", prop2: "prop2value3"),
+                CompassTestObject.add(prop1: "prop1value2", prop2: "prop2value2"),
+                CompassTestObject.add(prop1: "prop1value2", prop2: "prop2value1"),
+                CompassTestObject.add(prop1: "prop1value2", prop2: "prop2value3"),
+                CompassTestObject.add(prop1: "prop1value1", prop2: "prop2value2"),
+                CompassTestObject.add(prop1: "prop1value1", prop2: "prop2value1"),
+                CompassTestObject.add(prop1: "prop1value1", prop2: "prop2value3"),
+
+        ]
+        assertEquals(insts.size(), CompassTestObject.count());
+        def returnedObjects = CompassTestObject.search("alias:*", [sort: "prop1,  prop2"]).results
+        insts = insts.sort {obj1, obj2 ->
+            def obj1prop1 = obj1.prop1.toLowerCase();
+            def obj2prop1 = obj2.prop1.toLowerCase();
+            return obj1prop1 > obj2prop1 ? 1 : obj2prop1 > obj1prop1 ? -1 : obj1.prop2.toLowerCase() > obj2.prop2.toLowerCase() ? 1 : -1;
+        }
+        for (int i = 0; i < insts.size(); i++)
+        {
+            def expectedInstance = insts[i]
+            assertEquals(expectedInstance.id, returnedObjects[i].id)
+        }
+
+        returnedObjects = CompassTestObject.search("alias:*", [sort: ["prop1", "prop2"]]).results
+        for (int i = 0; i < insts.size(); i++)
+        {
+            def expectedInstance = insts[i]
+            assertEquals(expectedInstance.id, returnedObjects[i].id)
+        }
+        returnedObjects = CompassTestObject.search("alias:*", [sort: ["prop1", "prop2"], order: "asc, asc"]).results
+
+        for (int i = 0; i < insts.size(); i++)
+        {
+            def expectedInstance = insts[i]
+            assertEquals(expectedInstance.id, returnedObjects[i].id)
+        }
+
+        returnedObjects = CompassTestObject.search("alias:*", [sort: ["prop1", "prop2"], order: ["asc", "asc"]]).results
+
+        for (int i = 0; i < insts.size(); i++)
+        {
+            def expectedInstance = insts[i]
+            assertEquals(expectedInstance.id, returnedObjects[i].id)
+        }
+
+        returnedObjects = CompassTestObject.search("alias:*", [sort: "prop1,  prop2", order: ["desc", "desc"]]).results
+        insts = insts.sort {obj1, obj2 ->
+            def obj1prop1 = obj1.prop1.toLowerCase();
+            def obj2prop1 = obj2.prop1.toLowerCase();
+            return obj1prop1 < obj2prop1 ? 1 : obj2prop1 < obj1prop1 ? -1 : obj1.prop2.toLowerCase() < obj2.prop2.toLowerCase() ? 1 : -1;
+        }
+        for (int i = 0; i < insts.size(); i++)
+        {
+            def expectedInstance = insts[i]
+            assertEquals(expectedInstance.id, returnedObjects[i].id)
+        }
     }
 }
 
@@ -149,13 +209,13 @@ class SortTestParentObject {
     Long id
     Long version
     Long d = new Long(0);
-    Long rsInsertedAt =0;
-    Long rsUpdatedAt =0;
+    Long rsInsertedAt = 0;
+    Long rsUpdatedAt = 0;
 
     static constraints = {
-        errors(nullable:true);
-        __operation_class__(nullable:true);
-        __dynamic_property_storage__(nullable:true);
+        errors(nullable: true);
+        __operation_class__(nullable: true);
+        __dynamic_property_storage__(nullable: true);
     }
 }
 

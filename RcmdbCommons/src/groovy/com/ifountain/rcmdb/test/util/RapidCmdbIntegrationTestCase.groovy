747d7ca0beab89b369800381ed2f8a60472f5459
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2009-12-25 16:34:29 +0000

    CMDB-1791 - Fix test failures
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@8606 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RcmdbCommons/src/groovy/com/ifountain/rcmdb/test/util/RapidCmdbIntegrationTestCase.groovy
+++ RcmdbCommons/src/groovy/com/ifountain/rcmdb/test/util/RapidCmdbIntegrationTestCase.groovy
@@ -1,5 +1,7 @@
 package com.ifountain.rcmdb.test.util
 
+import com.ifountain.comp.test.util.CommonTestUtils
+
 /* All content copyright (C) 2004-2008 iFountain, LLC., except as may otherwise be
 * noted in a separate copyright notice. All rights reserved.
 * This file is part of RapidCMDB.

d4242a0e2b4cee6e25556c5053077c2f1fae1299
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2010-01-25 07:50:15 +0000

    added code to disconnect the connections also ( when http disconnect param is given )
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@8785 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidInsight/scripts/connectionKill.groovy
+++ RapidInsight/scripts/connectionKill.groovy
@@ -1,10 +1,28 @@
 import com.ifountain.core.connection.ConnectionManager;
 
 def name=params.name;
+def disconnect=params.disconnect;
 
 if(!name)
 {
 	return "name parameter can not be empty"
+}
+
+if(disconnect)
+{
+    def pools=ConnectionManager.pools;
+    pools[name].getBorrowedConnections().each{ con ->
+      Thread.start(){
+          try{
+            con.disconnect();
+          }
+          catch(e)
+          {
+            logger.warn("Exception occured while disconnecting connection : ${name} ${con}")
+          }
+      }
+
+    }
 }
 
 

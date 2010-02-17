34f70397a678eeba63f43bf6686ff210c38c4e4d
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2010-02-17 07:26:06 +0000

    CMDB-1876 - Default periodic script to take Data Backup ( not enabled by default ).
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@8918 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidInsight/scripts/backup.groovy
+++ RapidInsight/scripts/backup.groovy
@@ -8,7 +8,7 @@ def backupDirectory="backup";
 SimpleDateFormat df = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
 String directory = "${backupDirectory}/${df.format(new Date())}".toString();
 
-
+logger.warn("-----------------------------------------------------------------------------");
 logger.warn("********** Starting backup to directory '${directory}' **********");
 application.RapidApplication.backup (directory);
 logger.warn("********** Backup successfully done to directory '${directory}' **********");

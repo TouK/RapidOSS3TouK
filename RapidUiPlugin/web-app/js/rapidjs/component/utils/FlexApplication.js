3e9a8ccf8cdc555daac8644c6b8e4f6f6d0cf8b1
sezgin <sezgin@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2010-02-22 15:34:22 +0000

    TopologyMap does not show in Linux ( Ubuntu ) Firefox 3.5 Flash 9 and Flash 10
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@8950 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidUiPlugin/web-app/js/rapidjs/component/utils/FlexApplication.js
+++ RapidUiPlugin/web-app/js/rapidjs/component/utils/FlexApplication.js
@@ -73,12 +73,12 @@ YAHOO.rapidjs.component.FlexApplication.prototype =
 							<param name='allowScriptAccess' value='always' /> \
 							<param name='swliveconnect' value='true' /> \
 							<param value='Transparent' name='wmode'/> \
-							<param value='eventHandler=flexApplicationEventHandler' name='flashvars'/> \
+							<param value='eventHandler=flexApplicationEventHandler&appId=" + this.flexObjectId+"' name='flashvars'/> \
 							<embed src='"+this.swfUrl+"' id='"+this.flexEmbedId+"' quality='high' \
 								width='100%' height='100%' name='"+this.flexEmbedId+"' align='middle' \
 								play='true'  \
 								loop='false' \
-								flashvars='eventHandler=flexApplicationEventHandler' \
+								flashvars='eventHandler=flexApplicationEventHandler&appId=" + this.flexObjectId+"' \
 								quality='high' \
 								swliveconnect='true' \
 								wmode = 'Transparent' \

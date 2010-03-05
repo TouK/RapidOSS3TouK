42dc201659583b7703dbce7c9c28d7991c58091d
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2010-03-05 13:15:11 +0000

    change schdule time to 12 hours
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@9020 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidInsight/solutions/ActiveActiveRedundancy/scripts/createRedundancyDefaults.groovy
+++ RapidInsight/solutions/ActiveActiveRedundancy/scripts/createRedundancyDefaults.groovy
@@ -32,7 +32,7 @@ scriptsToAdd.add([name:"createRedundancyConnections",logFileOwn:true])
 scriptsToAdd.add([name:"updatedObjects", logFileOwn:true]);
 scriptsToAdd.add([name:"synchronizeUpdatedObjects", type:CmdbScript.SCHEDULED, scheduleType:CmdbScript.PERIODIC,enabled:true,period:60,logFileOwn:true]);
 scriptsToAdd.add([name:"synchronizeDeletedObjects", type:CmdbScript.SCHEDULED, scheduleType:CmdbScript.PERIODIC,enabled:true,period:60,startDelay:15,logFileOwn:true]);
-scriptsToAdd.add([name:"clearExpiredDeletedObjects", type:CmdbScript.SCHEDULED, scheduleType:CmdbScript.PERIODIC,enabled:true,period:360000*12,logFileOwn:true]);
+scriptsToAdd.add([name:"clearExpiredDeletedObjects", type:CmdbScript.SCHEDULED, scheduleType:CmdbScript.PERIODIC,enabled:true,period:3600*12,logFileOwn:true]);
 
 //master switching scripts
 scriptsToAdd.add([name:"enableLocalMaster", logFileOwn:true]);

7d1ee9a9ff31e7916f0126713b4eac78de2c2f4f
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2010-03-19 16:08:12 +0000

    CMDB-1902 - Active-Active Redundancy Solution Refactoring.
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@9056 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

---
+++ RapidInsight/solutions/ActiveActiveRedundancy/operations/auth/GroupOperations.groovy
@@ -0,0 +1,163 @@
+package auth
+
+import com.ifountain.rcmdb.auth.SegmentQueryHelper
+import com.ifountain.rcmdb.exception.MessageSourceException
+import com.ifountain.rcmdb.auth.UserConfigurationSpace
+
+/**
+* Created by IntelliJ IDEA.
+* User: mustafa sener
+* Date: Dec 17, 2008
+* Time: 5:16:08 PM
+* To change this template use File | Settings | File Templates.
+*/
+class GroupOperations extends com.ifountain.rcmdb.domain.operation.AbstractDomainOperation
+{
+    def beforeDelete()
+    {
+        if (name.equalsIgnoreCase(RsUser.RSADMIN))
+        {
+            throw new Exception("Can not delete group ${RsUser.RSADMIN}");
+        }
+    }
+
+    //	changed for Active-Active Redundancy
+    def afterInsert() {
+        SegmentQueryHelper.getInstance().calculateGroupFilters(name);
+        UserConfigurationSpace.getInstance().groupAdded(this.domainObject);
+
+        application.RapidApplication.getUtility("RedundancyUtility").objectInAfterInsert(this.domainObject);
+    }
+
+    def afterUpdate(params) {
+        if (params.updatedProps.containsKey("segmentFilter") || params.updatedProps.containsKey("segmentFilterType")) {
+            SegmentQueryHelper.getInstance().calculateGroupFilters(name);
+        }
+
+        if (params.updatedProps.containsKey("name")) {
+            UserConfigurationSpace.getInstance().groupRemoved(params.updatedProps.name);
+        }
+        UserConfigurationSpace.getInstance().groupAdded(this.domainObject);
+
+        application.RapidApplication.getUtility("RedundancyUtility").objectInAfterUpdate(this.domainObject);
+    }
+    def afterDelete() {
+        SegmentQueryHelper.getInstance().removeGroupFilters(name);
+        UserConfigurationSpace.getInstance().groupRemoved(name)
+
+        application.RapidApplication.getUtility("RedundancyUtility").objectInAfterDelete(this.domainObject);
+    }
+    //change ended
+
+
+    public static Group addGroup(params)
+    {
+        return _addGroup(params, false);
+    }
+    public static Group addUniqueGroup(params)
+    {
+        return _addGroup(params, true);
+    }
+    private static Group _addGroup(params, boolean addUnique)
+    {
+        def group = null;
+
+        if (params.role == null)
+        {
+            throw new MessageSourceException("no.role.specified", [] as Object[]);
+        }
+
+        params.users = getUsersFromRepository(params.users)
+
+        if (addUnique)
+        {
+            group = Group.addUnique(params);
+        }
+        else
+        {
+            group = Group.add(params);
+        }
+        return group;
+    }
+
+    public static Group updateGroup(group, params)
+    {
+        if (params.users != null)
+        {
+            params.users = getUsersFromRepository(params.users)
+        }
+        if (params.containsKey("role"))
+        {
+            if (params.role == null)
+            {
+                throw new MessageSourceException("no.role.specified", [] as Object[]);
+            }
+        }
+
+        group.update(params);
+        return group;
+    }
+
+    public static void removeGroup(Group group) {
+        group.remove();
+    }
+    private static List getUsersFromRepository(List users)
+    {
+        def usersToBeAdded = [];
+        users.each {userObject ->
+            def username = userObject;
+            if (userObject instanceof RsUser)
+            {
+                username = userObject.username;
+            }
+            RsUser rsUser = RsUser.get(username: username);
+            if (rsUser == null)
+            {
+                throw new Exception("Could not created group since user ${username} does not exist.");
+            }
+            usersToBeAdded.add(rsUser);
+        }
+        return usersToBeAdded;
+    }
+
+    public void addUsers(List userList)
+    {
+        List usersToBeAdded = getUsersFromRepository(userList);
+        addRelation(users: usersToBeAdded);
+        UserConfigurationSpace.getInstance().groupAdded(domainObject);
+    }
+
+    public void removeUsers(List userList)
+    {
+        List usersToBeRemoved = getUsersFromRepository(userList);
+        removeRelation(users: usersToBeRemoved);
+        UserConfigurationSpace.getInstance().groupAdded(domainObject);
+    }
+
+    public void assignRole(role)
+    {
+        addRelation(role: getRole(role));
+        UserConfigurationSpace.getInstance().groupAdded(domainObject);
+    }
+
+    public void removeRole(role)
+    {
+        removeRelation(role: getRole(role));
+        UserConfigurationSpace.getInstance().groupAdded(domainObject);
+    }
+
+    private static getRole(role)
+    {
+        String roleName = role;
+        if (role instanceof Role)
+        {
+            roleName = role.name;
+        }
+        def roleFromRi = Role.get(name: roleName);
+        if (roleFromRi == null)
+        {
+            throw new Exception("Role ${roleName} does not exist");
+        }
+        return roleFromRi;
+    }
+}
---
+++ RapidInsight/solutions/ActiveActiveRedundancy/operations/auth/RsUserOperations.groovy
@@ -0,0 +1,310 @@
+package auth
+
+import application.RapidApplication
+import com.ifountain.rcmdb.auth.UserConfigurationSpace
+import com.ifountain.rcmdb.domain.util.ControllerUtils
+import com.ifountain.rcmdb.exception.MessageSourceException
+import org.jsecurity.authc.AccountException
+import org.jsecurity.crypto.hash.Sha1Hash
+import com.ifountain.rcmdb.util.ExecutionContextManagerUtils
+import com.ifountain.rcmdb.util.DataStore
+import com.ifountain.rcmdb.util.RapidCMDBConstants
+
+/**
+* Created by IntelliJ IDEA.
+* User: mustafa sener
+* Date: Dec 17, 2008
+* Time: 3:21:02 PM
+* To change this template use File | Settings | File Templates.
+*/
+class RsUserOperations extends com.ifountain.rcmdb.domain.operation.AbstractDomainOperation
+{
+    public static List getChannelTypes()
+    {
+        def channelTypes=[];
+        channelTypes.addAll(getConfiguredDestinationNames());
+        //channelTypes.add("email");     //existing destinations from connectors can be added/removed
+        return channelTypes;
+    }
+    public static List getEditableChannelTypes()
+    {
+        def channelTypes=[];
+        channelTypes.addAll(getConfiguredDestinationNames());
+        //channelTypes.add("email");     // existing destinations from connectors can be added/removed
+        return channelTypes;
+    }
+    public static def getConfiguredDestinationNames()
+    {
+        def configuredDestinationNames=DataStore.get(RapidCMDBConstants.CONFIGURED_DESTINATIONS_CACHE_KEY_NAME);
+        if(configuredDestinationNames==null)
+        {
+           configuredDestinationNames=[];
+        }
+        return configuredDestinationNames;
+    }
+
+    public static def getAuthenticator()
+    {
+        return "auth.RsUserLocalAuthenticator";
+    }
+    public static RsUser authenticateUser(params)
+    {
+        String username = params.login;
+        String loginToken = params.loginToken;
+
+        if (username)
+        {
+            String selectedAuthenticator = RsUser.getAuthenticator();
+            if(username == RsUser.RSADMIN)
+            {
+               selectedAuthenticator="auth.RsUserLocalAuthenticator";
+            }
+            return RapidApplication.getUtility(selectedAuthenticator).authenticateUser(params);
+        }
+        else if (loginToken)
+        {
+            return RapidApplication.getUtility("auth.RsUserTokenAuthenticator").authenticateUser(params);
+        }
+        else
+        {
+            throw new AccountException('Login or LoginToken must be specified for authentication.');
+        }
+    }
+
+    //Before & After Triggers
+    def beforeDelete()
+    {
+        if (username.equalsIgnoreCase(getCurrentUserName()))
+        {
+            throw new Exception("Can not delete your own account");
+        }
+        if (username.equalsIgnoreCase(RsUser.RSADMIN))
+        {
+            throw new Exception("Can not delete user ${RsUser.RSADMIN}");
+        }
+    }
+    //	changed for Active-Active Redundancy
+	def afterInsert()
+    {
+        UserConfigurationSpace.getInstance().userAdded(this.domainObject);
+
+        application.RapidApplication.getUtility("RedundancyUtility").objectInAfterInsert(this.domainObject);
+    }
+	def afterUpdate(params)
+    {
+        if (params.updatedProps.containsKey("username")) {
+            UserConfigurationSpace.getInstance().userRemoved(params.updatedProps.username);
+        }
+        UserConfigurationSpace.getInstance().userAdded(this.domainObject);
+
+        application.RapidApplication.getUtility("RedundancyUtility").objectInAfterUpdate(this.domainObject);
+    }
+	def afterDelete()
+    {
+        UserConfigurationSpace.getInstance().userRemoved(username);
+
+        application.RapidApplication.getUtility("RedundancyUtility").objectInAfterDelete(this.domainObject);
+    }
+    //change ended
+    //Before & After Triggers End
+
+    public static String hashPassword(password)
+    {
+        return new Sha1Hash(password).toHex();
+    }
+    public boolean isPasswordSame(passwordParam)
+    {
+        return passwordHash == hashPassword(passwordParam);
+    }
+    public static RsUser updateUser(user, params)
+    {
+        if (params.password != null)
+        {
+            params.passwordHash = hashPassword(params.password);
+        }
+
+        if (params.groups != null)
+        {
+            if (params.groups.isEmpty())
+            {
+                throw new MessageSourceException("no.group.specified", [] as Object[]);
+            }
+
+            params.groups = getGroupsFromRepository(params.groups);
+        }
+
+        user.update(params);
+        return user;
+    }
+    private static void restoreOldData(oldProperties)
+    {
+        oldProperties.each {object, oldObjectProperties ->
+            object.update(oldObjectProperties);
+        }
+    }
+    public static RsUser addUser(params)
+    {
+        return _addUser(params, false);
+    }
+    public static RsUser addUniqueUser(params)
+    {
+        return _addUser(params, true);
+    }
+    private static RsUser _addUser(params, boolean addUnique)
+    {
+        params.passwordHash = hashPassword(params.password);
+
+        def rsUser = null;
+
+        if (params.groups == null || params.groups.isEmpty())
+        {
+            throw new MessageSourceException("no.group.specified", [] as Object[]);
+        }
+
+        params.groups = getGroupsFromRepository(params.groups)
+
+        if (addUnique)
+        {
+            rsUser = RsUser.addUnique(params);
+        }
+        else
+        {
+            rsUser = RsUser.add(params);
+        }
+        return rsUser;
+    }
+    public static void removeUser(RsUser user) {
+        user.remove();
+    }
+
+    private static List getGroupsFromRepository(List groups)
+    {
+        def groupsToBeAssigned = [];
+        groups.each {groupObject ->
+            def groupName = groupObject;
+            if (groupObject instanceof Group)
+            {
+                groupName = groupObject.name;
+            }
+            Group group = Group.get(name: groupName);
+            if (group == null)
+            {
+                throw new Exception("Could not create user since Group ${groupName} does not exist.");
+            }
+            groupsToBeAssigned.add(group);
+        }
+        return groupsToBeAssigned;
+    }
+
+    public static String getCurrentUserName()
+    {
+        def currentuser=ExecutionContextManagerUtils.getUsernameFromCurrentContext();
+        if(currentuser ==null)
+        {
+            currentuser = "system";
+        }
+        return currentuser;
+    }
+
+
+    def addChannelInformation(channelParams) {
+        def channelInformation = ChannelUserInformation.add(userId: id, type: channelParams.type, destination: channelParams.destination)
+        if (!channelInformation.hasErrors())
+        {
+            addRelation(userInformations: channelInformation);
+        }
+        return channelInformation;
+    }
+
+    def retrieveChannelInformation(channelType) {
+        return ChannelUserInformation.get(userId: id, type: channelType);
+    }
+
+
+
+    def addChannelInformationsAndRollBackIfErrorOccurs(channelInformationList)
+    {
+        def oldInformationProperties = [:];
+        //save all old destinations
+        ChannelUserInformation.searchEvery("userId:${id}").each {userInfo ->
+            oldInformationProperties[userInfo.id] = ControllerUtils.backupOldData(userInfo, ["destination": ""])
+        }
+
+        def errorOccured = false;
+
+        def addedInformations = addChannelInformations(channelInformationList);
+
+        //add all informations and track if error occured
+        addedInformations.each {addedInfo ->
+            if (addedInfo.hasErrors())
+            {
+                errorOccured = true;
+            }
+        }
+
+        //roll back the information adds / updates if error occured
+        if (errorOccured)
+        {
+            addedInformations.each {addedInfo ->
+                if (!addedInfo.hasErrors()) //if has error no change is done
+                {
+                    //if information exists earlier update it else remove it
+                    def oldProperties = oldInformationProperties[addedInfo.id];
+                    if (oldProperties != null)
+                    {
+                        addedInfo.update(oldProperties);
+                    }
+                    else
+                    {
+                        addedInfo.remove();
+                    }
+                }
+            }
+        }
+
+        return addedInformations;
+    }
+
+
+    def addChannelInformations(channelInformationList)
+    {
+        def addedInformations = [];
+        channelInformationList.each {channelParams ->
+            addedInformations.add(addChannelInformation(channelParams));
+        }
+        return addedInformations;
+    }
+
+    def retrieveLdapInformation() {
+        return LdapUserInformation.get(userId: id, type: "ldap");
+    }
+
+    def addLdapInformation(params) {
+        params.userId = id;
+        params.type = "ldap"
+        def ldapInformation = LdapUserInformation.add(params)
+        if (!ldapInformation.hasErrors())
+        {
+            addRelation(userInformations: ldapInformation);
+        }
+        return ldapInformation;
+    }
+
+    def static hasRole(uName, roleName) {
+        return UserConfigurationSpace.getInstance().hasRole(uName, roleName)
+    }
+
+    def static hasAllRoles(uName, roleNames)
+    {
+        return UserConfigurationSpace.getInstance().hasAllRoles(uName, roleNames)
+    }
+
+    def static hasGroup(uName, groupName) {
+        return UserConfigurationSpace.getInstance().hasGroup(uName, groupName)
+    }
+
+    def static hasAllGroups(uName, groupNames)
+    {
+        return UserConfigurationSpace.getInstance().hasAllGroups(uName, groupNames)
+    }
+}

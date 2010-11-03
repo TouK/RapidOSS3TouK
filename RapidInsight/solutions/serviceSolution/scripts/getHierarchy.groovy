39b7c121ddc3c377826d830d22c06402664c6991
abdurrahim <abdurrahim@a08bcbf9-5f6c-4d33-af0a-e494bc2cacca>
2010-11-03 13:20:48 +0000

    support sub services
    git-svn-id: http://www.ifountain.org/repos/os/RapidModules@9268 a08bcbf9-5f6c-4d33-af0a-e494bc2cacca

--- RapidInsight/solutions/serviceSolution/scripts/getHierarchy.groovy
+++ RapidInsight/solutions/serviceSolution/scripts/getHierarchy.groovy
@@ -9,26 +9,26 @@ def sw = new StringWriter();
 builder = new MarkupBuilder(sw);
 
 searchParams = [max: "1000",sort:"name",order:"asc"];
-
+serviceStateMap=[:];
+serviceSubServicesMap=[:];
 
 builder.Objects() {
-    RsService.searchEvery("alias:*",searchParams).each { service ->
+    RsService.searchEvery("serviceName:\"\" ",searchParams).each { service ->
         printService(service);
     }
 }
 return sw.toString();
 
 
-def printService(service){
-   def state=0;
-   def severityResults=RsEvent.getPropertyValues("serviceName:${service.name.toQuery()}", ["severity"], [sort: "severity", order: "desc", max: 1]).severity;
-   if (severityResults.size() > 0)
-   {
-       state = severityResults[0];
-   }
+def printService(service)
+{
+   def state=getServiceState(service);
    builder.Object(id: service.id, name: service.name, displayName: service.displayName, nodeType: service.className,state:state){
+        getSubServices(service).each{ subService ->
+            printService(subService);
+        }
         RsComputerSystem.searchEvery("serviceName:${service.name.toQuery()}",searchParams).each{ device ->
-            printDevice(device);
+           printDevice(device);
         }
    }
 }
@@ -37,4 +37,34 @@ def printDevice(device)
 {
    def state=device.getState(); //device state is already saved by StateCalculator
    builder.Object(id: device.id, name: device.name, displayName: device.displayName, nodeType: device.className,state:state); 
+}
+
+def getSubServices(service)
+{
+    if(! serviceSubServicesMap.containsKey(service.name))
+   {
+         serviceSubServicesMap[service.name]=RsService.searchEvery("serviceName:${service.name.toQuery()}",searchParams);
+   }
+   return  serviceSubServicesMap[service.name];
+}
+def getServiceState(service)
+{
+   if(! serviceStateMap.containsKey(service.name))
+   {
+       def state=0;
+       def severityResults=RsEvent.getPropertyValues("serviceName:${service.name.toQuery()}", ["severity"], [sort: "severity", order: "desc", max: 1]).severity;
+       if (severityResults.size() > 0)
+       {
+           state = severityResults[0];
+       }
+       getSubServices(service).each{ subService ->
+            def subServiceState=getServiceState(subService);
+            if(subServiceState > state)
+            {
+                state=subServiceState;
+            }
+       }
+       serviceStateMap[service.name]=state;
+   }
+   return  serviceStateMap[service.name];
 }
